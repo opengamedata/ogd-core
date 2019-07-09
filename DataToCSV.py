a9b5347dab84f283e354def01c147ee4a8def410
Luke Swanson <swansonl@cs.wisc.edu>
2019-07-09 09:30:03 -0500

    Update function calls based on changes in parameters in other files, add some code to use metadata header from feature class, and remove some unnecessary code left from the old PHP code, which I initially had in because I wasn't sure if it would be needed or not.

--- DataToCSV.py
+++ DataToCSV.py
@@ -11,14 +11,8 @@ def dataToCSV(request: Request, db, settings):
     DATA_DIR = settings["DATA_DIR"] + request.game_id
     db_settings = settings["db_config"]
     if request.game_id is not None:
-        # TODO: Should we get model file still? Not sure what it's used for.
-        model = utils.loadJSONFile("model.json", "./")
-        # TODO: figure out if these are even needed.
-        SQL_QUESTION_CUSTOM = model[request.game_id]['sqlEventCustoms']['question']
-        SQL_MOVE_CUSTOM = model[request.game_id]['sqlEventCustoms']['move']
-        SQL_OTHER_CUSTOMS = model[request.game_id]['sqlEventCustoms']['other']
 
-        parse_result = getAndParseData(request, model, db, db_settings, DATA_DIR)
+        parse_result = getAndParseData(request, db, db_settings, DATA_DIR)
         
         # output results
         if parse_result is not None:
@@ -29,7 +23,7 @@ def dataToCSV(request: Request, db, settings):
     else:
         logging.error("Game ID was not given!")
 
-def getAndParseData(request: Request, model, db, settings, data_directory):
+def getAndParseData(request: Request, db, settings, data_directory):
     db_cursor = db.cursor(buffered=True)
 
     ## NOTE: I've left the bit of cache code ported from the old logger,
@@ -47,18 +41,29 @@ def getAndParseData(request: Request, model, db, settings, data_directory):
     #         utils.SQL.server500Error(err)
     
     max_level_raw = utils.SQL.SELECT(cursor=db_cursor, db_name=db.database, table=settings["table"],
-                                     columns=["MAX(level)"], filter="app_id=\"WAVES\"", distinct=True)
+                                     columns=["MAX(level)"], filter="app_id=\"{}\"".format(request.game_id),
+                                     distinct=True)
     max_level = max_level_raw[0][0]
     min_level_raw = utils.SQL.SELECT(cursor=db_cursor, db_name=db.database, table=settings["table"],
-                                     columns=["MIN(level)"], filter="app_id=\"WAVES\"", distinct=True)
+                                     columns=["MIN(level)"], filter="app_id=\"{}\"".format(request.game_id),
+                                     distinct=True)
     min_level = min_level_raw[0][0]
     # logging.debug("max_level: " + str(max_level))
 
+    # event_counts_by_session = utils.SQL.SELECT(cursor=db_cursor, db_name=db.database, table=settings["table"],
+                                    #    columns=["session_id", "COUNT(*) as event_count"], filter="app_id=\"{}\"".format(request.game_id),
+                                    #    grouping="session_id", distinct=False)
+    # logging.debug("event counts by session ID: " + str(event_counts_by_session))
+    # logging.debug("Number of sessions: " + str(len(event_counts_by_session)))
+    # sliced_by_session = [event_counts_by_session[i*1000:min((i+1)*1000, len(event_counts_by_session)-1)] for i in range(0,16)]
+    # counts_by_slice = [[elem[1] for elem in next_slice] for next_slice in sliced_by_session]
+    # logging.debug("events by 1000 ids" + str([sum(next_slice) for next_slice in counts_by_slice]))
+
     session_ids_raw = utils.SQL.SELECT(cursor=db_cursor, db_name=db.database, table=settings["table"],
-                                       columns=["session_id"],
-                                       filter="app_id=\"WAVES\"", limit=4, distinct=True)
+                                       columns=["session_id"], filter="app_id=\"{}\"".format(request.game_id),
+                                       distinct=True, limit=1)
     session_ids = [sess[0] for sess in session_ids_raw]
-    # logging.debug("session_ids: " + str(session_ids))
+    logging.debug("session_ids: " + str(session_ids))
 
     db_cursor.execute("SHOW COLUMNS from {}.{}".format(db.database, settings["table"]))
     col_names = [col[0] for col in db_cursor.fetchall()]
@@ -71,13 +76,23 @@ def getAndParseData(request: Request, model, db, settings, data_directory):
     #                + tuple(json.loads(single_elem[0][complex_data_index]).keys()) \
     #                + db_cursor.column_names[complex_data_index+1:-1]
 
+    WaveFeature.initializeClass("./game_features/schemas/", "WAVES.json")
+
     raw_csv_file = open("{}/{}_{}_raw.csv".format(data_directory, utils.dateToFileSafeString(request.start_date),
                                                utils.dateToFileSafeString(request.end_date)), "w")
     proc_csv_file = open("{}/{}_{}_proc.csv".format(data_directory, utils.dateToFileSafeString(request.start_date),
                                                  utils.dateToFileSafeString(request.end_date)), "w")
-    WaveFeature.initializeClass("./game_features/schemas/", "WAVES.json")
+    raw_metadata = utils.csvMetadata(game_name=request.game_id, begin_date=request.start_date, end_date=request.end_date,
+                                      field_list=WaveFeature.schema["db_columns"])
+    feature_descriptions = {**WaveFeature.schema["features"]["perlevel"], **WaveFeature.schema["features"]["aggregate"]}
+    proc_metadata = utils.csvMetadata(game_name=request.game_id, begin_date=request.start_date, end_date=request.end_date,
+                                      field_list=feature_descriptions)
 
-    raw_csv_file.write(",".join(col_names))
+    # after generating the metadata, write to each file
+    raw_csv_file.write(raw_metadata)
+    proc_csv_file.write(proc_metadata)
+    # then write the column headers for the raw csv. We'll get the processed csv later.
+    raw_csv_file.write(",".join(col_names) + "\n")
     session_features = []
     for sess_id in session_ids:
         # initialize vars for session data.
@@ -89,7 +104,7 @@ def getAndParseData(request: Request, model, db, settings, data_directory):
         next_data_set = utils.SQL.SELECT(cursor=db_cursor, db_name=db.database, table=settings["table"],
                                          filter="app_id=\"WAVES\" AND session_id={}".format(sess_id),
                                          sort_columns=["client_time"], sort_direction = "ASC",
-                                         limit=3, distinct=False)
+                                         distinct=False)
 
         # now, we process each row.
         for row in next_data_set:
@@ -105,6 +120,7 @@ def getAndParseData(request: Request, model, db, settings, data_directory):
         raw_csv_file.write("\n".join(raw_csv_lines))
         raw_csv_file.write("\n")
 
+    # Use functions from the feature class to write actual data
     session_features[0].writeCSVHeader(proc_csv_file)
     for session in session_features:
         session.writeCurrentFeatures(proc_csv_file)
