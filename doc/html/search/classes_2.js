feb47fed659678f4a9918dfd9ed102b416d2f0c6
Luke Swanson <swansonl@cs.wisc.edu>
2019-07-30 15:35:16 -0500

    One more update to code documentation files

--- doc/html/search/classes_2.js
+++ doc/html/search/classes_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['featureexporter',['FeatureExporter',['../class_data_to_c_s_v_1_1_feature_exporter.html',1,'DataToCSV']]]
+  ['featureexporter',['FeatureExporter',['../class_feature_exporter_1_1_feature_exporter.html',1,'FeatureExporter.FeatureExporter'],['../class_feature_exporter.html',1,'FeatureExporter']]]
 ];
