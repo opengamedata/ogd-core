81bcbfbb8907a34d24f7cef3838f62a68edee376
John <johnmccloskey2010@hotmail.com>
2019-08-01 13:01:01 -0500

    Added two rough webpages for opengamedata and realtime.html
    Included css and js files are in /websrc and the data used is in /data. Note that these pages are made mostly for functionality and still rough. The data isn't being staged. I'll have to commit that separately I think.

---
+++ websrc/scripts.js
@@ -0,0 +1,79 @@
+var tables;
+var table;
+var headers = {
+  "start_date": "Start Date",
+  "end_date": "End Date",
+  "date_modified": "Date Modified",
+  "Dataset ID": "Dataset ID",
+  "sessions": "Sessions",
+  "raw": "Downloads",
+
+}
+function change_tables(value, start=false) {
+  let table = document.querySelector("table");
+  table.innerHTML = '';
+  jQuery.getJSON("/data/file_list.json",function(result){
+    tables = result;
+    let table = document.querySelector("table");
+    generateTableHead(table, headers);
+    generateTable(table, tables[value], headers);
+    if(start) generate_options();
+  });
+}
+
+function generateTableHead(table, headers) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key in headers) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(headers[key]);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+function generateTable(table, data, headers) {
+  for (let setID in data) {
+    var set = data[setID]
+    let row = table.insertRow();
+    for (key in headers) {
+      let cell = row.insertCell();
+      var text = null;
+      switch (key) {
+        case "Dataset ID":
+          text = document.createTextNode(setID);
+          cell.appendChild(text);
+          break;
+        case "raw":
+            var a = document.createElement('a');
+            var linkText = document.createTextNode("Raw");
+            a.appendChild(linkText);
+            a.title = "Raw";
+            a.href = set["raw"];
+            cell.appendChild(a);
+            cell.append(document.createTextNode(' - '))
+            var a = document.createElement('a');
+            var linkText = document.createTextNode("Processed");
+            a.appendChild(linkText);
+            a.title = "Processed";
+            a.href = set["proc"];
+            cell.appendChild(a);
+          break;
+        default:
+            text = document.createTextNode(set[key]);
+            cell.appendChild(text);
+
+      }
+    }
+  }
+}
+
+function generate_options(){
+  select = document.getElementById("mySelect");
+  for(table_name in tables){
+    var option = document.createElement("option");
+    option.text = table_name;
+    select.add(option);
+  }
+}
+
+change_tables("CRYSTAL",true);
