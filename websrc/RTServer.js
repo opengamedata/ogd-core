61adeaaff039ec19654b03cdf5affbf87e9e39b0
Luke Swanson <swansonl@cs.wisc.edu>
2019-08-06 12:07:08 -0500

    dumb mistake, left out content-type from http requests

--- websrc/RTServer.js
+++ websrc/RTServer.js
@@ -1,20 +1,21 @@
 class Server
 {
-    static getGreeting(callback)
-    {
-        console.log("Received request for greeting")
-        var req = new XMLHttpRequest();
-        req.onreadystatechange = function()
-        {
-            if (this.readyState == 4 && this.status == 200)
-            {
-    	       console.log("onreadystatechange is executing")
-	       console.log("responseText was: " + this.responseText)
-               callback(this.responseText.toString());
-            }
-        }
-        req.open("POST", "realtime.cgi", true);
-        req.send("method=say_hello");
-        console.log("Sent POST call for say_hello function")
-    }
+   static getGreeting(callback)
+   {
+      console.log("Received request for greeting")
+      var req = new XMLHttpRequest();
+      req.onreadystatechange = function()
+      {
+         if (this.readyState == 4 && this.status == 200)
+         {
+            console.log("onreadystatechange is executing")
+            console.log("responseText was: " + this.responseText)
+            callback(this.responseText.toString());
+         }
+      }
+      req.open("POST", "realtime.cgi", true);
+      req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      req.send("method=say_hello");
+      console.log("Sent POST call for say_hello function")
+   }
 }
