b976a5b3eb6cb09f0ae06a875ace2155a8f9f3f3
Luke Swanson <swansonl@cs.wisc.edu>
2019-08-07 15:21:16 -0500

    Split Request to have two subtypes.
    One is for date ranges, as used by main.py for csv exports.
    The other is for specific session ids, which will be used
    by the realtime API.

--- Request.py
+++ Request.py
@@ -15,15 +15,25 @@ class Request:
     #  @param min_moves    The minimum number of moves (events) per session.
     #                      Any session with fewer moves is ignored.
     # TODO: Not actually sure if I ever set up code to ignore sessions with < min_moves.
-    def __init__(self, game_id: str = None, start_date: date = None, end_date: date = None,
-                 max_sessions: int = None, min_moves: int = None):
+    def __init__(self, game_id: str = None, max_sessions: int = None, min_moves: int = None):
         self.game_id = game_id
-        self.start_date = start_date
-        self.end_date = end_date
         self.max_sessions = max_sessions
         self.min_moves = min_moves
+
+class DateRangeRequest(Request):
+    def __init__(self, game_id: str = None, max_sessions: int = None, min_moves: int = None,
+                 start_date: date = None, end_date: date = None):
+        Request.__init__(self, game_id=game_id, max_sessions=max_sessions, min_moves=min_moves)
+        self.start_date = start_date
+        self.end_date = end_date
 
     ## String representation of a request. Just gives game id, and date range.
     def __str__(self):
         fmt = "%Y%m%d"
         return f"{self.game_id}: {self.start_date.strftime(fmt)}-{self.end_date.strftime(fmt)}"
+
+class IDListRequest(Request):
+    def __init__(self, game_id: str = None, max_sessions: int = None, min_moves: int = None,
+                    session_ids = []):
+        Request.__init__(self, game_id=game_id, max_sessions=max_sessions, min_moves=min_moves)
+        self._session_ids = session_ids
