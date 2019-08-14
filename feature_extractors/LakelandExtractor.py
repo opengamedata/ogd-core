6424cca4c320e98a0a869514576d78fb9976d4ed
John <johnmccloskey2010@hotmail.com>
2019-08-14 11:08:15 -0500

    Update Lakeland feature names and some extraction methods
    Not yet tested, so it may not work

--- feature_extractors/LakelandExtractor.py
+++ feature_extractors/LakelandExtractor.py
@@ -9,12 +9,17 @@ from feature_extractors.Extractor import Extractor
 from GameTable import GameTable
 from schemas.Schema import Schema
 from enum import Enum
+from collections import defaultdict
 
 
 
 ## @class CrystalExtractor
 #  Extractor subclass for extracting features from Crystal game data.
 class LakelandExtractor(Extractor):
+
+    _STR_TO_ENUM = utils.loadJSONFile("Lakeland Enumerators/Lakeland Enumerators.json")
+    _ENUM_TO_STR = {cat: {y: x for x, y in ydict.items()} for cat, ydict in _STR_TO_ENUM.items()}
+
     ## Constructor for the CrystalExtractor class.
     #  Initializes some custom private data (not present in base class) for use
     #  when calculating some features.
@@ -29,7 +34,9 @@ class LakelandExtractor(Extractor):
     #                     structured.
     def __init__(self, session_id: int, game_table: GameTable, game_schema: Schema):
         super().__init__(session_id=session_id, game_table=game_table, game_schema=game_schema)
-        self._ENUMERATORS = utils.loadJSONFile("Lakeland Enumerators/Lakeland Enumerators.json")
+        self._tile_marks = defaultdict(lambda x: [1,1,1,1])
+        self._client_start_time = None
+        self._num_checkpoints_completed = 0
 
 
 
@@ -45,6 +52,8 @@ class LakelandExtractor(Extractor):
         event_data_complex_parsed = row_with_complex_parsed[game_table.complex_data_index]
         event_type = row_with_complex_parsed[game_table.event_index]
         event_client_time = row_with_complex_parsed[game_table.client_time_index]
+        if not self._client_start_time:
+            self._client_start_time = event_client_time
         # Check for invalid row.
         if row_with_complex_parsed[game_table.session_id_index] != self.session_id:
             logging.error("Got a row with incorrect session id!")
@@ -114,74 +123,110 @@ class LakelandExtractor(Extractor):
                 raise Exception("Found an unrecognized event type: {}".format(event_type))
 
     def _extractFromGamestate(self, event_client_time, event_data_complex_parsed):
-        self.features.
+        d = event_data_complex_parsed
 
     def _extractFromStartgame(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromCheckpoint(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
+        self._num_checkpoints_completed += 1
+        if self._num_checkpoints_completed == 50:
+            self.features.setValByName(feature_name="time_in_tutorial_mode", new_value=event_client_time-self._client_start_time)
 
     def _extractFromSelecttile(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
+        self.features.incAggregateVal("num_inspect_tile")
 
     def _extractFromSelectfarmbit(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromSelectitem(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromSelectbuy(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromBuy(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
+        if d["success"]:
+            self.features.incAggregateVal(feature_name=
+                                          self._get_feature(base='num_buy_',enum=d["buy"],enum_type='Buys'))
 
     def _extractFromCancelbuy(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromRoadbuilds(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromTileuseselect(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
+        tile_array, marks = d["tile"], d["marks"]
+        if self._tile_mark_change(tile_array, marks):
+            self.features.incAggregateVal("num_change_tile_mark")
+
 
     def _extractFromItemuseselect(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
+        item_array = d["item"]
+        curr_mark = item_array[3]
+        if d["prev_mark"] != curr_mark:
+            self.features.incAggregateVal("num_change_tile_mark")
 
     def _extractFromTogglenutrition(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromToggleshop(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
+        if d["shop_open"]:
+            self.features.incAggregateVal("num_open_buy_menu")
 
     def _extractFromToggleachievements(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
+        if d["achievements_open"]:
+            self.features.incAggregateVal("num_open_achievements")
 
     def _extractFromSkiptutorial(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromSpeed(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromAchievement(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromFarmbitdeath(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromBlurb(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromClick(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromRainstopped(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromHistory(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
     def _extractFromEndgame(self, event_client_time, event_data_complex_parsed):
-        pass
+        d = event_data_complex_parsed
 
+    # UTILS
+
+    def _tile_mark_change(self, tile_array, marks):
+        tx,ty = tile_array[4], tile_array[5]
+        if marks == self._tile_marks[(tx,ty)]:
+            return False
+        else:
+            self._tile_marks[(tx,ty)] = marks
+            return True
+
+    @staticmethod
+    def get_feature(base, enum, enum_type):
+        return base + LakelandExtractor._ENUM_TO_STR[enum_type][enum]
+
+    @staticmethod
+    def str_to_int(str):
+        return int(str.strip())
