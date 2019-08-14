18300e1c097ad2f65d78705278a20e8971cf44e7
John <johnmccloskey2010@hotmail.com>
2019-08-14 14:57:37 -0500

    Update Lakeland's ENUM json and schema
    mostly cosmetic

--- Lakeland Enumerators/produce_lakeland_enumerators.py
+++ Lakeland Enumerators/produce_lakeland_enumerators.py
@@ -1,31 +1,5 @@
 import re
 import json
-event_categories = {
-        "gamestate": 0,
-        "startgame": 1,
-        "checkpoint": 2,
-        "selecttile": 3,
-        "selectfarmbit": 4,
-        "selectitem": 5,
-        "selectbuy": 6,
-        "buy": 7,
-        "cancelbuy": 8,
-        "roadbuilds": 9,
-        "tileuseselect": 10,
-        "itemuseselect": 11,
-        "togglenutrition": 12,
-        "toggleshop": 13,
-        "toggleachievements": 14,
-        "skiptutorial": 15,
-        "speed": 16,
-        "achievement": 17,
-        "farmbitdeath": 18,
-        "blurb": 19,
-        "click": 20,
-        "rainstopped": 21,
-        "history": 22,
-        "endgame": 23
-    }
 
 
 def main(path_to_lakeland_readme, outpath=None):
@@ -39,7 +13,6 @@ def main(path_to_lakeland_readme, outpath=None):
     """
     json_enums = {}
     table_enums = {}
-    json_enums['Event Categories'] = event_categories
 
     with open(path_to_lakeland_readme) as infile:
         enums = False
@@ -51,11 +24,14 @@ def main(path_to_lakeland_readme, outpath=None):
                 continue
             elif line.strip() == "## Data Structures":
                 enums = False
+                json_enums[table] = table_enums
+                table = ''
+                write = False
                 continue
             elif enums:
                 print(table, write, line)
                 if line.strip()[:4] == "####":
-                    table = line.strip()[4:].strip()
+                    table = line.strip()[4:].strip().upper()
                     table_enums = {}
                     continue
                 elif table and line.strip()[:2] == "<a":
@@ -77,5 +53,10 @@ def main(path_to_lakeland_readme, outpath=None):
         with open(outpath,'w') as file:
             json.dump(json_enums, file, indent=2)
     return json_enums
+
+if __name__ == '__main__':
+    path = r"/Users/johnmccloskey/Development/lakeland/README.md"
+    out = r"/Users/johnmccloskey/Development/opengamedata/Lakeland Enumerators/Lakeland Enumerators.json"
+    main(path, out)
 
 
