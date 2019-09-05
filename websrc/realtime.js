ae0d9e5c78f6a61d7c210aea394d84064cabe520
Luke Swanson <lwswanson2@wisc.edu>
2019-09-05 15:23:24 -0500

    Add code to display session list and session details.
    The code now creates a list of links for active sessions.
    Clicking on a link will display prediction values in the
    main display area.
    Active sessions are refreshed every 5 seconds. Prediction
    values are not refreshed, currently. This is a todo.

--- websrc/realtime.js
+++ websrc/realtime.js
@@ -27,7 +27,6 @@ function onload()
   window.setInterval(() => {
     try {
       sess_list.refreshActiveSessions();
-      sess_list.refreshDisplayedSessions();
     }
     catch(err) {
       console.log(err.message);
@@ -48,15 +47,16 @@ class SessionList
     this.active_game = document.getElementById("mySelect").value;
     this.active_session_ids = [];
     this.displayed_session_ids = [];
+    this.selected_session_id = -1;
     this.refreshActiveSessions();
-    this.refreshDisplayedSessions();
   }
 
   refreshActiveSessions() {
     let that = this;
     function active_sessions_handler(result) {
-      that.active_session_ids = result;
+      that.active_session_ids = JSON.parse(result);
       console.log(`Refreshed session IDs: ${that.active_session_ids}`);
+      that.refreshDisplayedSessions();
     };
     Server.get_all_active_sessions(active_sessions_handler, this.active_game);
   }
@@ -68,21 +68,64 @@ class SessionList
     let remove_set = setMinus(display_set, active_set); // subtract active from display to get inactives.
     let add_set    = setMinus(active_set, display_set); // subtract display from active to get new sessions.
     let session_list_area = document.getElementById("session_list");
-    for (let session_link in session_list_area.children) {
-      if (remove_set.has(session_link.id)) {
-        session_link.remove();
+    if (remove_set.size > 0) {
+      let child_nodes = Array.from(session_list_area.children);
+      for (let session_link_id in child_nodes) {
+        let session_link = child_nodes[session_link_id];
+        if (remove_set.has(session_link.id)) {
+          session_link.remove();
+          if (this.selected_session_id == session_link.id) { this.clearSelected(); }
+        }
       }
     }
-    for (let id in add_set) {
-      let session_link = document.createElement("a");
+    for (let id of add_set) {
       let session_id = id;
-      session_link.onclick = function() { that.displaySession(session_id); }
+      let session_div = document.createElement("div");
+      session_div.id = session_id;
+      let session_link = document.createElement("a");
+      session_link.onclick = function() { that.displaySelectedSession(session_id); }
       session_link.innerText = session_id;
+      session_link.href = `#${session_id}`;
+      session_div.appendChild(session_link);
+      session_div.appendChild(document.createElement("br"));
+      session_list_area.appendChild(session_div);
     }
     this.displayed_session_ids = [...this.active_session_ids]; // at this point, these should be the same.
   }
 
-  displaySession(session_id) {
-    document.getElementById("prediction_display_area").innerText = session_id;
+  displaySelectedSession(session_id) {
+    let that = this;
+    this.selected_session_id = session_id;
+    let display_area = document.getElementById("prediction_display_area");
+    display_area.innerText = session_id;
+    let predictions_handler = function(result) {
+      console.log(`Got back predictions: ${result}`);
+      let predictions_raw = JSON.parse(result);
+      let prediction_list = predictions_raw[that.selected_session_id]
+      for (let prediction_name in prediction_list) {
+        let prediction_value = prediction_list[prediction_name];
+        let next_prediction = document.createElement("div");
+        next_prediction.id=prediction_name;
+        let title = document.createElement("h3");
+        title.innerText = prediction_name;
+        next_prediction.appendChild(title);
+        let value = document.createElement("div");
+        value.innerText = prediction_value;
+        next_prediction.appendChild(value);
+        next_prediction.appendChild(document.createElement("br"));
+        display_area.appendChild(next_prediction);
+      }
+    };
+    Server.get_predictions_by_sessID(predictions_handler, session_id, that.active_game);
+  }
+
+  clearSelected() {
+    // here we'll just clear the stuff displayed in the prediction area.
+    let prediction_display_area = document.getElementById("prediction_display_area");
+    prediction_display_area.innerText = '';
+    while (prediction_display_area.firstChild > 0) {
+      prediction_display_area.removeChild(prediction_display_area.firstChild);
+    }
+    this.selected_session_id = -1;
   }
 }
