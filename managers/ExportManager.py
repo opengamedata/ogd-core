67b8d97c6cf0fa58b0cadc63adc34bdbaf374e82
Luke Swanson <swansonl@cs.wisc.edu>
2019-10-31 16:35:17 -0500

    Zip exported files, to reduce storage space.

--- managers/ExportManager.py
+++ managers/ExportManager.py
@@ -11,6 +11,7 @@ import os
 import subprocess
 import traceback
 import typing
+import zipfile
 from datetime import datetime
 ## import local files
 import utils
@@ -104,25 +105,44 @@ class ExportManager:
             existing_csvs = utils.loadJSONFile("file_list.json", self._settings['DATA_DIR'])
 
             os.makedirs(name=data_directory, exist_ok=True)
-            raw_csv_full_path:  str = None
-            proc_csv_full_path: str = None
+            raw_csv_path:  str = None
+            proc_csv_path: str = None
+            raw_zip_path:  str = None
+            proc_zip_path: str = None
             num_sess:           int = len(game_table.session_ids)
             if game_schema is not None:
-                raw_csv_full_path  = f"{data_directory}/{dataset_id}_{short_hash}_raw.csv"
-                proc_csv_full_path = f"{data_directory}/{dataset_id}_{short_hash}_proc.csv"
+                raw_csv_path  = f"{data_directory}/{dataset_id}_{short_hash}_raw.csv"
+                proc_csv_path = f"{data_directory}/{dataset_id}_{short_hash}_proc.csv"
+                raw_zip_path  = f"{data_directory}/{dataset_id}_{short_hash}_raw.zip"
+                proc_zip_path = f"{data_directory}/{dataset_id}_{short_hash}_proc.zip"
                 if self._game_id in existing_csvs and dataset_id in existing_csvs[self._game_id]:
                     src_raw = existing_csvs[self._game_id][dataset_id]['raw']
                     src_proc = existing_csvs[self._game_id][dataset_id]['proc']
-                    os.rename(src_raw, raw_csv_full_path)
-                    os.rename(src_proc, proc_csv_full_path)
-                self._extractToCSVs(raw_csv_path=raw_csv_full_path, proc_csv_path=proc_csv_full_path,\
+                    os.rename(src_raw, raw_zip_path)
+                    os.rename(src_proc, proc_zip_path)
+                self._extractToCSVs(raw_csv_path=raw_csv_path, proc_csv_path=proc_csv_path,\
                                     db_settings=db_settings,\
                                     game_schema=game_schema, game_table=game_table, game_extractor=game_extractor)
-            sql_dump_full_path = f"{data_directory}/{dataset_id}_{short_hash}.sql.gz"
-            self._dumpToSQL(sql_dump_path=sql_dump_full_path, game_table=game_table, db_settings=db_settings)
+                raw_zip_file = zipfile.ZipFile(raw_zip_path, "w")
+                proc_zip_file = zipfile.ZipFile(proc_zip_path, "w")
+                raw_zip_file.write(raw_csv_path, f"{dataset_id}_{short_hash}_raw.csv")
+                proc_zip_file.write(proc_csv_path, f"{dataset_id}_{short_hash}_proc.csv")
+                raw_zip_file.close()
+                proc_zip_file.close()
+                os.remove(raw_csv_path)
+                os.remove(proc_csv_path)
+            sql_dump_path = f"{data_directory}/{dataset_id}_{short_hash}.sql"
+            sql_zip_path = f"{data_directory}/{dataset_id}_{short_hash}.zip"
+            if self._game_id in existing_csvs and dataset_id in existing_csvs[self._game_id]:
+                src_sql = existing_csvs[self._game_id][dataset_id]['sql']
+                os.rename(src_sql, sql_zip_path)
+            self._dumpToSQL(sql_dump_path=sql_dump_path, game_table=game_table, db_settings=db_settings)
+            sql_zip_file = zipfile.ZipFile(f"{data_directory}/{dataset_id}_{short_hash}_sql.zip", "w")
+            sql_zip_file.write(sql_dump_path, f"{dataset_id}_{short_hash}_sql.csv")
+            os.remove(sql_dump_path)
             # Finally, update the list of csv files.
-            self._updateFileExportList(dataset_id, raw_csv_full_path, proc_csv_full_path,
-                                    sql_dump_full_path, request, num_sess)
+            self._updateFileExportList(dataset_id, raw_csv_path, proc_csv_path,
+                                    sql_dump_path, request, num_sess)
 
             ret_val = True
         except Exception as err:
@@ -215,7 +235,7 @@ class ExportManager:
             command = f"mysqldump --host={db_settings['DB_HOST']} \
 --where=\"session_id BETWEEN '{game_table.session_ids[0]}' AND '{game_table.session_ids[-1]}'\" \
 --user={db_settings['DB_USER']} --password={db_settings['DB_PW']} {db_settings['DB_NAME_DATA']} {db_settings['table']} \
-| gzip > {sql_dump_path}"
+ > {sql_dump_path}"
             sql_dump_file = open(sql_dump_path, "w")
             utils.Logger.toStdOut(f"running sql dump command: {command}", logging.INFO)
             os.system(command)
