8108188c8518c2b81ed9a08a2e03177a056bd89b
John <johnmccloskey2010@hotmail.com>
2019-11-05 14:38:17 -0600

    Lakeland: Put multiple playthroughs in multiple rows with play_num feat

--- managers/ProcManager.py
+++ managers/ProcManager.py
@@ -42,7 +42,10 @@ class ProcManager:
         session_id = row_with_complex_parsed[self._game_table.session_id_index]
         # ensure we have an extractor for the given session:
         if not session_id in self._session_extractors.keys():
-            self._session_extractors[session_id] = self._ExtractorClass(session_id, self._game_table, self._game_schema)
+            if row_with_complex_parsed[2] == 'LAKELAND':
+                self._session_extractors[session_id] = self._ExtractorClass(session_id, self._game_table, self._game_schema, self._proc_file)
+            else:
+                self._session_extractors[session_id] = self._ExtractorClass(session_id, self._game_table, self._game_schema)
         self._session_extractors[session_id].extractFromRow(row_with_complex_parsed, self._game_table)
 
     ##  Function to empty the list of lines stored by the ProcManager.
