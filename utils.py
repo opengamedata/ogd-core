6f7829f2ea7836690185f742626794dc9a80e83a
John McCloskey <john.gaoxing@gmail.com>
2020-04-30 09:37:53 -0500

    SQL Dump new table without remote_addr

--- utils.py
+++ utils.py
@@ -184,13 +184,7 @@ class SQL:
     #  @return              A collection of all rows from the selection, if fetch_results is true,
     #                           otherwise None.
     @staticmethod
-    def SELECT(cursor, db_name: str, table: str,
-               columns: typing.List[str] = None, filter: str = None, limit: int = -1,
-               sort_columns: typing.List[str] = None, sort_direction = "ASC", grouping: str = None,
-               distinct: bool = False, fetch_results: bool = True) -> typing.List[typing.Tuple]:
-        query = SQL._prepareSelect(db_name=db_name, table=table, columns=columns, filter=filter, limit=limit,
-                                   sort_columns=sort_columns, sort_direction=sort_direction,
-                                   grouping=grouping, distinct=distinct)
+    def SELECT(cursor, query: str, fetch_results: bool = True) -> typing.List[typing.Tuple]:
         Logger.toStdOut("Running query: " + query, logging.INFO)
         # print(f"running query: {query}")
         start = datetime.datetime.now()
