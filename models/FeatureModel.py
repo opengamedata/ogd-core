f947c7d055ef979d78ce1d2c3deb77fc6193ca39
LswaN58 <lwswanson2@wisc.edu>
2020-08-19 09:12:27 -0500

    Initial version of Model base class for our dashboard models, plus some simple subclasses.

---
+++ models/FeatureModel.py
@@ -0,0 +1,28 @@
+## import standard libraries
+import abc
+import typing
+import logging
+## import local libraries
+from Model import *
+
+## @class FeatureModel
+#  Abstract base class for models that use per-session feature data as input rows.
+#  For these models, we wrap the functionality in a private _eval function, which is called once per row.
+class FeatureModel(Model):
+    def __init__(self, levels: typing.List[int] = []):
+        super().__init__(levels=levels, input_type=ModelInputType.FEATURE)
+    ## Very simple implementation of the Eval function.
+    #  We assume each row is a session, so call private _eval function once per row.
+    #
+    #  @param rows : A list of rows, where each row contains all columns expected by the specific model implementation.
+    #  @return     : A list of results for the data (how many results depends on the model)
+    def Eval(self, rows: typing.List) -> typing.List:
+        return [self._eval(row) for row in rows]
+
+    ## Abstract declaration of a function to perform calculation of a model results from a row.
+    #
+    #  @param rows : A row of data for a session, which should be a mapping of column names to 
+    #  @return     : A result for the given row of data
+    @abc.abstractmethod
+    def _eval(self, row: typing.Dict):
+        pass
---
+++ models/SingleFeatureModel.py
@@ -0,0 +1,20 @@
+## import standard libraries
+import logging
+import typing
+## import local libraries
+from FeatureModel import FeatureModel
+
+## @class SingleFeatureModel
+#  Implementation for models which simply mirror a single feature to the dashboard.
+#  Dead simple "model," but convenient way to integrate individual features into the dashboard.
+class SingleFeatureModel(FeatureModel):
+    def __init__(self, column_name: str, levels: typing.List[int] = []):
+        super().__init__(levels)
+        self._col_name = column_name
+
+    ## Abstract declaration of a function to perform calculation of a model results from a row.
+    #
+    #  @param rows : A row of data for a session, which should be a mapping of column names to 
+    #  @return     : A result for the given row of data
+    def _eval(self, row: typing.Dict):
+        return row[self._col_name]
