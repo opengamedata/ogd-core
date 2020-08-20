f02fc62428ef3f608134a53ea5d7b3750b2ff024
LswaN58 <lwswanson2@wisc.edu>
2020-08-20 09:01:45 -0500

    Fix include path, and add Logistic as a model type.

--- realtime/ModelManager.py
+++ realtime/ModelManager.py
@@ -5,6 +5,7 @@ import typing
 import logging
 ## import local files
 from models.SingleFeatureModel import SingleFeatureModel
+from models.LogisticModel import LogisticModel
 import utils
 
 ## @class ModelManager
@@ -20,6 +21,8 @@ class ModelManager():
         model_info = self._models[model_name]
         if model_info["type"] == "SingleFeature":
             return SingleFeatureModel(**model_info["params"])
+        elif model_info["type"] == "Logistic":
+            return LogisticModel(**model_info["params"])
     
     def ListModels(self, level: int = None):
         if level is None:
