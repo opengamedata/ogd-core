40c0ef47450a5c7249f0342f22eb1f886ee29d3e
LswaN58 <lwswanson2@wisc.edu>
2020-08-21 08:25:51 -0500

    Move the validLevel function from Model to ModelManager.

--- realtime/ModelManager.py
+++ realtime/ModelManager.py
@@ -12,6 +12,7 @@ import utils
 class ModelManager():
     def __init__(self, game_name):
         self._models = utils.loadJSONFile(filename=f"{game_name}_models.json", path="./models/")
+        print(f"In ModelManager, got the following list: {self._models}")
         # in the future, extend this with other ways of loading models.
     
     def AddModelInfo(self, model_info):
@@ -23,9 +24,19 @@ class ModelManager():
             return SingleFeatureModel(**model_info["params"])
         elif model_info["type"] == "Logistic":
             return LogisticModel(**model_info["params"])
+
+    def _validLevel(self, model_name: str, level: int):
+        levels = self._models[model_name]["levels"]
+        if levels == None or levels == []:
+            return True
+        elif level in levels:
+            return True
+        else:
+            return False
     
     def ListModels(self, level: int = None):
         if level is None:
             return list(self._models.keys())
         else:
-            return [key for key in self._models.keys() if level in self._models[key]['levels']]
+            print(f"Listing models for level {level}.")
+            return [key for key in self._models.keys() if self._validLevel(model_name=key, level=level)]
