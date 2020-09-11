e9f3f5a1a1b434f1b2a3d8d57ac91583f6ae6f2e
John McCloskey <john.gaoxing@gmail.com>
2020-09-11 14:22:44 -0500

    individual tests for each session

--- tests/test_lakeland_models.py
+++ tests/test_lakeland_models.py
@@ -1,5 +1,12 @@
 '''
-In the commandline, run "py.test -s tests\" from the project directory.
+Example commands to be run in the commandline from the project directory:
+py.test tests -s
+pytest  tests/test_lakeland_models.py -s -k PopulationModel
+pytest tests/test_lakeland_models.py::test_sequence_model[20070509155287116-PopulationModel] -vsl
+
+-v: verbose
+-s: show print statements
+-l: show local variables on error
 '''
 
 import unittest
@@ -32,24 +39,32 @@ dfs = helpers.LazyDict(get_df, kind_to_path_func=zip_path)
 v18_dfs = helpers.LazyDict(get_df, kind_to_path_func=v18_zip_paths)
 
 proc_sessions = v18_dfs['proc'].to_dict('records')
+proc_session_names = [f'{x["sessID"]}.{x["num_play"]}' for x in proc_sessions]
 v18_dumps = [groupdf.to_dict('records') for _, groupdf in v18_dfs['dump'].groupby('sess_id')]
+v18_dump_ids = [d[0]["sess_id"] for d in v18_dumps]
 
 model_mgr = ModelManager(game_name="LAKELAND")
-all_models = [model_mgr.LoadModel(model_name) for model_name in model_mgr.ListModels()]
+model_names = model_mgr.ListModels()
+all_models = [model_mgr.LoadModel(model_name) for model_name in model_names]
 # print(*list(zip(model_mgr.ListModels(), all_models)), sep='\n')
-feature_models = [model for model in all_models if model and model.GetInputType() == ModelInputType.FEATURE]
-sequence_models = [model for model in all_models if model and model.GetInputType() == ModelInputType.SEQUENCE]
+feature_models, feature_model_names = zip(*[(model,model_name) for model,model_name in zip(all_models, model_names)
+                  if model and model.GetInputType() == ModelInputType.FEATURE])
+sequence_models, sequence_model_names = zip(*[(model,model_name) for model,model_name in zip(all_models, model_names)
+                   if model and model.GetInputType() == ModelInputType.SEQUENCE])
 
-@pytest.mark.parametrize("model", feature_models)
-def test_feature_model(model):
-    print(f'\nRunning {model}:', model.Eval(proc_sessions), sep='\n')
 
-@pytest.mark.parametrize("model", sequence_models)
-def test_sequence_model(model):
-    print(f'\nRunning {model}:')
-    for session_dump in v18_dumps:
-        print(model.Eval(session_dump), end=', ')
-    print()
+@pytest.mark.parametrize("model", feature_models, ids=feature_model_names)
+@pytest.mark.parametrize("proc_sessions", proc_sessions, ids=proc_session_names)
+def test_feature_model(model,proc_sessions):
+    if type(proc_sessions) is not list:
+        proc_sessions = [proc_sessions]
+    print(model.Eval(proc_sessions), end=', ')
+
+
+@pytest.mark.parametrize("model", sequence_models, ids=sequence_model_names)
+@pytest.mark.parametrize("session_dump", v18_dumps, ids=v18_dump_ids)
+def test_sequence_model(model, session_dump):
+    print(model.Eval(session_dump), end=', ')
 
 
 if __name__ == '__main__':
