a4d4378c6401df5b2ec69e8e7157129b176a4f25
Glenn Palmer <glennpalmer55@gmail.com>
2020-09-28 22:08:42 -0500

    adding third value to diagonal detector, and cleaning up json formatting

--- models/DiagonalFarmDetectorModel.py
+++ models/DiagonalFarmDetectorModel.py
@@ -22,11 +22,13 @@ class DiagonalFarmDetectorModel(SequenceModel):
         assert events
         farmcount = 0
         diagonals = []
+        diagonals_only = 0
         for event in events:
             # gamestate
             if event["event_custom"] == 0:
                 farmcount = 0
                 diagonals = []
+                diagonals_only = 0
                 tile_string = event["event_data_complex"]["tiles"]
                 _tile = self._read_stringified_array(tile_string)
                 tile = self._array_to_mat(4, _tile)
@@ -35,11 +37,13 @@ class DiagonalFarmDetectorModel(SequenceModel):
                         farmcount += 1
                         diagonals.append(self._right_diagonal(i, tile))
                         diagonals.append(self._left_diagonal(i, tile))
+                        if self._diagonal_neighbors_present(i, tile) and not self._adjacent_neighbors_present(i, tile):
+                            diagonals_only += 1
         if diagonals:
             biggest_diagonal = max(diagonals)
         else:
             biggest_diagonal = 0
-        return [farmcount, biggest_diagonal]
+        return [farmcount, biggest_diagonal, diagonals_only]
 
     def _right_diagonal(self, index: int, tile: List[List[int]]) -> int:
         next = index + 51
@@ -62,6 +66,51 @@ class DiagonalFarmDetectorModel(SequenceModel):
             else:
                 break
         return count
+
+    def _diagonal_neighbors_present(self, index: int, tile: List[List[int]]) -> bool:
+        # check all four possibilities if it's not in the first or last row
+        if 49 < index < 2450:
+            if index % 50 != 0:
+                if tile[index + 49][3] == 9 or tile[index - 51][3] == 9:
+                    return True
+            if index % 50 != 49:
+                if tile[index + 51][3] == 9 or tile[index - 49][3] == 9:
+                    return True
+        # check both possibilities if it's in the first row
+        elif index <= 49:
+            if index != 0:
+                if tile[index + 49][3] == 9:
+                    return True
+            if index != 49:
+                if tile[index + 51][3] == 9:
+                    return True
+        # check both possibilities if it's in the last row
+        elif index >= 2450:
+            if index % 50 != 0:
+                if tile[index - 51][3] == 9:
+                    return True
+            if index % 50 != 49:
+                if tile[index - 49][3] == 9:
+                    return True
+        return False
+
+    def _adjacent_neighbors_present(self, index: int, tile: List[List[int]]) -> bool:
+        if index > 49:
+            if tile[index - 50][3] == 9:
+                return True
+        if index < 2450:
+            if tile[index + 50][3] == 9:
+                return True
+        if index % 50 != 0:
+            if tile[index - 1][3] == 9:
+                return True
+        if index % 50 != 49:
+            if tile[index + 1][3] == 9:
+                return True
+        return False
+
+
+
 
     # reformat raw variable functions
     def _read_stringified_array(self, arr: str) -> List[int]:
