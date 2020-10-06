df74ec00269c71b21564366e3a9606e1c6fe447a
LswaN58 <lwswanson2@wisc.edu>
2020-10-06 06:58:32 -0500

    Add initial class for managing the output files used during an export

--- managers/FileManager.py
+++ managers/FileManager.py
@@ -10,8 +10,8 @@ class FileManager(abc.ABC):
         self._zip_names  : typing.Dict = {"proc":None, "raw":None, "dump":None}
         self._files      : typing.Dict = {"proc":None, "raw":None, "dump":None}
         self._data_dir   : str         = data_dir
-        self._readme_path: str
         self._game_id    : str         = game_id
+        self._readme_path: str
         self._dataset_id : str
         self._short_hash : str
         try:
@@ -119,3 +119,57 @@ class FileManager(abc.ABC):
         except FileNotFoundError as err:
             utils.Logger.Log(str(err), logging.ERROR)
             traceback.print_tb(err.__traceback__)
+
+    ## Public function to update the list of exported files.
+    #  Given the paths of the exported files, and some other variables for
+    #  deriving file metadata, this simply updates the JSON file to the latest
+    #  list of files.
+    #  @param dataset_id    The id used to identify a specific data set, based on
+    #                       game id, and range of dates.
+    #  @param raw_csv_path  Path to the newly exported raw csv, including filename
+    #  @param proc_csv_path Path to the newly exported feature csv, including filename
+    #  @param request       The original request for data export
+    #  @param num_sess      The number of sessions included in the recent export.
+    def UpdateFileExportList(self, date_range: typing.Tuple, num_sess: int):
+        self._backupFileExportList()
+        try:
+            existing_csvs = utils.loadJSONFile("file_list.json", self._settings['DATA_DIR'])
+        except Exception as err:
+            msg = f"{type(err)} {str(err)}"
+            utils.Logger.toFile(msg, logging.WARNING)
+            existing_csvs = {}
+        finally:
+            existing_csv_file = open(f"{self._settings['DATA_DIR']}file_list.json", "w")
+            utils.Logger.toStdOut(f"opened existing csv file at {existing_csv_file.name}", logging.INFO)
+            if not self._game_id in existing_csvs.keys():
+                existing_csvs[self._game_id] = {}
+            # raw_stat = os.stat(raw_csv_full_path)
+            # proc_stat = os.stat(proc_csv_full_path)
+            if dataset_id in existing_csvs[self._game_id].keys():
+                proc_path = self._file_names["proc"] if self._file_names["proc"] is not None else existing_csvs[self._game_id][self._dataset_id]["proc"]
+                raw_path  = self._file_names["raw"]  if self._file_names["raw"]  is not None else existing_csvs[self._game_id][self._dataset_id]["raw"]
+                dump_path = self._file_names["dump"] if self._file_names["dump"] is not None else existing_csvs[self._game_id][self._dataset_id]["dump"]
+            existing_csvs[self._game_id][dataset_id] = \
+                {
+                    "proc":proc_path,
+                    "raw" :raw_path,
+                    "dump":dump_path,
+                    "start_date"   :date_range[0].strftime("%m/%d/%y"),
+                    "end_date"     :date_range[1].strftime("%m/%d/%y"),
+                    "date_modified":datetime.now().strftime("%m/%d/%Y"),
+                    "sessions":num_sess
+                }
+            existing_csv_file.write(json.dumps(existing_csvs, indent=4))
+
+    def _backupFileExportList(self) -> bool:
+        try:
+            existing_csvs = utils.loadJSONFile("file_list.json", self._settings['DATA_DIR']) or {}
+        except Exception as err:
+            msg = f"{type(err)} {str(err)}"
+            utils.Logger.toStdOut(f"Could not back up file_list.json. Got the following error: {msg}", logging.ERROR)
+            utils.Logger.toFile(f"Could not back up file_list.json. Got the following error: {msg}", logging.ERROR)
+            return False
+        backup_csv_file = open(f"{self._settings['DATA_DIR']}file_list.json.bak", "w")
+        backup_csv_file.write(json.dumps(existing_csvs, indent=4))
+        utils.Logger.toStdOut(f"Backed up file_list.json to {backup_csv_file.name}", logging.INFO)
+        return True
