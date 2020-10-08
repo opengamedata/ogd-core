be4e7320fd639c9ddb171872e95ca98c6ed7f8d9
Glenn Palmer <glennpalmer55@gmail.com>
2020-10-08 10:53:56 -0500

    updating RecentPurchasesModel to return timestamps of purchases

--- models/RecentPurchasesModel.py
+++ models/RecentPurchasesModel.py
@@ -1,51 +1,86 @@
 from typing import List, Optional, Dict, Any
 from models.SequenceModel import SequenceModel
+import datetime
 
 
 ## @class RecentPurchasesModel
-# Returns an 8-tuple with the number of (in this order) [homes, food, farms, fertilizer, livestock,
-# skimmers, signs, roads] purchased within the last 10 purchases
+# Returns a list of 8 lists, corresponding to the 8 items that can be purchased: (in this order) [homes, food,
+# farms, fertilizer, livestock, skimmers, signs, roads].
+# In each list is the number of seconds since that item was purchased, for each time it was purchased within the
+# 10 most recent purchases.
+# Rationale: This gives us options for displaying the data however we feel is most helpful. For example, we could
+# just indicate the length of each list to indicate the number of purchases of each type within the last ten overall
+# purchases, or we could incorporate the time stamps as well.
 # @param levels: Levels applicable for model
 class RecentPurchasesModel(SequenceModel):
     def __init__(self, levels: List[int] = []):
         '''
         @class RecentPurchasesModel
-        Returns an 8-tuple with the number of (in this order) [homes, food, farms, fertilizer, livestock,
-        skimmers, signs, roads] purchased within the last 10 purchases
+        Returns a list of 8 lists, corresponding to the 8 items that can be purchased: (in this order) [homes, food,
+        farms, fertilizer, livestock, skimmers, signs, roads].
+        In each list is the number of seconds since that item was purchased, for each time it was purchased within the
+        10 most recent purchases.
         :param levels: Levels applicable for model
         '''
         super().__init__()
 
-    def _eval(self, events: List[Dict[str, Any]], verbose: bool = False) -> List[int]:
+    def _eval(self, events: List[Dict[str, Any]], verbose: bool = False) -> List[List[int]]:
         assert events
-        homes = 0
-        food = 0
-        farms = 0
-        fertilizer = 0
-        livestock = 0
-        skimmers = 0
-        signs = 0
-        roads = 0
+        homes = []
+        food = []
+        farms = []
+        fertilizer = []
+        livestock = []
+        skimmers = []
+        signs = []
+        roads = []
         count = 0
+        now = events[-1]["client_time"]
+        if type(now) is str:
+            now = datetime.datetime.fromisoformat(now)
         for event in reversed(events):
             # gamestate
             if event["event_custom"] == 7 and event["event_data_complex"]["success"]:
                 if event["event_data_complex"]["buy"] == 1:
-                    homes += 1
+                    event_time = event["client_time"]
+                    if type(event_time) is str:
+                        event_time = datetime.datetime.fromisoformat(event_time)
+                    homes.append((now - event_time).seconds)
                 elif event ["event_data_complex"]["buy"] == 2:
-                    food += 1
+                    event_time = event["client_time"]
+                    if type(event_time) is str:
+                        event_time = datetime.datetime.fromisoformat(event_time)
+                    food.append((now - event_time).seconds)
                 elif event["event_data_complex"]["buy"] == 3:
-                    farms += 1
+                    event_time = event["client_time"]
+                    if type(event_time) is str:
+                        event_time = datetime.datetime.fromisoformat(event_time)
+                    farms.append((now - event_time).seconds)
                 elif event["event_data_complex"]["buy"] == 4:
-                    fertilizer += 1
+                    event_time = event["client_time"]
+                    if type(event_time) is str:
+                        event_time = datetime.datetime.fromisoformat(event_time)
+                    fertilizer.append((now - event_time).seconds)
                 elif event["event_data_complex"]["buy"] == 5:
-                    livestock += 1
+                    event_time = event["client_time"]
+                    if type(event_time) is str:
+                        event_time = datetime.datetime.fromisoformat(event_time)
+                    livestock.append((now - event_time).seconds)
                 elif event["event_data_complex"]["buy"] == 6:
-                    skimmers += 1
+                    event_time = event["client_time"]
+                    if type(event_time) is str:
+                        event_time = datetime.datetime.fromisoformat(event_time)
+                    skimmers.append((now - event_time).seconds)
                 elif event["event_data_complex"]["buy"] == 7:
-                    signs += 1
+                    event_time = event["client_time"]
+                    if type(event_time) is str:
+                        event_time = datetime.datetime.fromisoformat(event_time)
+                    signs.append((now - event_time).seconds)
                 elif event["event_data_complex"]["buy"] == 8:
-                    roads += 1
+                    event_time = event["client_time"]
+                    if type(event_time) is str:
+                        event_time = datetime.datetime.fromisoformat(event_time)
+                    roads.append((now - event_time).seconds)
                 # Check if ten purchases have been tallied. If so, break from the loop.
                 count += 1
                 if count >= 10:
--- models/TimeSinceLastSaleModel.py
+++ models/TimeSinceLastSaleModel.py
@@ -1,12 +1,11 @@
-from typing import List, Optional, Dict, Any, Union, Tuple, Callable
-from collections import defaultdict
+from typing import List, Optional, Dict, Any
 from models.SequenceModel import SequenceModel
 import datetime
-import operator
-from functools import partial
 
 ## @class TimeSinceEventTypesModel
 # Returns the number of seconds since the last purchase a player made
+# Rationale: This seems like the simplest and easiest to interpret measure of whether a player
+# is stagnating by not selling anything.
 # @param levels: Levels applicable for model
 
 class TimeSinceLastSaleModel(SequenceModel):
@@ -20,52 +19,17 @@ class TimeSinceLastSaleModel(SequenceModel):
 
     def _eval(self, events: List[Dict[str, Any]], verbose: bool = False) -> Optional[int]:
         assert events
-        now = datetime.datetime.now()
+        now = events[-1]["client_time"]
         for event in reversed(events):
-            # tally money for selling stuff since most recent gamestate event
             if event["event_custom"] == 37:
-                event_time = event["server_time"]
+                event_time = event["client_time"]
                 if type(event_time) is str:
                     event_time = datetime.datetime.fromisoformat(event_time)
+                if type(now) is str:
+                    now = datetime.datetime.fromisoformat(now)
                 return (now - event_time).seconds
         return None
 
 
     def __repr__(self):
         return f"TimeSinceLastSaleModel(levels={self._levels}, input_type={self._input_type})"
-
-
-_comparison_str_to_func = {
-    "lt": operator.lt,
-    "<": operator.lt,
-    "le": operator.le,
-    "<=": operator.le,
-    "eq": operator.eq,
-    "==": operator.eq,
-    "ne": operator.ne,
-    "!=": operator.ne,
-    "ge": operator.ge,
-    ">=": operator.ge,
-    "gt": operator.gt,
-    ">": operator.gt,
-    "in": lambda a,b: a in b,
-    "notin": lambda a, b: a not in b,
-}
-
-
-def _base_filter(event, funcs_all):
-    for keys, comparison_str, value in funcs_all:
-        event_value = event
-        for key in keys:
-            # print(event_value, key)
-            # print(type(event_value))
-            event_value = event_value[key]
-        comparison_func = _comparison_str_to_func[comparison_str]
-        if not comparison_func(event_value, value):
-            return False
-    return True
-
-def _chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
