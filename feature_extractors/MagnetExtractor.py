29decd19af5d4680cf3ec8cbf3d599a6e9ae4688
LswaN58 <lwswanson2@wisc.edu>
2021-02-06 19:50:09 -0600

    Add a comment to magnet extractor, so we can test CI script trigger.

--- feature_extractors/MagnetExtractor.py
+++ feature_extractors/MagnetExtractor.py
@@ -9,6 +9,7 @@ from feature_extractors.Extractor import Extractor
 from GameTable import GameTable
 from schemas.Schema import Schema
 
+# add a comment to magnet extractor, so we can test CI script trigger.
 
 ## @class MagnetExtractor
 #  Extractor subclass for extracting features from Magnet game data.
