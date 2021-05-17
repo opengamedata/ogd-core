0c301edb6b4b27e0359b1f2d0bbdde205e9d9895
LswaN58 <lwswanson2@wisc.edu>
2021-05-17 15:09:40 -0500

    Fix some file path nonsense in reindexer.

--- reindexer.py
+++ reindexer.py
@@ -50,23 +50,24 @@ def index_zip(root, name, indexed_files):
     end_date = pieces[-3]
     dataset_id  = f"{game_id}_{start_date}_to_{end_date}"
     kind = pieces[-1]
-    # after getting info from filename on the game id, start/end dates, etc. we can peek at the file and count the rows, *if* it's a session file.
-    session_ct = None
-    if kind == "session-features":
-        with zipfile.ZipFile(f"{root}{name}", 'r') as zip:
-            data = pd.read_csv(zip.open(f"{root}{top[0]}.tsv"))
-            session_ct = len(data.index)
     if not game_id in indexed_files.keys():
         indexed_files[game_id] = {}
     # if we already indexed something with this dataset id, then only update if this one is newer.
     # else, just stick this new meta in the index.
+    file_path = f"{root}/{name}" # os.path.join(root, name) is technically proper, but gives bad results in Windows.
     if not dataset_id in indexed_files[game_id].keys():
-        utils.Logger.toPrint(f"Indexing {os.path.join(root, name)}", logging.INFO)
+        # after getting info from filename on the game id, start/end dates, etc. we can peek at the file and count the rows, *if* it's a session file.
+        session_ct = None
+        if kind == "session-features":
+            with zipfile.ZipFile(file_path, 'r') as zip:
+                data = pd.read_csv(zip.open(f"{dataset_id}/{top[0]}.csv"), sep='\t')
+                session_ct = len(data.index)
+        utils.Logger.toStdOut(f"Indexing {file_path}", logging.INFO)
         indexed_files[game_id][dataset_id] = \
             {
-                "sessions_f"   :f"{root}{name}" if kind == 'session-features' else None,
-                "raw_f"        :f"{root}{name}" if kind == 'raw' else None,
-                "events_f"     :f"{root}{name}" if kind == 'events' else None,
+                "sessions_f"   :file_path if kind == 'session-features' else None,
+                "raw_f"        :file_path if kind == 'raw' else None,
+                "events_f"     :file_path if kind == 'events' else None,
                 "start_date"   :start_date,
                 "end_date"     :end_date,
                 "date_modified":None,
@@ -74,14 +75,14 @@ def index_zip(root, name, indexed_files):
             }
     else:
         if indexed_files[game_id][dataset_id]["sessions_f"] == None and kind == 'session-features':
-            utils.Logger.toPrint(f"Updating index with {os.path.join(root, name)}", logging.INFO)
-            indexed_files[game_id][dataset_id]["sessions_f"] = f"{root}{name}"
+            utils.Logger.toStdOut(f"Updating index with {file_path}", logging.INFO)
+            indexed_files[game_id][dataset_id]["sessions_f"] = file_path
         if indexed_files[game_id][dataset_id]["raw_f"] == None and kind == 'raw':
-            utils.Logger.toPrint(f"Updating index with {os.path.join(root, name)}", logging.INFO)
-            indexed_files[game_id][dataset_id]["raw_f"] = f"{root}{name}"
+            utils.Logger.toStdOut(f"Updating index with {file_path}", logging.INFO)
+            indexed_files[game_id][dataset_id]["raw_f"] = file_path
         if indexed_files[game_id][dataset_id]["events_f"] == None and kind == 'events':
-            utils.Logger.toPrint(f"Updating index with {os.path.join(root, name)}", logging.INFO)
-            indexed_files[game_id][dataset_id]["events_f"] = f"{root}{name}"
+            utils.Logger.toStdOut(f"Updating index with {file_path}", logging.INFO)
+            indexed_files[game_id][dataset_id]["events_f"] = file_path
     return indexed_files
 
 def generate_index(walk_data):
@@ -92,15 +93,15 @@ def generate_index(walk_data):
             if not 'BACKUP' in root:
                 ext = name.split('.')[-1]
                 if (ext == 'meta'):
-                    utils.Logger.toPrint(f"Indexing {os.path.join(root, name)}", logging.INFO)
+                    utils.Logger.toStdOut(f"Indexing {os.path.join(root, name)}", logging.INFO)
                     indexed_files = index_meta(root, name, indexed_files)
                 elif (ext == 'zip'):
-                    utils.Logger.toPrint(f"Reserving {os.path.join(root, name)}", logging.DEBUG)
+                    utils.Logger.toStdOut(f"Reserving {os.path.join(root, name)}", logging.DEBUG)
                     zips.append((root, name))
                 else:
-                    utils.Logger.toPrint(f"Doing nothing with {os.path.join(root, name)}", logging.DEBUG)
+                    utils.Logger.toStdOut(f"Doing nothing with {os.path.join(root, name)}", logging.DEBUG)
             else:
-                utils.Logger.toPrint(f"Doing nothing with {os.path.join(root, name)}", logging.DEBUG)
+                utils.Logger.toStdOut(f"Doing nothing with {os.path.join(root, name)}", logging.DEBUG)
     for root,name in zips:
         indexed_files = index_zip(root, name, indexed_files)
     return indexed_files
