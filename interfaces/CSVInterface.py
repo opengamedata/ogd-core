4aac7f1c13a6d8a6eb429e872107cb5b59c2fb2b
LswaN58 <lwswanson2@wisc.edu>
2021-05-18 13:42:59 -0500

    Move CSVDataManager to an interface.

---
+++ interfaces/CSVInterface.py
@@ -0,0 +1,16 @@
+import abc
+import typing
+import pandas as pd
+## import local files
+from interfaces.DataInterface import DataInterface
+from GameTable import GameTable
+from schemas.Schema import Schema
+
+class CSVInterface(DataInterface):
+    # TODO: Take a path, rather than an existing dataframe.
+    def __init__(self, game_id: str, data_frame: pd.DataFrame):
+        DataManager.__init__(self, game_id=game_id)
+        self._data = data_frame
+
+    def RetrieveSliceData(self, id_list) -> typing.List[typing.Tuple]:
+        return list(self._data.loc[self._data['session_id'].isin(id_list)].itertuples(index=False, name=None))
--- managers/DataManager.py
+++ managers/DataManager.py
@@ -17,12 +17,3 @@ class DataManager(abc.ABC):
     @abc.abstractmethod
     def RetrieveSliceData(self, id_list) -> typing.List[typing.Tuple]:
         pass
-
-class CSVDataManager(DataManager):
-    # TODO: Take a path, rather than an existing dataframe.
-    def __init__(self, game_id: str, data_frame: pd.DataFrame):
-        DataManager.__init__(self, game_id=game_id)
-        self._data = data_frame
-
-    def RetrieveSliceData(self, id_list) -> typing.List[typing.Tuple]:
-        return list(self._data.loc[self._data['session_id'].isin(id_list)].itertuples(index=False, name=None))
