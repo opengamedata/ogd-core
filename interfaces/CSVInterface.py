f65c789f383a86b8f5f4cd0ee614b21f447c4fbc
LswaN58 <lwswanson2@wisc.edu>
2021-05-18 14:44:24 -0500

    WIP adding the functions to CSVInterface.

--- interfaces/CSVInterface.py
+++ interfaces/CSVInterface.py
@@ -9,8 +9,25 @@ from schemas.Schema import Schema
 class CSVInterface(DataInterface):
     # TODO: Take a path, rather than an existing dataframe.
     def __init__(self, game_id: str, data_frame: pd.DataFrame):
-        super().__init__(self, game_id=game_id)
+        super().__init__(game_id=game_id)
         self._data = data_frame
 
-    def RetrieveSliceData(self, id_list) -> typing.List[typing.Tuple]:
+    @abc.abstractmethod
+    def Open(self) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def Close(self) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def _retrieveFromIDs(self, id_list: typing.List[int]) -> typing.List:
         return list(self._data.loc[self._data['session_id'].isin(id_list)].itertuples(index=False, name=None))
+
+    @abc.abstractmethod
+    def _IDsFromDates(self, min, max):
+        pass
+
+    @abc.abstractmethod
+    def _datesFromIDs(self, id_list:typing.List[int]):
+        pass
