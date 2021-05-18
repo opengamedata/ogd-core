2d0551222beeb5e73d9b0e14d175390eb51848ad
LswaN58 <lwswanson2@wisc.edu>
2021-05-18 15:04:35 -0500

    Various intermediate work on the interface classes.

--- interfaces/CSVInterface.py
+++ interfaces/CSVInterface.py
@@ -1,16 +1,19 @@
 import abc
-import typing
 import pandas as pd
+from typing import Any, Dict, IO, List, Tuple, Union
 ## import local files
 from interfaces.DataInterface import DataInterface
 from GameTable import GameTable
 from schemas.Schema import Schema
 
 class CSVInterface(DataInterface):
-    # TODO: Take a path, rather than an existing dataframe.
-    def __init__(self, game_id: str, data_frame: pd.DataFrame):
+    def __init__(self, game_id:str, filepath_or_buffer:Union[str, IO[bytes]], delim:str = ','):
+        # set up data from params
         super().__init__(game_id=game_id)
-        self._data = data_frame
+        self._file      : Union[str, IO[bytes]] = filepath_or_buffer
+        self._delimiter : str = delim
+        # set up data from file
+        self._data      : Union[pd.DataFrame, None] = None
 
     def Open(self, force_reopen:bool = False) -> bool:
         if force_reopen or not self.IsOpen():
@@ -18,13 +21,16 @@ class CSVInterface(DataInterface):
             self._is_open = True
         return True
 
-    @abc.abstractmethod
     def Close(self) -> bool:
-        pass
+        self._is_open = False
+        return True
 
     @abc.abstractmethod
-    def _retrieveFromIDs(self, id_list: typing.List[int]) -> typing.List:
-        return list(self._data.loc[self._data['session_id'].isin(id_list)].itertuples(index=False, name=None))
+    def _retrieveFromIDs(self, id_list: List[int]) -> List:
+        if self.IsOpen():
+            return list(self._data.loc[self._data['session_id'].isin(id_list)].itertuples(index=False, name=None))
+        else:
+            return []
 
     @abc.abstractmethod
     def _IDsFromDates(self, min, max):
