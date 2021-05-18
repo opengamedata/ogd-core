82bf01fd08f46e42e5fc59fa3f64a6e6c6f47e68
LswaN58 <lwswanson2@wisc.edu>
2021-05-18 13:41:04 -0500

    Pulled the SQLDataManager out, it'll become an interface.

--- managers/DataManager.py
+++ managers/DataManager.py
@@ -18,33 +18,6 @@ class DataManager(abc.ABC):
     def RetrieveSliceData(self, id_list) -> typing.List[typing.Tuple]:
         pass
 
-class SQLDataManager(DataManager):
-    def __init__(self, game_id: str, game_schema: Schema, settings):
-        DataManager.__init__(self, game_id=game_id)
-        self._game_schema = game_schema
-        self._settings = settings
-        self._tunnel, self._db  = SQL.prepareDB(db_settings=settings["db_config"], ssh_settings=settings["ssh_config"])
-        self._db_cursor = self._db.cursor()
-
-    def RetrieveSliceData(self, id_list) -> typing.List[typing.Tuple]:
-        # grab data for the given session range. Sort by event time, so
-        if self._game_id == 'LAKELAND' or self._game_id == 'JOWILDER':
-            ver_filter = f" AND app_version in ({','.join([str(x) for x in self._game_schema.schema()['config']['SUPPORTED_VERS']])}) "
-        else:
-            ver_filter = ''
-        id_string = ','.join([f"'{x}'" for x in id_list])
-        # filt = f"app_id='{self._game_id}' AND (session_id  BETWEEN '{next_slice[0]}' AND '{next_slice[-1]}'){ver_filter}"
-        filt = f"app_id='{self._game_id}' AND session_id  IN ({id_string}){ver_filter}"
-        query = SQL._prepareSelect(db_name=self._settings["db_config"]["DB_NAME_DATA"],
-                                         table=self._settings["db_config"]["TABLE"], columns=None, filter=filt, limit=-1,
-                                         sort_columns=["session_id", "session_n"], sort_direction="ASC",
-                                         grouping=None, distinct=False)
-        # self._select_queries.append(select_query) # this doesn't appear to be used???
-        return SQL.SELECTfromQuery(cursor=self._db_cursor, query=query, fetch_results=True)
-
-    def __del__(self):
-        SQL.disconnectMySQLViaSSH(tunnel=self._tunnel, db=self._db)
-
 class CSVDataManager(DataManager):
     # TODO: Take a path, rather than an existing dataframe.
     def __init__(self, game_id: str, data_frame: pd.DataFrame):
