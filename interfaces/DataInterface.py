73ca1563847899726a7a56bc2d53eb79e8652c8f
LswaN58 <lwswanson2@wisc.edu>
2021-05-18 12:32:30 -0500

    DataInterface should probably know what game_id is, and add some more functions to interface.

--- interfaces/DataInterface.py
+++ interfaces/DataInterface.py
@@ -5,23 +5,48 @@ import typing
 import utils
 
 class DataInterface(abc.ABC):
-    def __init__(self):
+    def __init__(self, game_name):
+        self._game_id : str  = game_name
         self._is_open : bool = False
 
     def RetrieveFromIDs(self, ids: typing.List[int]) -> typing.List:
         if not self._is_open:
             utils.Logger.Log("Can't retrieve data, the source interface is not open!")
+            return []
         else:
             return self._retrieveFromIDs(ids)
 
+    def IDsFromDates(self, min, max):
+        if not self._is_open:
+            utils.Logger.Log("Can't retrieve IDs, the source interface is not open!")
+        else:
+            return self._IDsFromDates(min=min, max=max)
+
+    def DatesFromIDs(self, ids:typing.List[int]):
+        if not self._is_open:
+            utils.Logger.Log("Can't retrieve dates, the source interface is not open!")
+        else:
+            return self._datesFromIDs(ids=ids)
+    
+    def IsOpen(self) -> bool:
+        return True if self._is_open else False
+
     @abc.abstractmethod
-    def Open(self) -> None:
+    def Open(self) -> bool:
         pass
 
     @abc.abstractmethod
-    def Close(self) -> None:
+    def Close(self) -> bool:
         pass
 
     @abc.abstractmethod
     def _retrieveFromIDs(self, ids: typing.List[int]) -> typing.List:
+        pass
+
+    @abc.abstractmethod
+    def _IDsFromDates(self, min, max):
+        pass
+
+    @abc.abstractmethod
+    def _datesFromIDs(self, ids:typing.List[int]):
         pass
