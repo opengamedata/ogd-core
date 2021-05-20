ae99bd78e7ed4af7a1c188f8c237151806344d76
LswaN58 <lwswanson2@wisc.edu>
2021-05-20 14:07:08 -0500

    Various little fixes for CSVInterface.

--- interfaces/CSVInterface.py
+++ interfaces/CSVInterface.py
@@ -13,7 +13,7 @@ class CSVInterface(DataInterface):
         self._file      : Union[str, IO[bytes]] = filepath_or_buffer
         self._delimiter : str = delim
         # set up data from file
-        self._data      : Union[pd.DataFrame, None] = None
+        self._data      : pd.DataFrame = pd.DataFrame()
 
     def Open(self, force_reopen:bool = False) -> bool:
         if force_reopen or not self.IsOpen():
@@ -31,13 +31,13 @@ class CSVInterface(DataInterface):
         else:
             return []
 
-    def _IDsFromDates(self, min, max) -> List[int]:
-        if self._data != None:
+    def _IDsFromDates(self, min:datetime, max:datetime) -> List[int]:
+        if not self._data.empty:
             return list(self._data.loc[self._data['server_time'] > min and self._data['server_time'] < max, ['session_id']])
         else:
             return []
 
-    def _datesFromIDs(self, id_list:List[int]) -> Tuple[datetime, datetime]:
-        min_date = min(self._data.loc[self._data['session_id'] in id_list, ['server_time']])
-        max_date = max(self._data.loc[self._data['session_id'] in id_list, ['server_time']])
-        return (min, max)
+    def _datesFromIDs(self, id_list:List[int]) -> Dict[str, datetime]:
+        min_date = self._data.loc[self._data['session_id'] in id_list, ['server_time']].min
+        max_date = self._data.loc[self._data['session_id'] in id_list, ['server_time']].max
+        return {'min':pd.to_datetime(min_date), 'max':pd.to_datetime(max_date)}
