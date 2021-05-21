a1036104310a9f30cb04d637e3379ef8ee49afaf
LswaN58 <lwswanson2@wisc.edu>
2021-05-21 09:00:24 -0500

    Add a simple testing file for utils, specifically to test loading of JSON files.

---
+++ tests/t_utils.py
@@ -0,0 +1,28 @@
+import datetime
+import json
+import logging
+import os
+import traceback
+import typing
+import unittest
+from unittest import TestCase
+# local import(s)
+from config import settings
+import utils
+
+class t_utils(TestCase):
+    def RunAll(self):
+        self.test_loadJSONFile()
+        print("Ran all t_utils tests.")
+
+    def test_loadJSONFile(self):
+        json_content = utils.loadJSONFile("t_utils.json", "./tests")
+        self.assertEqual(json_content['first'], "the worst")
+        self.assertEqual(json_content['second'], ["the best", "born, second place"])
+        self.assertTrue("fourth" in json_content.keys())
+        self.assertEqual(json_content['fourth']['a'], "why's it out of order?")
+        self.assertEqual(json_content['fourth']['b'], 4)
+        self.assertEqual(json_content['fourth']['c'], False)
+
+if __name__ == '__main__':
+    unittest.main()
