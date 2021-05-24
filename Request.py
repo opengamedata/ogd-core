34619889063157335b95e5175e9af9d552968f66
LswaN58 <lwswanson2@wisc.edu>
2021-05-24 11:10:47 -0500

    Minor fixes to last work on Request, and remove a lot of dead code.
    Most of the removed code was from old architecture where we had
    different requests for different combinations of range type and source,
    but not all possible combinations.
    
    With refactoring, we now give a Request an Interface, an ExporterRange,
    and ExporterFiles.
    The Interface and ExporterRange will let us get session IDs from any
    source and based on date or ID ranges, in theory.

--- Request.py
+++ Request.py
@@ -3,9 +3,10 @@ import abc
 import enum
 import typing
 from datetime import datetime
-from typing import List, Union
+from typing import Dict, List, Union
 # include local files
 import utils
+from GameTable import GameTable
 from interfaces.DataInterface import DataInterface
 from interfaces.MySQLInterface import SQL
 from schemas.Schema import Schema
@@ -57,82 +58,19 @@ class Request(abc.ABC):
     #  @param start_date   The starting date for our range of data to process.
     #  @param end_date     The ending date for our range of data to process.
     def __init__(self, interface:DataInterface, range:ExporterRange, exporter_files:ExporterFiles = ExporterFiles()):
-        self._interface  = interface
-        self._range = range
-        self._exporter_files = exporter_files
+        self._interface : DataInterface = interface
+        self._range     : ExporterRange = range
+        self._files     : ExporterFiles = exporter_files
 
     ## String representation of a request. Just gives game id, and date range.
     def __str__(self):
         fmt = "%Y%m%d"
         rng = self._range.GetDateRange()
-        return f"{self._game_id}: {self._range.GetDateRange()['min'].strftime(fmt)}-{self._range.GetDateRange()['max'].strftime(fmt)}"
+        return f"{self._interface._game_id}: {rng['min'].strftime(fmt)}-{rng['max'].strftime(fmt)}"
 
     ## Method to retrieve the list of IDs for all sessions covered by
     #  the request.
-    def retrieveSessionIDs(self, cursor, db_settings) -> typing.List:
-        # We grab the ids for all sessions that have 0th move in the proper date range.
-        if self._game_id == 'LAKELAND' or self._game_id == 'JOWILDER':
-            ver_filter = f" AND app_version in ({','.join([str(x) for x in self._game_schema.schema()['config']['SUPPORTED_VERS']])}) "
-        start = self.start_date.isoformat()
-        end = self.end_date.isoformat()
+    def retrieveSessionIDs(self, cursor, db_settings) -> Union[List[int],None]:
         supported_vers = Schema(schema_name=f"{self.game_id}.json")['config']['SUPPORTED_VERS']
-        ver_filter = f" AND `app_version` in ({','.join([str(x) for x in supported_vers])}) " if supported_vers else ''
-        filt = f"`app_id`=\"{self.game_id}\" AND `session_n`='0' AND (`server_time` BETWEEN '{start}' AND '{end}'){ver_filter}"
-        session_ids_raw = SQL.SELECT(cursor=cursor, db_name=db_settings["DB_NAME_DATA"], table=db_settings["TABLE"],
-                                columns=["`session_id`"], filter=filt,
-                                sort_columns=["`session_id`"], sort_direction="ASC", distinct=True)
-        return [sess[0] for sess in session_ids_raw]
-
-## Class representing a request that includes a range of dates to be retrieved
-#  for the given game.
-class DateRangeRequest(Request):
-    def __init__(self, game_id: str = None, start_date: date = None, end_date: date = None,
-                 exporter_files: ExporterFiles = ExporterFiles()):
-        Request.__init__(self, game_id=game_id, exporter_files=exporter_files)
-        self.start_date = start_date
-        self.end_date = end_date
-
-    ## String representation of a request. Just gives game id, and date range.
-    def __str__(self):
-        fmt = "%Y%m%d"
-        return f"{self.game_id}: {self.start_date.strftime(fmt)}-{self.end_date.strftime(fmt)}"
-
-    ## Method to retrieve the list of IDs for all sessions covered by
-    #  the request.
-    def retrieveSessionIDs(self, db_cursor, db_settings) -> typing.List:
-        # We grab the ids for all sessions that have 0th move in the proper date range.
-        start = self.start_date.isoformat()
-        end = self.end_date.isoformat()
-        supported_vers = Schema(schema_name=f"{self.game_id}.json")['config']['SUPPORTED_VERS']
-        ver_filter = f" AND `app_version` in ({','.join([str(x) for x in supported_vers])}) " if supported_vers else ''
-        filt = f"`app_id`=\"{self.game_id}\" AND `session_n`='0' AND (`server_time` BETWEEN '{start}' AND '{end}'){ver_filter}"
-        session_ids_raw = SQL.SELECT(cursor=db_cursor, db_name=db_settings["DB_NAME_DATA"], table=db_settings["TABLE"],
-                                columns=["`session_id`"], filter=filt,
-                                sort_columns=["`session_id`"], sort_direction="ASC", distinct=True)
-        return [sess[0] for sess in session_ids_raw]
-
-## Class representing a request for a specific list of session IDs.
-class IDListRequest(Request):
-    def __init__(self, game_id: str = None, session_ids = [], exporter_files: ExporterFiles = ExporterFiles()):
-        Request.__init__(self, game_id=game_id, exporter_files=exporter_files)
-        self._session_ids = session_ids
-
-    def __str__(self):
-        return f"{self.game_id}: {self._session_ids[0]}-{self._session_ids[-1]}"
-
-    ## Method to retrieve the list of IDs for all sessions covered by
-    #  the request. Should just be the original request list.
-    def retrieveSessionIDs(self, db_cursor, db_settings) -> typing.List:
-        return self._session_ids
-
-class FileRequest(Request):
-    def __init__(self, file_path, game_id: str = None, exporter_files: ExporterFiles = ExporterFiles()):
-        Request.__init__(self, game_id=game_id, exporter_files=exporter_files)
-        self.file_path = file_path
-
-    def __str__(self):
-        return f"{self.game_id}: {str(self.file_path)}"
-
-    # TODO: actually get the sessionIDs, so this request behaves properly.
-    def retrieveSessionIDs(self, db_cursor, db_settings) -> typing.List:
-        return []
+        dates = self._range.GetDateRange()
+        return self._interface.IDsFromDates(dates['min'], dates['max'], versions=supported_vers)
