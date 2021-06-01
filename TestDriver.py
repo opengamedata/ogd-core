dac7d804a30bd417e2616b33751cca490d0c7272
LswaN58 <lwswanson2@wisc.edu>
2021-05-31 21:09:09 -0500

    Should probably run utils tests first, since utils may be used by other modules.

--- TestDriver.py
+++ TestDriver.py
@@ -1,7 +1,7 @@
 from tests.t_interfaces.t_CSVInterface import t_CSVInterface
 from tests.t_utils import t_utils
 
-test_CSVInterface = t_CSVInterface()
-test_CSVInterface.RunAll()
 test_utils = t_utils()
 test_utils.RunAll()
+test_CSVInterface = t_CSVInterface()
+test_CSVInterface.RunAll()
