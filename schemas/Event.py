9f452f7952ca5a5e9cf9793cf36af9f7670801e9
LswaN58 <lwswanson2@wisc.edu>
2021-06-04 12:16:54 -0500

    Update Event class to use agree-upon internal data structure.

--- schemas/Event.py
+++ schemas/Event.py
@@ -1,29 +1,22 @@
+from datetime import datetime
+from typing import Any, Dict, Union
+
 ## @class Event
 #  Completely dumb struct that enforces a particular structure for the data we get from a source.
 #  Basically, whenever we fetch data, the TableSchema will be used to map columns to the required elements of an Event.
 #  Then the extractors etc. can just access columns in a direct manner.
 class Event:
-    def __init__(self, id, app_id, app_id_fast, app_version,
-                 session_id, persistent_session_id, player_id,
-                 level, event, event_custom, event_data_simple, event_data_complex,
-                 client_time, client_time_ms, server_time,
-                 remote_addr, req_id, session_n, http_user_agent):
-        self.id = id
+    def __init__(self, session_id:int, app_id:str, timestamp:datetime, event_name:str, event_data:Dict[str,Any],
+                 user_id:Union[int,None] = None,   user_data:Union[Dict[str,Any],None] = None,
+                 game_state:Union[Dict[str,Any],None] = None, time_offset:Union[int,None] = None,
+                 event_sequence_index:Union[int,None] = None):
+        self.session_id = session_id
         self.app_id = app_id
-        self.app_id_fast = app_id_fast
-        self.app_version = app_version
-        self.session_id  = session_id
-        self.persistent_session_id  = persistent_session_id
-        self.player_id  = player_id
-        self.level      = level
-        self.event      = event
-        self.event_custom = event_custom
-        self.event_data_simple = event_data_simple
-        self.event_data_complex = event_data_complex
-        self.client_time = client_time
-        self.client_time_ms = client_time_ms
-        self.server_time = server_time
-        self.remote_addr = remote_addr
-        self.req_id = req_id
-        self.session_n  = session_n
-        self.http_user_agent  = http_user_agent
+        self.timestamp = timestamp
+        self.event_name = event_name
+        self.event_data = event_data
+        self.user_id = user_id
+        self.user_data = user_data
+        self.game_state = game_state
+        self.time_offset = time_offset
+        self.event_sequence_index = event_sequence_index
