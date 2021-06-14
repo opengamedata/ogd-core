65ddf545f87e4fbe5aa9181d90b512f6e7abb9d8
LswaN58 <lwswanson2@wisc.edu>
2021-06-14 08:32:06 -0500

    Convert to Sphinx docstring and add better default path.

--- schemas/GameSchema.py
+++ schemas/GameSchema.py
@@ -13,22 +13,25 @@ import utils
 #  The class includes several functions for easy access to the various parts of
 #  this schema data.
 class GameSchema:
-    ## Constructor for the GameSchema class.
-    #  Given a path and filename, it loads the data from a JSON schema,
-    #  storing the full schema into a private variable, and compiling a list of
-    #  all features to be extracted.
-    #
-    #  @param schema_name The name of the JSON schema file
-    #                     (if .json is not the file extension, .json will be appended)
-    #  @param schema_path Path to the folder containing the JSON schema file
-    #                     (if the path does not end in "/", a "/" will be appended)
-    def __init__(self, schema_name:str, schema_path:str = os.path.dirname(__file__) + "/GAMES/"):
+    def __init__(self, schema_name:str, schema_path:Union[str,None] = None):
+        """Constructor for the GameSchema class.
+        Given a path and filename, it loads the data from a JSON schema,
+        storing the full schema into a private variable, and compiling a list of
+        all features to be extracted.
+
+        :param schema_name: The name of the JSON schema file (if .json is not the file extension, .json will be appended)
+        :type schema_name: str
+        :param schema_path: schema_path Path to the folder containing the JSON schema file, defaults to None
+        :type schema_path: str, optional
+        """
         # define instance vars
         self._schema:       Dict = {}
         self._feature_list: Union[List,None] = None
         # set instance vars
         if not schema_name.lower().endswith(".json"):
             schema_name += ".json"
+        if schema_path == None:
+            schema_path = f"games/{schema_name.split('.')[0]}"
         self._schema = utils.loadJSONFile(schema_name, schema_path)
         if self._schema is None:
             utils.Logger.Log(f"Could not find event_data_complex schemas at {schema_path}{schema_name}", logging.ERROR)
