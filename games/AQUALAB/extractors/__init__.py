660ed168c7d4dd97042172cee2542b0f29efbb3d
nspevacek <nspevacek@wisc.edu>
2021-07-02 15:54:26 -0500

    Create `__init__.py` for Aqualab extractors, add new events to json

---
+++ games/AQUALAB/extractors/__init__.py
@@ -0,0 +1,34 @@
+__all__ = [
+    "DiveSitesCount",
+    "JobArgumentationTime",
+    "JobCompletionTime",
+    "JobDiveTime",
+    "JobExperimentationTime",
+    "JobModelingTime",
+    "JobTasksCompleted",
+    "SessionDuration",
+    "SessionGuideCount",
+    "SessionHelpCount",
+    "SessionJobsCompleted",
+    "SwitchJobsCount",
+    "SyncCompletionTime",
+    "TotalArgumentationTime",
+    "TotalDiveTime",
+    "TotalExperimentationTime"
+]
+
+from . import DiveSitesCount
+from . import JobArgumentationTime
+from . import JobCompletionTime
+from . import JobExperimentationTime
+from . import JobModelingTime
+from . import JobTasksCompleted
+from . import SessionDuration
+from . import SessionGuideCount
+from . import SessionHelpCount
+from . import SessionJobsCompleted
+from . import SwitchJobsCount
+from . import SyncCompletionTime
+from . import TotalArgumentationTime
+from . import TotalDiveTime
+from . import TotalExperimentationTime
