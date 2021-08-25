a0e0c588d2aca13d4f4e1979ba727d451e7c1044
LswaN58 <lwswanson2@wisc.edu>
2021-08-25 12:39:56 -0500

    Be more consistent with using Path objects.
    Also, cleaned up some formatting stuff, and got rid of the 'base path'
    thing. I'm not sure why I thought the 'base path' was useful.

--- managers/FileManager.py
+++ managers/FileManager.py
@@ -17,14 +17,16 @@ from managers.Request import Request, ExporterFiles, ExporterRange
 
 class FileManager(abc.ABC):
     def __init__(self, exporter_files: ExporterFiles, game_id, data_dir: str, date_range: Dict[str,datetime]):
-        self._file_names : Dict[str,Union[Path,None]] = {"sessions_f":None, "events_f":None}
-        self._zip_names  : Dict[str,Union[Path,None]] = {"sessions_f":None, "events_f":None}
-        self._files      : Dict = {"sessions_f":None, "events_f":None}
-        self._data_dir   : Path = Path("./" + data_dir)
-        self._game_id    : str  = game_id
-        self._readme_path: Path
-        self._dataset_id : str
-        self._short_hash : str
+        self._file_names   : Dict[str,Union[Path,None]] = {"sessions_f":None, "events_f":None}
+        self._zip_names    : Dict[str,Union[Path,None]] = {"sessions_f":None, "events_f":None}
+        self._files        : Dict = {"sessions_f":None, "events_f":None}
+        self._data_dir     : Path = Path("./" + data_dir)
+        self._game_data_dir: Path = self._data_dir / self._game_id
+        # self._base_path    : Path = self._game_data_dir / f"{self._dataset_id}_{self._short_hash}"
+        self._game_id      : str  = game_id
+        self._readme_path  : Path
+        self._dataset_id   : str
+        self._short_hash   : str
         try:
             # figure out dataset ID.
             start = date_range['min'].strftime("%Y%m%d")
@@ -34,14 +36,13 @@ class FileManager(abc.ABC):
             repo = git.Repo(search_parent_directories=True)
             self._short_hash = str(repo.git.rev_parse(repo.head.object.hexsha, short=7))
             # then set up our paths, and ensure each exists.
-            full_data_dir     : Path = self._data_dir / game_id
-            self._readme_path : Path = full_data_dir / "readme.md"
+            self._readme_path : Path = self._game_data_dir/ "readme.md"
             base_file_name    : str  = f"{self._dataset_id}_{self._short_hash}"
             # finally, generate file names.
-            self._file_names["sessions_f"] = full_data_dir / f"{base_file_name}_session-features.csv" if exporter_files.sessions else None
-            self._file_names["events_f"]   = full_data_dir / f"{base_file_name}_events.tsv" if exporter_files.events else None
-            self._zip_names["sessions_f"]  = full_data_dir / f"{base_file_name}_session-features.zip" if exporter_files.sessions else None
-            self._zip_names["events_f"]    = full_data_dir / f"{base_file_name}_events.zip" if exporter_files.events else None
+            self._file_names["sessions_f"] = self._game_data_dir / f"{base_file_name}_session-features.csv" if exporter_files.sessions else None
+            self._file_names["events_f"]   = self._game_data_dir / f"{base_file_name}_events.tsv" if exporter_files.events else None
+            self._zip_names["sessions_f"]  = self._game_data_dir / f"{base_file_name}_session-features.zip" if exporter_files.sessions else None
+            self._zip_names["events_f"]    = self._game_data_dir / f"{base_file_name}_events.zip" if exporter_files.events else None
         except Exception as err:
             msg = f"{type(err)} {str(err)}"
             utils.Logger.Log(msg, logging.ERROR)
@@ -57,11 +58,9 @@ class FileManager(abc.ABC):
         return self._files["events_f"]
 
     def OpenFiles(self):
-        full_data_dir     : Path = self._data_dir / self._game_id
-        base_path         : Path = full_data_dir / f"{self._dataset_id}_{self._short_hash}"
-        self._data_dir.mkdir(exist_ok=True)
-        full_data_dir.mkdir(exist_ok=True)
-        base_path.mkdir(exist_ok=True)
+        # self._data_dir.mkdir(exist_ok=True)
+        self._game_data_dir.mkdir(exist_ok=True, parents=True)
+        # self._base_path.mkdir(exist_ok=True)
         self._files["sessions_f"] = open(self._file_names["sessions_f"], "w+", encoding="utf-8") if (self._file_names["sessions_f"] is not None) else None
         self._files["events_f"]   = open(self._file_names["events_f"],   "w+", encoding="utf-8") if (self._file_names["events_f"] is not None) else None
 
@@ -78,18 +77,18 @@ class FileManager(abc.ABC):
             existing_csvs = {}
         # if we have already done this dataset before, rename old zip files
         # (of course, first check if we ever exported this game before).
-        try:
-            if (self._game_id in existing_csvs and self._dataset_id in existing_csvs[self._game_id]):
-                src_sessions_f = existing_csvs[self._game_id][self._dataset_id]['sessions_f']
-                src_events_f = existing_csvs[self._game_id][self._dataset_id]['events_f']
+        if (self._game_id in existing_csvs and self._dataset_id in existing_csvs[self._game_id]):
+            src_sessions_f = existing_csvs[self._game_id][self._dataset_id]['sessions_f']
+            src_events_f = existing_csvs[self._game_id][self._dataset_id]['events_f']
+            try:
                 if src_sessions_f is not None and self._zip_names["sessions_f"] is not None:
                     os.rename(src_sessions_f, str(self._zip_names["sessions_f"]))
                 if src_events_f is not None and self._zip_names["events_f"] is not None:
                     os.rename(src_events_f, str(self._zip_names["events_f"]))
-        except Exception as err:
-            msg = f"Error while setting up zip files! {type(err)} : {err}"
-            utils.Logger.Log(msg, logging.ERROR)
-            traceback.print_tb(err.__traceback__)
+            except Exception as err:
+                msg = f"Error while setting up zip files! {type(err)} : {err}"
+                utils.Logger.Log(msg, logging.ERROR)
+                traceback.print_tb(err.__traceback__)
         # for each file, try to save out the csv/tsv to a file - if it's one that should be exported, that is.
         base_path = f"{self._dataset_id}/{self._dataset_id}_{self._short_hash}"
         if self._zip_names["sessions_f"] is not None:
@@ -128,36 +127,30 @@ class FileManager(abc.ABC):
     #  deriving file metadata, this simply outputs a new file_name.meta file.
     #  @param date_range    The range of dates included in the exported data.
     #  @param num_sess      The number of sessions included in the recent export.
-    def WriteMetadataFile(self, date_range: Dict[str,datetime], num_sess: int):
+    def WriteMetadataFile(self, date_range:Dict[str,datetime], num_sess:int):
         # First, ensure we have a data directory.
-        full_data_dir : Path = self._data_dir / self._game_id
         try:
-            full_data_dir.mkdir(exist_ok=True)
+            self._game_data_dir.mkdir(exist_ok=True, parents=True)
         except Exception as err:
-            msg = f"Could not set up folder {full_data_dir}. {type(err)} {str(err)}"
+            msg = f"Could not set up folder {self._game_data_dir}. {type(err)} {str(err)}"
             utils.Logger.toFile(msg, logging.WARNING)
         else:
             # Second, remove old metas, if they exist.
             start_range = date_range['min'].strftime("%Y%m%d")
             end_range = date_range['max'].strftime("%Y%m%d")
             match_string = f"{self._game_id}_{start_range}_to_{end_range}_\\w*\\.meta"
-            old_metas = [f for f in os.listdir(full_data_dir) if re.match(match_string, f)]
+            old_metas = [f for f in os.listdir(self._game_data_dir) if re.match(match_string, f)]
             for old_meta in old_metas:
                 try:
                     utils.Logger.Log(f"Removing old meta file, {old_meta}")
-                    os.remove(f"{full_data_dir}/{old_meta}")
+                    os.remove(self._game_data_dir / old_meta)
                 except Exception as err:
                     msg = f"Could not remove old meta file {old_meta}. {type(err)} {str(err)}"
                     utils.Logger.Log(msg, logging.WARNING)
             # Third, write the new meta file.
-            try:
-                # calculate the path and name of the metadata file, and open/make it.
-                meta_file_name = f"{full_data_dir}/{self._dataset_id}_{self._short_hash}.meta"
-                meta_file = open(meta_file_name, "w", encoding="utf-8")
-            except Exception as err:
-                msg = f"Could not open file for metadata. {type(err)} {str(err)}"
-                utils.Logger.toFile(msg, logging.WARNING)
-            else:
+            # calculate the path and name of the metadata file, and open/make it.
+            meta_file_path : Path = self._game_data_dir/ f"{self._dataset_id}_{self._short_hash}.meta"
+            with open(meta_file_path, "w", encoding="utf-8") as meta_file :
                 metadata  = \
                 {
                     "game_id"      :self._game_id,
