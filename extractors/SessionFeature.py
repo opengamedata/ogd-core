1465cf4175eb83e4bf3c1f1b8c545852a880a294
LswaN58 <lwswanson2@wisc.edu>
2021-08-31 12:07:54 -0500

    Add a SessionFeature class, which basically just ignores count_index.

---
+++ extractors/SessionFeature.py
@@ -0,0 +1,13 @@
+# Local imports
+from Feature import Feature
+from schemas.Event import Event
+
+## @class Model
+#  Abstract base class for session-level Wave features.
+#  Models only have one public function, called Eval.
+#  The Eval function takes a list of row data, computes some statistic, and returns a list of results.
+#  If the model works on features from session data, it should calculate one result for each row (each row being a session).
+#  If the model works on a raw list of recent events, it should calculate a single result (each row being an event).
+class SessionFeature(Feature):
+    def __init__(self, name:str, description:str):
+        Feature.__init__(self, name=name, description=description, count_index=0)
