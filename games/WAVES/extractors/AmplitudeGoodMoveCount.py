e7069f04701d098d162558c1bfa6e34004922584
LswaN58 <lwswanson2@wisc.edu>
2021-09-01 20:02:32 -0500

    Also need to update AmplitudeGoodMoveCount to use actual event names.

--- games/WAVES/extractors/AmplitudeGoodMoveCount.py
+++ games/WAVES/extractors/AmplitudeGoodMoveCount.py
@@ -14,13 +14,13 @@ class AmplitudeGoodMoveCount(Feature):
         return self._count
 
     def _extractFromEvent(self, event:Event):
-        self._count += 1
         if event.event_data['slider'] == 'Amplitude':
             if event.event_data['closeness_end'] > event.event_data['closeness_start']:
                 self._count += 1
 
     def GetEventTypes(self) -> List[str]:
-        return ["SLIDER_MOVE_RELEASE", "ARROW_MOVE_RELEASE"]
+        return ["CUSTOM.1", "CUSTOM.2"]
+        # return ["SLIDER_MOVE_RELEASE", "ARROW_MOVE_RELEASE"]
 
     def MinVersion(self) -> Union[str,None]:
         return None
