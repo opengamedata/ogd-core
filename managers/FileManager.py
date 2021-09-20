2e325bb7c5e6dc5c8ae84f52df7d426c29aa03be
LswaN58 <lwswanson2@wisc.edu>
2021-09-19 21:02:58 -0500

    Remove _f suffix from most file dict keys, and add a 'population' file set

--- managers/FileManager.py
+++ managers/FileManager.py
@@ -17,9 +17,9 @@ from managers.Request import ExporterFiles, ExporterRange
 
 class FileManager(abc.ABC):
     def __init__(self, exporter_files: ExporterFiles, game_id, data_dir: str, date_range: Dict[str,Union[datetime,None]], extension:str="tsv"):
-        self._file_names   : Dict[str,Union[Path,None]] = {"sessions_f":None, "events_f":None}
-        self._zip_names    : Dict[str,Union[Path,None]] = {"sessions_f":None, "events_f":None}
-        self._files        : Dict[str,Union[IO,None]]   = {"sessions_f":None, "events_f":None}
+        self._file_names   : Dict[str,Union[Path,None]] = {"population":None, "sessions":None, "events":None}
+        self._zip_names    : Dict[str,Union[Path,None]] = {"population":None, "sessions":None, "events":None}
+        self._files        : Dict[str,Union[IO,None]]   = {"population":None, "sessions":None, "events":None}
         self._game_id      : str  = game_id
         self._data_dir     : Path = Path("./" + data_dir)
         self._game_data_dir: Path = self._data_dir / self._game_id
@@ -40,10 +40,10 @@ class FileManager(abc.ABC):
             # then set up our paths, and ensure each exists.
             base_file_name    : str  = f"{self._dataset_id}_{self._short_hash}"
             # finally, generate file names.
-            self._file_names["events_f"]   = self._game_data_dir / f"{base_file_name}_events.{self._extension}" if exporter_files.events else None
-            self._zip_names["events_f"]    = self._game_data_dir / f"{base_file_name}_events.zip" if exporter_files.events else None
-            self._file_names["sessions_f"] = self._game_data_dir / f"{base_file_name}_session-features.{self._extension}" if exporter_files.sessions else None
-            self._zip_names["sessions_f"]  = self._game_data_dir / f"{base_file_name}_session-features.zip" if exporter_files.sessions else None
+            self._file_names["events"]   = self._game_data_dir / f"{base_file_name}_events.{self._extension}" if exporter_files.events else None
+            self._zip_names["events"]    = self._game_data_dir / f"{base_file_name}_events.zip" if exporter_files.events else None
+            self._file_names["sessions"] = self._game_data_dir / f"{base_file_name}_session-features.{self._extension}" if exporter_files.sessions else None
+            self._zip_names["sessions"]  = self._game_data_dir / f"{base_file_name}_session-features.zip" if exporter_files.sessions else None
         except Exception as err:
             msg = f"{type(err)} {str(err)}"
             utils.Logger.Log(msg, logging.ERROR)
@@ -52,24 +52,27 @@ class FileManager(abc.ABC):
     def GetFiles(self) -> Dict[str,Union[IO,None]]:
         return self._files
 
+    def GetPopulationFile(self) -> Union[IO,None]:
+        return self._files["population"]
+
     def GetSessionsFile(self) -> Union[IO,None]:
-        return self._files["sessions_f"]
+        return self._files["sessions"]
 
     def GetEventsFile(self) -> Union[IO,None]:
-        return self._files["events_f"]
+        return self._files["events"]
 
     def OpenFiles(self) -> None:
         # self._data_dir.mkdir(exist_ok=True)
         self._game_data_dir.mkdir(exist_ok=True, parents=True)
         # self._base_path.mkdir(exist_ok=True)
-        self._files["sessions_f"] = open(self._file_names["sessions_f"], "w+", encoding="utf-8") if (self._file_names["sessions_f"] is not None) else None
-        self._files["events_f"]   = open(self._file_names["events_f"],   "w+", encoding="utf-8") if (self._file_names["events_f"] is not None) else None
+        self._files["sessions"] = open(self._file_names["sessions"], "w+", encoding="utf-8") if (self._file_names["sessions"] is not None) else None
+        self._files["events"]   = open(self._file_names["events"],   "w+", encoding="utf-8") if (self._file_names["events"] is not None) else None
 
     def CloseFiles(self) -> None:
-        if self._files["sessions_f"] is not None:
-            self._files["sessions_f"].close()
-        if self._files["events_f"] is not None:
-            self._files["events_f"].close()
+        if self._files["sessions"] is not None:
+            self._files["sessions"].close()
+        if self._files["events"] is not None:
+            self._files["events"].close()
 
     def ZipFiles(self) -> None:
         try:
@@ -79,41 +82,41 @@ class FileManager(abc.ABC):
         # if we have already done this dataset before, rename old zip files
         # (of course, first check if we ever exported this game before).
         if (self._game_id in existing_csvs and self._dataset_id in existing_csvs[self._game_id]):
-            src_sessions_f = existing_csvs[self._game_id][self._dataset_id]['sessions_f']
-            src_events_f = existing_csvs[self._game_id][self._dataset_id]['events_f']
+            src_sessions_f = existing_csvs[self._game_id][self._dataset_id]['sessions']
+            src_events_f = existing_csvs[self._game_id][self._dataset_id]['events']
             try:
-                if src_sessions_f is not None and self._zip_names["sessions_f"] is not None:
-                    os.rename(src_sessions_f, str(self._zip_names["sessions_f"]))
-                if src_events_f is not None and self._zip_names["events_f"] is not None:
-                    os.rename(src_events_f, str(self._zip_names["events_f"]))
+                if src_sessions_f is not None and self._zip_names["sessions"] is not None:
+                    os.rename(src_sessions_f, str(self._zip_names["sessions"]))
+                if src_events_f is not None and self._zip_names["events"] is not None:
+                    os.rename(src_events_f, str(self._zip_names["events"]))
             except Exception as err:
                 msg = f"Error while setting up zip files! {type(err)} : {err}"
                 utils.Logger.Log(msg, logging.ERROR)
                 traceback.print_tb(err.__traceback__)
         # for each file, try to save out the csv/tsv to a file - if it's one that should be exported, that is.
-        if self._zip_names["sessions_f"] is not None:
-            with zipfile.ZipFile(self._zip_names["sessions_f"], "w", compression=zipfile.ZIP_DEFLATED) as sessions_zip_file:
+        if self._zip_names["sessions"] is not None:
+            with zipfile.ZipFile(self._zip_names["sessions"], "w", compression=zipfile.ZIP_DEFLATED) as sessions_zip_file:
                 try:
                     session_file = Path(self._dataset_id) / f"{self._dataset_id}_{self._short_hash}_session_features.{self._extension}"
                     readme_file  = Path(self._dataset_id) / "readme.md"
-                    self._addToZip(path=self._file_names["sessions_f"], zip_file=sessions_zip_file, path_in_zip=session_file)
-                    self._addToZip(path=self._readme_path,        zip_file=sessions_zip_file, path_in_zip=readme_file)
+                    self._addToZip(path=self._file_names["sessions"], zip_file=sessions_zip_file, path_in_zip=session_file)
+                    self._addToZip(path=self._readme_path,            zip_file=sessions_zip_file, path_in_zip=readme_file)
                     sessions_zip_file.close()
-                    if self._file_names["sessions_f"] is not None:
-                        os.remove(self._file_names["sessions_f"])
+                    if self._file_names["sessions"] is not None:
+                        os.remove(self._file_names["sessions"])
                 except FileNotFoundError as err:
                     utils.Logger.Log(f"FileNotFoundError Exception: {err}", logging.ERROR)
                     traceback.print_tb(err.__traceback__)
-        if self._zip_names["events_f"] is not None:
-            with zipfile.ZipFile(self._zip_names["events_f"], "w", compression=zipfile.ZIP_DEFLATED) as events_zip_file:
+        if self._zip_names["events"] is not None:
+            with zipfile.ZipFile(self._zip_names["events"], "w", compression=zipfile.ZIP_DEFLATED) as events_zip_file:
                 try:
                     events_file = Path(self._dataset_id) / f"{self._dataset_id}_{self._short_hash}_events.{self._extension}"
                     readme_file = Path(self._dataset_id) / "readme.md"
-                    self._addToZip(path=self._file_names["events_f"], zip_file=events_zip_file, path_in_zip=events_file)
+                    self._addToZip(path=self._file_names["events"], zip_file=events_zip_file, path_in_zip=events_file)
                     self._addToZip(path=self._readme_path,        zip_file=events_zip_file, path_in_zip=readme_file)
                     events_zip_file.close()
-                    if self._file_names["events_f"] is not None:
-                        os.remove(self._file_names["events_f"])
+                    if self._file_names["events"] is not None:
+                        os.remove(self._file_names["events"])
                 except FileNotFoundError as err:
                     utils.Logger.Log(f"FileNotFoundError Exception: {err}", logging.ERROR)
                     traceback.print_tb(err.__traceback__)
@@ -159,8 +162,8 @@ class FileManager(abc.ABC):
                     "game_id"      :self._game_id,
                     "dataset_id"   :self._dataset_id,
                     "ogd_revision" :self._short_hash,
-                    "sessions_f"   :str(self._zip_names["sessions_f"]),
-                    "events_f"     :str(self._zip_names["events_f"]),
+                    "sessions_f"   :str(self._zip_names["sessions"]),
+                    "events_f"     :str(self._zip_names["events"]),
                     "start_date"   :self._date_range['min'].strftime("%m/%d/%Y") if self._date_range['min'] is not None else "Unknown",
                     "end_date"     :self._date_range['max'].strftime("%m/%d/%Y") if self._date_range['max'] is not None else "Unknown",
                     "date_modified":datetime.now().strftime("%m/%d/%Y"),
@@ -192,8 +195,8 @@ class FileManager(abc.ABC):
                     existing_csvs[self._game_id] = {}
                 # sessions_stat = os.stat(sessions_csv_full_path)
                 prior_export = self._dataset_id in existing_csvs[self._game_id].keys()
-                sessions_path = str(self._zip_names["sessions_f"]) if self._zip_names["sessions_f"] is not None else (existing_csvs[self._game_id][self._dataset_id]["sessions_f"] if prior_export else None)
-                events_path   = str(self._zip_names["events_f"]) if self._zip_names["events_f"] is not None else (existing_csvs[self._game_id][self._dataset_id]["events_f"] if prior_export else None)
+                sessions_path = str(self._zip_names["sessions"]) if self._zip_names["sessions"] is not None else (existing_csvs[self._game_id][self._dataset_id]["sessions"] if prior_export else None)
+                events_path   = str(self._zip_names["events"]) if self._zip_names["events"] is not None else (existing_csvs[self._game_id][self._dataset_id]["events"] if prior_export else None)
                 existing_csvs[self._game_id][self._dataset_id] = \
                 {
                     "ogd_revision" :self._short_hash,
