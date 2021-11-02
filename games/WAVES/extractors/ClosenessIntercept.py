5e0a026357a670ca62fc42de2495904fa9b69593
LswaN58 <lwswanson2@wisc.edu>
2021-11-02 04:48:43 -0500

    Implement PercentAmplitudeGoodMoves, and add commented code for ClosenessIntercept and other Closeness stuff.

--- games/WAVES/extractors/ClosenessIntercept.py
+++ games/WAVES/extractors/ClosenessIntercept.py
@@ -16,7 +16,24 @@ class ClosenessIntercept(Feature):
         return
 
     def _extractFromEvent(self, event:Event) -> None:
-        return
+    #     closenesses = self.move_closenesses_tx[lvl]['completeness']
+    #     times = self.move_closenesses_tx[lvl]['t']
+    #     ranges = self.move_closenesses_tx[lvl]['range']
+    #     if times:
+    #         try:
+    #             X = [(times[i]-times[0]).seconds for i in range(len(times))]
+    #         except Exception as err:
+    #             utils.Logger.toStdOut(times[0])
+    #             raise err
+    #         y = closenesses
+    #         if len(X) > 1:
+    #             intercept, slope, r_sq = self._2D_linear_regression(X, y)
+    #             r_sq = r_sq if not np.isnan(r_sq) else 0
+    #         else:
+    #             intercept, slope, r_sq = 0,0,0
+    #         self._features.setValByIndex(feature_name='closenessIntercept', index=lvl, new_value=intercept)
+    #         self._features.setValByIndex(feature_name='closenessSlope', index=lvl, new_value=slope)
+    #         self._features.setValByIndex(feature_name='closenessR2', index=lvl, new_value=r_sq)
 
     def MinVersion(self) -> Union[str,None]:
         return None
