91774de823c6563166fbf8593b647b0b143410e4
LswaN58 <lwswanson2@wisc.edu>
2021-11-09 12:33:05 -0600

    Further fixes for differences between CUSTOM.1 and CUSTOM.2

--- games/WAVES/extractors/PercentOffsetGoodMoves.py
+++ games/WAVES/extractors/PercentOffsetGoodMoves.py
@@ -21,8 +21,14 @@ class PercentOffsetGoodMoves(Feature):
     def _extractFromEvent(self, event:Event) -> None:
         if event.event_data['slider'].upper() == 'OFFSET':
             self._offset_count += 1
-            if event.event_data['end_closeness'] > event.event_data['begin_closeness']:
-                self._good_count += 1
+            if event.event_name == "CUSTOM.1":
+                if event.event_data['end_closeness'] > event.event_data['begin_closeness']:
+                    self._good_count += 1
+            elif event.event_name == "CUSTOM.2":
+                start_dist = event.event_data['correct_val'] - event.event_data['begin_val']
+                end_dist = event.event_data['correct_val'] - event.event_data['end_val']
+                if abs(end_dist) < abs(start_dist):
+                    self._good_count += 1
 
     def MinVersion(self) -> Union[str,None]:
         return None
