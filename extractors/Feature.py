80b844f70dadd4ec8564da6419a3dba67b4d31ef
LswaN58 <lwswanson2@wisc.edu>
2021-11-22 11:54:53 -0600

    Rearrange Feature.py to be better organized. Add GetFeature Names function as well.

--- extractors/Feature.py
+++ extractors/Feature.py
@@ -1,7 +1,7 @@
 ## import standard libraries
 import abc
 import typing
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Tuple, Union
 # Local imports
 from schemas.Event import Event
 
@@ -12,16 +12,8 @@ from schemas.Event import Event
 #  If the model works on features from session data, it should calculate one result for each row (each row being a session).
 #  If the model works on a raw list of recent events, it should calculate a single result (each row being an event).
 class Feature(abc.ABC):
-    def __init__(self, name:str, description:str, count_index:int):
-        self._name = name
-        self._desc = description
-        self._count_index = count_index
 
-    def __str__(self):
-        return f"{self._name} : {self._desc}"
-
-    def Name(self):
-        return self._name
+    # *** ABSTRACTS ***
 
     ## Abstract function to get a list of event types the Feature wants.
     @abc.abstractmethod
@@ -34,21 +26,62 @@ class Feature(abc.ABC):
         :rtype: List[str]
         """
         pass
-    
+
+    ## Abstract declaration of a function to get the calculated value of the feature, given data seen so far.
     @abc.abstractmethod
-    def CalculateFinalValues(self) -> typing.Tuple:
+    def CalculateFinalValues(self) -> List[Any]:
+        """Abstract declaration of a function to get the calculated value of the feature, given data seen so far.
+
+        :return: Returns the values of all columns for the Feature, based on data the feature has seen so far.
+        :rtype: typing.Tuple
+        """
         pass
 
     ## Abstract declaration of a function to perform update of a feature from a row.
-    #
-    #  @param row : A row, which will be used to update the feature's data.
     @abc.abstractmethod
     def _extractFromEvent(self, event:Event):
+        """Abstract declaration of a function to perform update of a feature from a row.
+
+        :param event: An event, used to update the feature's data.
+        :type event: Event
+        """
         pass
 
-    ## Abstract function to get the minimum game data version the feature can handle.
+    # *** PUBLIC BUILT-INS ***
+
+    def __init__(self, name:str, description:str, count_index:int):
+        self._name = name
+        self._desc = description
+        self._count_index = count_index
+
+    def __str__(self):
+        return f"{self._name} : {self._desc}"
+
+    # *** PUBLIC STATICS ***
+
+    # *** PUBLIC METHODS ***
+
+    def Name(self):
+        return self._name
+
+    def GetFeatureNames(self) -> List[str]:
+        """Base function to get a list of names of the feature(s) a given Feature class outputs.
+        By default, a Feature class just generates one value, and uses its own name (defined in the schema.json file).
+        However, if a Feature class is written to output multiple values, it will need to override this function.
+        Note, CalculateFinalValues **must** match the ordering from the override of this GetFeatureNames, if returning a list of length > 1.
+
+        :return: [description]
+        :rtype: Tuple[str]
+        """
+        return [self.Name()]
+
+    def ExtractFromEvent(self, event:Event):
+        if self._validateEvent(event):
+            self._extractFromEvent(event)
+
+    ## Base function to get the minimum game data version the feature can handle.
     def MinVersion(self) -> Union[str,None]:
-        """ Abstract function to get the minimum game data version the feature can handle.
+        """ Base function to get the minimum game data version the feature can handle.
             A value of None will set no minimum, so all levels are accepted (unless a max is set).
             Typically default to None, unless there is a required element of the event data that was not added until a certain version.        
             The versions of data accepted by a feature are a responsibility of the Feature's developer,
@@ -59,9 +92,9 @@ class Feature(abc.ABC):
         """
         return None
 
-    ## Abstract function to get the maximum game data version the feature can handle.
+    ## Base function to get the maximum game data version the feature can handle.
     def MaxVersion(self) -> Union[str,None]:
-        """ Abstract function to get the maximum game data version the feature can handle.
+        """ Base function to get the maximum game data version the feature can handle.
             A value of None will set no maximum, so all levels are accepted (unless a min is set).
             Typically default to None, unless the feature is not compatible with new data and is only kept for legacy purposes.
             The versions of data accepted by a feature are a responsibility of the Feature's developer,
@@ -72,11 +105,18 @@ class Feature(abc.ABC):
         """
         return None
 
-    def ExtractFromEvent(self, event:Event):
-        if self._validateEvent(event):
-            self._extractFromEvent(event)
+    # *** PRIVATE STATICS ***
 
-    def _validateEvent(self, event:Event):
+    # *** PRIVATE METHODS ***
+
+    def _validateEvent(self, event:Event) -> bool:
+        """Private function to check if a given event has valid version and type for this Feature.
+
+        :param event: The event to be checked.
+        :type event: Event
+        :return: True if the event has valid version and type, otherwise false.
+        :rtype: bool
+        """
         return (
             self._validateVersion(event.app_version)
         and self._validateEventType(event_type=event.event_name)
@@ -84,6 +124,13 @@ class Feature(abc.ABC):
 
     ## Private function to check whether the given data version from a row is acceptable by this feature extractor.
     def _validateVersion(self, data_version:str) -> bool:
+        """Private function to check whether a given version is valid for this Feature.
+
+        :param data_version: The logging version for some event to be checked.
+        :type data_version: str
+        :return: True if the given version is valid for this feature, otherwise false.
+        :rtype: bool
+        """
         min = self.MinVersion()
         if min is not None:
             if Event.CompareVersions(data_version, min) < 0:
@@ -95,6 +142,13 @@ class Feature(abc.ABC):
         return True # passed both cases, valid.
 
     def _validateEventType(self, event_type:str) -> bool:
+        """Private function to check whether a given event type is accepted by this Feature.
+
+        :param event_type: The name of the event type to be checked.
+        :type event_type: str
+        :return: True if the given event type is in this feature's list, otherwise false.
+        :rtype: bool
+        """
         if event_type in self.GetEventTypes():
             return True
         else:
