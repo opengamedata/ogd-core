ab2f0112c782edfbaf09604c97f0153a401220d2
LswaN58 <lwswanson2@wisc.edu>
2021-11-26 14:44:25 -0600

    Ok, don't try to import in config's init file, and fix some path issues in games folder init.

--- games/__init__.py
+++ games/__init__.py
@@ -8,10 +8,10 @@ __all__ = [
     "WaveExtractor"
 ]
 
-from AQUALAB import AqualabExtractor
-from CRYSTAL import CrystalExtractor
-from JOWILDER import JowilderExtractor
-from LAKELAND import LakelandExtractor
-from MAGNET import MagnetExtractor
-from SHADOWSPECT import ShadowspectExtractor
-from WAVES import WaveExtractor
+from .AQUALAB import AqualabExtractor
+from .CRYSTAL import CrystalExtractor
+from .JOWILDER import JowilderExtractor
+from .LAKELAND import LakelandExtractor
+from .MAGNET import MagnetExtractor
+from .SHADOWSPECT import ShadowspectExtractor
+from .WAVES import WaveExtractor
