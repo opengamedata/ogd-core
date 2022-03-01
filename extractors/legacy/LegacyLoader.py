fb751ad7920e32eb24f7dfd068f30bead1512eec
LswaN58 <lwswanson2@wisc.edu>
2022-03-01 14:49:49 -0600

    Move back to storing game_schema within the FeatureLoader, forgot LoadFeature needs it.

--- extractors/legacy/LegacyLoader.py
+++ extractors/legacy/LegacyLoader.py
@@ -6,10 +6,10 @@ from extractors.FeatureRegistry import FeatureRegistry
 from schemas.GameSchema import GameSchema
 
 class LegacyLoader(FeatureLoader):
-    def __init__(self, player_id:str, session_id:str, feature_overrides:Union[List[str],None]):
-        super().__init__(player_id=player_id, session_id=session_id, feature_overrides=feature_overrides)
+    def __init__(self, player_id:str, session_id:str, game_schema:GameSchema, feature_overrides:Union[List[str],None]):
+        super().__init__(player_id=player_id, session_id=session_id, game_schema=game_schema, feature_overrides=feature_overrides)
 
-    def LoadToRegistry(self, registry:FeatureRegistry, game_schema:GameSchema) -> None:
+    def LoadToRegistry(self, registry:FeatureRegistry) -> None:
         feat = self.LoadFeature(feature_type="", name="", feature_args={}, count_index=0)
         # treat the monolithic LegacyFeature extractor as a single aggregate.
         registry.Register(feat, FeatureRegistry.Listener.Kinds.AGGREGATE)
