102978e70cf6d95c72af17506ed5eb97ae8b0c1d
nspevacek <nspevacek@wisc.edu>
2022-03-04 08:51:35 -0600

    `JobsAttempted` fixes
    - Check if `complete_job` user code matches `accept_job`
    - Calculate percent/avg/stdev in `GetFeatureValues`
    - Set `job_id` and `job_name` in init

--- games/AQUALAB/extractors/JobsAttempted.py
+++ games/AQUALAB/extractors/JobsAttempted.py
@@ -13,9 +13,11 @@ class JobsAttempted(Feature):
         self._job_num = job_num
         self._job_map = job_map
         super().__init__(name=name, description=description, count_index=job_num)
+        self._user_code = None
+
         # Subfeatures
-        self._job_id = None
-        self._job_name = None
+        self._job_id = job_num
+        self._job_name = list(job_map.keys())[job_num]
         self._num_starts = 0
         self._num_completes = 0
         self._percent_complete = 0
@@ -33,6 +35,15 @@ class JobsAttempted(Feature):
         return ["job-name", "num-starts", "num-completes", "percent-complete", "avg-time-complete", "std-dev-complete"]
 
     def GetFeatureValues(self) -> List[Any]:
+        if self._num_starts > 0:
+            self._percent_complete = (self._num_completes / (self._num_completes + self._num_starts)) * 100
+        
+        if len(self._times) > 0:
+            self._avg_time_complete = sum(self._times) / len(self._times)
+
+        if len(self._times) > 1:
+            self._std_dev_complete = stdev(self._times)
+
         return [self._job_id, self._job_name, self._num_starts, self._num_completes, self._percent_complete, self._avg_time_complete, self._std_dev_complete]
 
     def MinVersion(self) -> Union[str,None]:
@@ -40,26 +51,19 @@ class JobsAttempted(Feature):
 
     def _extractFromEvent(self, event:Event) -> None:
         if self._validate_job(event.event_data["job_id"]):
-            if not self._job_id and not self._job_name:
-                self._job_id = event.event_data["job_id"]["int_value"]
-                self._job_name = event.event_data["job_name"]["string_value"]
+            user_code = event.event_data["user_code"]["string_value"]
 
             if event.event_name == "accept_job" and self._job_id == self._job_num:
-                self._job_start_time = event.timestamp
                 self._num_starts += 1
+                self._user_code = user_code
+                self._job_start_time = event.timestamp
 
-            elif event.event_name == "complete_job" and self._job_id == self._job_num:
+            elif event.event_name == "complete_job" and self._job_id == self._job_num and user_code == self._user_code:
                 self._num_completes += 1
 
                 if self._job_start_time:
                     self._times.append((event.timestamp - self._job_start_time).total_seconds())
-                    self._avg_time_complete = sum(self._times) / len(self._times)
                     self._job_start_time = None
-
-            self._percent_complete = (self._num_completes / (self._num_completes + self._num_starts)) * 100
-                
-            if len(self._times) > 1:
-                self._std_dev_complete = stdev(self._times)
 
     def _validate_job(self, job_data):
         ret_val : bool = False
