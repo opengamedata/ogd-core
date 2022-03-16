8778297b15655c9ea70d11cf7b7904d0fa4d48f7
Luke Swanson <lwswanson2@wisc.edu>
2022-03-16 13:21:14 -0500

    Trying to update TopJobCompletionDestinations to return player IDs.

--- games/AQUALAB/features/TopJobCompletionDestinations.py
+++ games/AQUALAB/features/TopJobCompletionDestinations.py
@@ -5,6 +5,7 @@ from typing import Any, List, Union
 from features.Feature import Feature
 from features.FeatureData import FeatureData
 from schemas.Event import Event
+import utils
 
 class TopJobCompletionDestinations(Feature):
 
@@ -12,8 +13,7 @@ class TopJobCompletionDestinations(Feature):
         super().__init__(name=name, description=description, count_index=0)
         self._current_user_code = None
         self._last_completed_id = None
-        self._job_complete_pairs = defaultdict(list)
-        self._top_destinations = defaultdict(list)
+        self._job_complete_pairs = defaultdict(dict)
 
     def GetEventDependencies(self) -> List[str]:
         return ["accept_job", "complete_job"]
@@ -22,11 +22,18 @@ class TopJobCompletionDestinations(Feature):
         return []
 
     def GetFeatureValues(self) -> List[Any]:
-        # Count the top five accepted job ids for each completed job id
-        for key in self._job_complete_pairs.keys():
-            self._top_destinations[str(key)] = Counter(self._job_complete_pairs[key]).most_common(5)
-
-        return [dict(self._top_destinations)]
+        ret_val = {}
+        for src in self._job_complete_pairs.keys():
+            dests = sorted(
+                self._job_complete_pairs[src].items(),
+                key=lambda item: len(item[1]), # sort by length of list of ids.
+                reverse=True # sort largest to smallest
+            )
+            ret_val[src] = {
+                item[0]:item[1] for item in dests[0:5]
+            }
+            utils.Logger.Log(f"For TopJobDestinations, sorted dests as: {dests}")
+        return [ret_val]
 
     def MinVersion(self) -> Union[str,None]:
         return "2"
@@ -40,7 +47,9 @@ class TopJobCompletionDestinations(Feature):
             self._last_completed_id = event.event_data["job_id"]["int_value"] # here, we take what we last completed, and append where we switched to.
         elif event.event_name == "accept_job":
             if user_code == self._current_user_code and self._last_completed_id is not None:
-                self._job_complete_pairs[self._last_completed_id].append(job_id)
+                if not job_id in self._job_complete_pairs[self._last_completed_id].keys():
+                    self._job_complete_pairs[self._last_completed_id][job_id] = []
+                self._job_complete_pairs[self._last_completed_id][job_id].append(user_code)
                 self._last_completed_id = None
         # finally, once we process the event, we know we're looking at data for this event's user.
         self._current_user_code = user_code
