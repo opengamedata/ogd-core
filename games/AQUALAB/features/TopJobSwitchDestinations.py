1f3447ad66f25b76150a8eceec330c3c222efedd
nspevacek <nspevacek@wisc.edu>
2022-03-17 19:20:07 -0500

    Fix `TopJobSwitchDestinations`

--- games/AQUALAB/features/TopJobSwitchDestinations.py
+++ games/AQUALAB/features/TopJobSwitchDestinations.py
@@ -17,7 +17,7 @@ class TopJobSwitchDestinations(Feature):
         self._job_switch_pairs = defaultdict(dict)
 
     def GetEventDependencies(self) -> List[str]:
-        return ["accept_job", "switch_job"]
+        return ["switch_job"]
 
     def GetFeatureDependencies(self) -> List[str]:
         return []
@@ -44,19 +44,14 @@ class TopJobSwitchDestinations(Feature):
     def _extractFromEvent(self, event:Event) -> None:
         user_code = event.user_id
         job_name = event.event_data["job_name"]["string_value"]
+        prev_job_name = event.event_data["prev_job_name"]["string_value"]
+        
+        if user_code == self._current_user_code and job_name != prev_job_name and prev_job_name != "no-active-job":
+            if not job_name in self._job_switch_pairs[prev_job_name]:
+                self._job_switch_pairs[prev_job_name][job_name] = []
 
-        if event.event_name == "accept_job":
-            self._last_started_id = job_name
-        elif event.event_name == "switch_job":
-            if user_code == self._current_user_code and self._last_started_id is not None:
-                if not job_name in self._job_switch_pairs[self._last_started_id].keys():
-                    self._job_switch_pairs[self._last_started_id][job_name] = []
+            self._job_switch_pairs[prev_job_name][job_name].append(user_code)
 
-                self._job_switch_pairs[self._last_started_id][job_name].append(user_code) # here, we take what we switched to, and append where we switched from
-
-            self._last_started_id = job_name
-
-        # once we process the event, we know we're looking at data for this event's user next time.
         self._current_user_code = user_code
 
     def _extractFromFeatureData(self, feature: FeatureData):
