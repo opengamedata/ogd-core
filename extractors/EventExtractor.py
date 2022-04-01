00522fe8cfc373e71f8203e2c9c0d788184ac53e
Luke Swanson <lwswanson2@wisc.edu>
2022-04-01 14:30:44 -0500

    Further work to prepare the EventExtractor class.

--- extractors/EventExtractor.py
+++ extractors/EventExtractor.py
@@ -1,9 +1,11 @@
 # import libraries
 from typing import Any, Dict, List, Type
 # import locals
+from detectors.DetectorRegistry import DetectorRegistry
 from extractors.Extractor import Extractor
 from features.FeatureData import FeatureData
 from features.FeatureLoader import FeatureLoader
+from games.LAKELAND.LakelandLoader import LakelandLoader
 from schemas.Event import Event
 from schemas.GameSchema import GameSchema
 from schemas.Request import ExporterTypes
@@ -11,7 +13,7 @@ from schemas.Request import ExporterTypes
 class EventExtractor(Extractor):
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
     def _getFeatureNames(self) -> List[str]:
-        return ["No Features for Event extractor"]
+        return self._registry.GetFeatureNames()
 
     ## Abstract declaration of a function to get the calculated value of the feature, given data seen so far.
     def _getFeatureValues(self, export_types:ExporterTypes) -> Dict[str,List[Any]]:
@@ -28,12 +30,18 @@ class EventExtractor(Extractor):
         pass
 
     def _prepareLoader(self) -> FeatureLoader:
-        pass
+        ret_val : FeatureLoader
+        if self._LoaderClass is LakelandLoader:
+            ret_val = LakelandLoader(player_id="events", session_id="events", game_schema=self._game_schema, feature_overrides=self._overrides, output_file=self._session_file)
+        else:
+            ret_val = self._LoaderClass(player_id="events", session_id="events", game_schema=self._game_schema, feature_overrides=self._overrides)
+        return ret_val
 
     # *** PUBLIC BUILT-INS ***
 
     def __init__(self, LoaderClass: Type[FeatureLoader], game_schema: GameSchema):
         super().__init__(LoaderClass=LoaderClass, game_schema=game_schema, feature_overrides=None)
+        self._registry = DetectorRegistry
 
     # *** PUBLIC STATICS ***
 
