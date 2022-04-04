3518bc6233074c431b86539a83c9d3bce754420b
LswaN58 <lwswanson2@wisc.edu>
2022-04-04 10:57:38 -0500

    Bring code organization into line with convention.

--- interfaces/DataInterface.py
+++ interfaces/DataInterface.py
@@ -5,12 +5,49 @@ from datetime import datetime
 from typing import Dict, List, Tuple, Union
 
 class DataInterface(abc.ABC):
+
+    # *** ABSTRACTS ***
+
+    @abc.abstractmethod
+    def _open(self) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def _close(self) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def _allIDs(self) -> List[str]:
+        pass
+
+    @abc.abstractmethod
+    def _fullDateRange(self) -> Dict[str,datetime]:
+        pass
+
+    @abc.abstractmethod
+    def _rowsFromIDs(self, id_list:List[str], versions:Union[List[int],None] = None) -> List[Tuple]:
+        pass
+
+    @abc.abstractmethod
+    def _IDsFromDates(self, min:datetime, max:datetime, versions:Union[List[int],None] = None) -> List[str]:
+        pass
+
+    @abc.abstractmethod
+    def _datesFromIDs(self, id_list:List[str], versions:Union[List[int],None] = None) -> Dict[str,datetime]:
+        pass
+
+    # *** PUBLIC BUILT-INS ***
+
     def __init__(self, game_id):
         self._game_id : str  = game_id
         self._is_open : bool = False
 
     def __del__(self):
         self.Close()
+
+    # *** PUBLIC STATICS ***
+
+    # *** PUBLIC METHODS ***
 
     def Open(self, force_reopen:bool = False) -> bool:
         if force_reopen or not self._is_open:
@@ -62,30 +99,6 @@ class DataInterface(abc.ABC):
         else:
             return self._datesFromIDs(id_list=id_list, versions=versions)
 
-    @abc.abstractmethod
-    def _open(self) -> bool:
-        pass
+    # *** PRIVATE STATICS ***
 
-    @abc.abstractmethod
-    def _close(self) -> bool:
-        pass
-
-    @abc.abstractmethod
-    def _allIDs(self) -> List[str]:
-        pass
-
-    @abc.abstractmethod
-    def _fullDateRange(self) -> Dict[str,datetime]:
-        pass
-
-    @abc.abstractmethod
-    def _rowsFromIDs(self, id_list:List[str], versions:Union[List[int],None] = None) -> List[Tuple]:
-        pass
-
-    @abc.abstractmethod
-    def _IDsFromDates(self, min:datetime, max:datetime, versions:Union[List[int],None] = None) -> List[str]:
-        pass
-
-    @abc.abstractmethod
-    def _datesFromIDs(self, id_list:List[str], versions:Union[List[int],None] = None) -> Dict[str,datetime]:
-        pass
+    # *** PRIVATE METHODS ***
