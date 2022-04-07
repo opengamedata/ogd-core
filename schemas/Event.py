dfbfc873db463ded3ba9822022708d1245a15351
LswaN58 <lwswanson2@wisc.edu>
2022-04-06 22:54:45 -0500

    Add source as element of an Event.

--- schemas/Event.py
+++ schemas/Event.py
@@ -1,13 +1,18 @@
 from datetime import date, datetime
+from enum import IntEnum
 from typing import Any, Dict, List, Union
 Map = Dict[str, Any] # type alias: we'll call any dict using string keys a "Map"
+
+class EventSource(IntEnum):
+    GAME = 1
+    GENERATED = 2
 
 ## @class Event
 #  Completely dumb struct that enforces a particular structure for the data we get from a source.
 #  Basically, whenever we fetch data, the TableSchema will be used to map columns to the required elements of an Event.
 #  Then the extractors etc. can just access columns in a direct manner.
 class Event:
-    def __init__(self, session_id:str, app_id:str,   timestamp:datetime, event_name:str, event_data:Map,
+    def __init__(self, session_id:str, app_id:str,   timestamp:datetime, event_name:str, event_data:Map, event_source:EventSource,
                  app_version:Union[str,None] = None, log_version:Union[str,None] = None, time_offset:Union[int,None] = None,
                  user_id:Union[str,None] = "",   user_data:Union[Map,None] = {},
                  game_state:Union[Map,None] = {}, event_sequence_index:Union[int,None] = None):
@@ -38,11 +43,13 @@ class Event:
         :param event_sequence_index: [description], defaults to None
         :type  event_sequence_index: Union[int,None], optional
         """
+        # TODO: event source, e.g. from game or from detector
         self.session_id           : str             = session_id
         self.app_id               : str             = app_id
         self.timestamp            : datetime        = timestamp
         self.event_name           : str             = event_name
         self.event_data           : Map             = event_data
+        self.event_source         : EventSource     = event_source
         self.app_version          : str             = app_version if app_version is not None else "0"
         self.log_version          : str             = log_version if log_version is not None else "0"
         self.time_offset          : Union[int,None] = time_offset
@@ -52,18 +59,19 @@ class Event:
         self.event_sequence_index : Union[int,None] = event_sequence_index
 
     def __str__(self):
-        return f"session_id  : {self.session_id}\n"\
-             + f"app_id      : {self.app_id}\n"\
-             + f"timestamp   : {self.timestamp}\n"\
-             + f"event_name  : {self.event_name}\n"\
-             + f"event_data  : {self.event_data}\n"\
-             + f"app_version : {self.app_version}\n"\
-             + f"log_version : {self.log_version}\n"\
-             + f"offset      : {self.time_offset}\n"\
-             + f"user_id     : {self.user_id}\n"\
-             + f"user_data   : {self.user_data}\n"\
-             + f"game_state  : {self.game_state}\n"\
-             + f"index       : {self.event_sequence_index}\n"\
+        return f"session_id   : {self.session_id}\n"\
+             + f"app_id       : {self.app_id}\n"\
+             + f"timestamp    : {self.timestamp}\n"\
+             + f"event_name   : {self.event_name}\n"\
+             + f"event_data   : {self.event_data}\n"\
+             + f"event_source : {self.event_source.name}\n"\
+             + f"app_version  : {self.app_version}\n"\
+             + f"log_version  : {self.log_version}\n"\
+             + f"offset       : {self.time_offset}\n"\
+             + f"user_id      : {self.user_id}\n"\
+             + f"user_data    : {self.user_data}\n"\
+             + f"game_state   : {self.game_state}\n"\
+             + f"index        : {self.event_sequence_index}\n"\
 
     @staticmethod
     def CompareVersions(a:str, b:str, version_separator='.'):
@@ -83,11 +91,13 @@ class Event:
 
     @staticmethod
     def ColumnNames() -> List[str]:
-        return ["session_id",  "app_id",      "timestamp", "event_name", "event_data",
-                "app_version", "log_version", "offset",    "user_id",    "user_data",
-                "game_state",  "index"]
+        return ["session_id", "app_id",       "timestamp",   "event_name",
+                "event_data", "event_source", "app_version", "log_version",
+                "offset",     "user_id",      "user_data",   "game_state",
+                "index"]
 
     def ColumnValues(self) -> List[Union[str, datetime, Map, int, None]]:
-        return [self.session_id,  self.app_id,      self.timestamp,   self.event_name,
-                self.event_data,  self.app_version, self.log_version, self.time_offset,
-                self.user_id,     self.user_data,   self.game_state,  self.event_sequence_index]
+        return [self.session_id,  self.app_id,        self.timestamp,   self.event_name,
+                self.event_data,  self.event_source,  self.app_version, self.log_version,
+                self.time_offset, self.user_id,       self.user_data,   self.game_state,
+                self.event_sequence_index]
