af03577b0cb7ff2772a967bae265180fb8ad5446
LswaN58 <lwswanson2@wisc.edu>
2022-04-08 14:16:56 -0500

    Further work to reorganize ExportManager's code, and to improve the debug logging.

--- managers/ExportManager.py
+++ managers/ExportManager.py
@@ -19,6 +19,7 @@ from utils import Logger
 from config.config import settings as default_settings
 from managers.FileManager import FileManager
 from managers.EventManager import EventManager
+from schemas.Event import Event
 from schemas.GameSchema import GameSchema
 from schemas.TableSchema import TableSchema
 from schemas.Request import Request
@@ -69,23 +70,23 @@ class ExportManager:
         start = datetime.now()
         try:
             Logger.Log(f"Preparing managers...", logging.DEBUG)
-            self._prepareManagers(request=request, game_schema=_game_schema, feature_overrides=request._feat_overrides)
+            self._setupManagers(request=request, game_schema=_game_schema, feature_overrides=request._feat_overrides)
             Logger.Log(f"Done", logging.DEBUG)
 
             if request.ToFile():
                 Logger.Log(f"File output requested, setting up file manager...", logging.DEBUG)
-                self._prepareFileManager(request=request)
+                self._setupFileManager(request=request)
                 Logger.Log(f"Done", logging.DEBUG)
 
             Logger.Log(f"Executing...", logging.DEBUG)
             _result = self._executeDataRequest(request=request, table_schema=_table_schema, file_manager=self._file_manager)
             Logger.Log(f"Done", logging.DEBUG)
-            num_sess : int = _result.get("sessions_ct", 0)
 
             Logger.Log(f"Saving output...", logging.DEBUG)
             if request.ToFile() and self._file_manager is not None:
                 # 4) Save and close files
-                self._closeFiles(game_schema=_game_schema, table_schema=_table_schema, num_sess=num_sess)
+                num_sess : int = _result.get("sessions_ct", 0)
+                self._teardownFileManager(game_schema=_game_schema, table_schema=_table_schema, num_sess=num_sess)
             if request.ToDict():
                 ret_val.update(_result) # merge event, session, player, and population data into the return value.
             Logger.Log(f"Done", logging.DEBUG)
@@ -111,7 +112,7 @@ class ExportManager:
             _table_name = default_settings["GAME_SOURCE_MAP"][_game_id]["table"]
         return TableSchema(schema_name=f"{_table_name}.json")
 
-    def _prepareManagers(self, request:Request, game_schema:GameSchema, feature_overrides:Union[List[str],None]):
+    def _setupManagers(self, request:Request, game_schema:GameSchema, feature_overrides:Union[List[str],None]):
         if request.ExportEvents():
             self._event_mgr = EventManager()
         # If game doesn't have an extractor, make sure we don't try to export it.
@@ -122,9 +123,9 @@ class ExportManager:
                 request._exports.sessions   = False
                 request._exports.players    = False
                 request._exports.population = False
-                Logger.Log("Could not extract feature data, no game extractor given!", logging.WARNING, depth=1)
+                Logger.Log("Could not set up feature extractors, no feature loader given!", logging.WARNING, depth=1)
 
-    def _prepareFileManager(self, request:Request):
+    def _setupFileManager(self, request:Request):
         _data_dir : str = self._settings["DATA_DIR"] or default_settings["DATA_DIR"]
         self._file_manager = FileManager(request=request, data_dir=_data_dir, extension="tsv")
         self._file_manager.OpenFiles()
@@ -153,7 +154,7 @@ class ExportManager:
 
     def _executeDataRequest(self, request:Request, table_schema:TableSchema, file_manager:Union[FileManager, None]=None) -> Dict[str,Any]:
         ret_val       : Dict[str,Any]           = {"events":None, "sessions":None, "players":None, "population":None, "sessions_ct":0}
-        next_data_set : Union[List[Tuple],None] = None
+        next_slice_data : Union[List[Tuple],None] = None
 
         if request.ToDict():
             if request.ExportEvents() and self._event_mgr is not None:
@@ -168,13 +169,13 @@ class ExportManager:
         # 1) Get the IDs of sessions to process
         sess_ids = request.RetrieveIDs() or []
         ret_val["sessions_ct"] = len(sess_ids)
-        Logger.Log(f"Preparing to process {len(sess_ids)} sessions.", logging.INFO, depth=1)
         _session_slices : List[List[str]] = self._generateSlices(sess_ids=sess_ids)
         # 2) Loop over and process the sessions, slice-by-slice (where each slice is a list of sessions).
-        for i, next_slice in enumerate(_session_slices):
-            next_data_set = self._getSlice(request=request, next_slice=next_slice, slice_num=i+1, slice_count=len(_session_slices))
-            if next_data_set is not None:
-                self._processSlice(next_data_set=next_data_set, table_schema=table_schema, sess_ids=sess_ids, slice_num=i+1, slice_count=len(_session_slices))
+        Logger.Log(f"Preparing to process {len(sess_ids)} sessions...", logging.INFO, depth=1)
+        for i, next_slice_ids in enumerate(_session_slices):
+            next_slice_data = self._getSlice(request=request, next_slice_ids=next_slice_ids, slice_num=i+1, slice_count=len(_session_slices))
+            if next_slice_data is not None:
+                self._processSlice(next_slice_data=next_slice_data, table_schema=table_schema, sess_ids=sess_ids, slice_num=i+1, slice_count=len(_session_slices))
                 # 2b) After processing all rows for each slice, write out the session data and reset for next slice.
                 if request.ExportEvents() and self._event_mgr is not None:
                     _events = self._event_mgr.GetLines()
@@ -198,6 +199,7 @@ class ExportManager:
                         if request.ToFile() and file_manager is not None:
                             file_manager.GetPlayersFile().writelines(["\t".join(player) + "\n" for player in _player_feats])
                         self._extract_mgr.ClearPlayerLines()
+        Logger.Log(f"Done", logging.INFO, depth=1)
         # 3) If we made it all the way to the end, write population data and return the number of sessions processed.
         if self._extract_mgr is not None:
             if request.ExportPopulation():
@@ -213,60 +215,63 @@ class ExportManager:
         _num_sess = len(sess_ids)
         #TODO: rewrite this to slice across players, instead of sessions.
         _slice_size = self._settings["BATCH_SIZE"] or default_settings["BATCH_SIZE"]
-        Logger.Log(f"With slice size = {_slice_size}, there are {math.ceil(_num_sess / _slice_size)} slices", logging.INFO)
+        Logger.Log(f"With slice size = {_slice_size}, there are {math.ceil(_num_sess / _slice_size)} slices", logging.INFO, depth=1)
         return [[sess_ids[i] for i in range( j*_slice_size, min((j+1)*_slice_size, _num_sess) )]
                              for j in range( 0, math.ceil(_num_sess / _slice_size) )]
 
-    def _getSlice(self, request:Request, next_slice:List[str], slice_num:int, slice_count:int) -> Union[List[Tuple], None]:
+    def _getSlice(self, request:Request, next_slice_ids:List[str], slice_num:int, slice_count:int) -> Union[List[Tuple], None]:
         start : datetime = datetime.now()
 
-        ret_val = request.GetInterface().RowsFromIDs(id_list=next_slice, id_mode=request.GetRange().GetIDMode())
+        ret_val = request.GetInterface().RowsFromIDs(id_list=next_slice_ids, id_mode=request.GetRange().GetIDMode())
         time_delta = datetime.now() - start
         if ret_val is not None:
             Logger.Log(f"Retrieval time for slice [{slice_num}/{slice_count}]: {time_delta} to get {len(ret_val)} events", logging.INFO, depth=2)
         else:
-            Logger.Log(f"Could not retrieve data set for slice [{slice_num}/{slice_count}].", logging.WARN)
+            Logger.Log(f"Could not retrieve data set for slice [{slice_num}/{slice_count}].", logging.WARN, depth=2)
         return ret_val
 
-    def _processSlice(self, next_data_set:List[Tuple], table_schema:TableSchema, sess_ids:List[str], slice_num:int, slice_count:int):
+    def _processSlice(self, next_slice_data:List[Tuple], table_schema:TableSchema, sess_ids:List[str], slice_num:int, slice_count:int):
         start      : datetime = datetime.now()
-        num_events : int      = len(next_data_set)
+        num_events : int      = len(next_slice_data)
         _unsessioned_event_count : int = 0
         # 3a) If next slice yielded valid data from the interface, process row-by-row.
-        for row in next_data_set:
+        for row in next_slice_data:
             try:
                 next_event = table_schema.RowToEvent(row)
             except Exception as err:
                 if default_settings.get("FAIL_FAST", None):
-                    Logger.Log(f"Error while converting row to Event\nFull error: {err}\nRow data: {pformat(row)}", logging.ERROR)
+                    Logger.Log(f"Error while converting row to Event\nFull error: {err}\nRow data: {pformat(row)}", logging.ERROR, depth=2)
                     raise err
                 else:
-                    Logger.Log(f"Error while converting row to Event. This row will be skipped.\nFull error: {err}", logging.WARNING)
+                    Logger.Log(f"Error while converting row to Event. This row will be skipped.\nFull error: {err}", logging.WARNING, depth=2)
             else:
                 if next_event.session_id in sess_ids:
-                    try:
-                        if self._event_mgr is not None:
-                            self._event_mgr.ProcessEvent(event=next_event)
-                        if self._extract_mgr is not None:
-                            self._extract_mgr.ProcessEvent(event=next_event)
-                    except Exception as err:
-                        if default_settings.get("FAIL_FAST", None):
-                            Logger.Log(f"Error while processing event {next_event}.", logging.ERROR)
-                            raise err
-                        else:
-                            Logger.Log(f"Error while processing event {next_event}. This event will be skipped. \nFull error: {err}", logging.WARNING)
+                    self._processEvent(next_event=next_event)
                 elif next_event.session_id is not None and next_event.session_id.upper() != "NONE":
-                    Logger.Log(f"Found a session ({next_event.session_id}) which was in the slice but not in the list of sessions for processing.", logging.WARNING)
+                    Logger.Log(f"Found a session ({next_event.session_id}) which was in the slice but not in the list of sessions for processing.", logging.WARNING, depth=2)
                 else:
                     _unsessioned_event_count += 1
                     if _unsessioned_event_count < 10:
-                        Logger.Log(f"Original data for an 'unsessioned' row: {row}", logging.WARNING)
+                        Logger.Log(f"Found an event with no session ID, original row data: {row}", logging.WARNING, depth=2)
         if _unsessioned_event_count > 0:
-            Logger.Log(f"Found {_unsessioned_event_count} events with no session IDs.", logging.WARNING)
+            Logger.Log(f"Found {_unsessioned_event_count} events without session IDs.", logging.WARNING, depth=2)
         time_delta = datetime.now() - start
-        Logger.Log(f"Processing time for slice [{slice_num}/{slice_count}]: {time_delta} to handle {num_events} events", logging.INFO)
+        Logger.Log(f"Processing time for slice [{slice_num}/{slice_count}]: {time_delta} to handle {num_events} events", logging.INFO, depth=2)
 
-    def _closeFiles(self, game_schema:GameSchema, table_schema:TableSchema, num_sess:int):
+    def _processEvent(self, next_event:Event):
+        try:
+            if self._event_mgr is not None:
+                self._event_mgr.ProcessEvent(event=next_event)
+            if self._extract_mgr is not None:
+                self._extract_mgr.ProcessEvent(event=next_event)
+        except Exception as err:
+            if default_settings.get("FAIL_FAST", None):
+                Logger.Log(f"Error while processing event {next_event}.", logging.ERROR, depth=2)
+                raise err
+            else:
+                Logger.Log(f"Error while processing event {next_event}. This event will be skipped. \nFull error: {err}", logging.WARNING, depth=2)
+
+    def _teardownFileManager(self, game_schema:GameSchema, table_schema:TableSchema, num_sess:int):
         _game_id = game_schema._game_name
         try:
             # before we zip stuff up, let's ensure the readme is in place:
