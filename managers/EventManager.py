070032cbb0f9bac937ac9ded1c5a727d22dad4db
LswaN58 <lwswanson2@wisc.edu>
2022-04-10 10:32:48 -0500

    Remove old ProcessRow function.

--- managers/EventManager.py
+++ managers/EventManager.py
@@ -10,48 +10,25 @@ from schemas.TableSchema import TableSchema
 ## @class EventProcessor
 #  Class to manage data for a csv events file.
 class EventManager:
-    ## Constructor for the EventProcessor class.
-    #  Stores some of the passed data, and generates some other members.
-    #  In particular, generates a mapping from column names back to indices of columns in the
-    #  events csv.
-    #  @param game_table    A data structure containing information on how the db
-    #                       table assiciated with the given game is structured. 
-    #  @param game_schema   A dictionary that defines how the game data itself
-    #                       is structured.
-    #  @param events_csv_file The output file, to which we'll write the event game data.
     def __init__(self):
+        """Constructor for EventManager.
+        Just creates empty list of lines and generates list of column names.
+        """
         # define instance vars
         self._lines        : List[str]      = []
         self._columns      : List[str]      = Event.ColumnNames()
 
-    ## Function to handle processing one row of data.
-    #  @param row_with_complex_parsed A tuple of the row data. We assume the
-    #                      event_data_complex has already been parsed from JSON.
-    def ProcessRow(self, row_with_complex_parsed: Tuple, schema:TableSchema, separator:str = "\t"):
-        row_columns = schema.ColumnNames()
-        line : List[Any] = [None] * len(row_columns)
-        for i,col in enumerate(row_with_complex_parsed):
-            # only set a value if this was not the remote address (IP) column.
-            if row_columns[i] != "remote_addr":
-                if type(col) == str:
-                    line[i] = f"\"{col}\""
-                elif type(col) == dict:
-                    line[i] = json.dumps(col)
-                else:
-                    line[i] = col
-        # print(f"From EventProcessor, about to add event to lines: {[str(item) for item in line]}")
-        self._lines.append(separator.join([str(item) for item in line]) + "\n") # changed , to \t
-
     def ProcessEvent(self, event:Event, separator:str = "\t") -> None:
         col_values = event.ColumnValues()
         for i,col in enumerate(col_values):
+            # TODO: double-check if the remote_addr is there to be dropped/ignored.
+            # if row_columns[i] != "remote_addr":
             if type(col) == str:
                 col_values[i] = f"\"{col}\""
             elif type(col) == dict:
                 col_values[i] = json.dumps(col)
         # event.event_data = json.dumps(event.event_data)
         self._lines.append(separator.join([str(item) for item in col_values]) + "\n") # changed , to \t
-        # Logger.Log(f"Got event: {str(event)}")
 
     def GetColumnNames(self) -> List[str]:
         return self._columns
