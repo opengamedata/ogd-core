66c485ed29e013c9d755ac31f0b15bcee38b62e8
LswaN58 <lwswanson2@wisc.edu>
2022-04-11 09:18:19 -0500

    If population export isn't requested, ExtractorManager should hold a PlayerExtractor as top-level extractor, etc.

--- managers/ExtractorManager.py
+++ managers/ExtractorManager.py
@@ -6,6 +6,8 @@ from typing import Any, Dict, List, Type, Union
 from utils import Logger
 from features.FeatureLoader import FeatureLoader
 from extractors.PopulationExtractor import PopulationExtractor
+from extractors.PlayerExtractor import PlayerExtractor
+from extractors.SessionExtractor import SessionExtractor
 from games.AQUALAB.AqualabLoader import AqualabLoader
 from games.CRYSTAL.CrystalLoader import CrystalLoader
 from games.JOWILDER.JowilderLoader import JowilderLoader
@@ -20,22 +22,36 @@ from schemas.Request import ExporterTypes
 
 class ExtractorManager:
     def __init__(self, game_id:str, exp_types:ExporterTypes, game_schema:GameSchema, feature_overrides:Union[List[str],None]):
-        self._exp_types      : ExporterTypes       = exp_types
-        self._latest_results : Dict[str,List[Any]] = {}
-        self._up_to_date     : bool                = True
-        self._LoaderClass    : Union[Type[FeatureLoader],None]
-        self._pop_extractor  : Union[PopulationExtractor, None] = None
+        self._exp_types        : ExporterTypes       = exp_types
+        self._latest_results   : Dict[str,List[Any]] = {}
+        self._up_to_date       : bool                = True
+        self._LoaderClass      : Union[Type[FeatureLoader],None]
+        self._pop_extractor    : Union[PopulationExtractor, None] = None
+        self._player_extractor : Union[PlayerExtractor, None]     = None
+        self._sess_extractor   : Union[SessionExtractor, None]    = None
 
         self._LoaderClass   = self._prepareLoaderClass(game_id=game_id)
         if self._LoaderClass is not None:
             if exp_types.population:
-                self._pop_extractor = PopulationExtractor(LoaderClass=self._LoaderClass, game_schema=game_schema, feature_overrides=feature_overrides)
+                self._pop_extractor = PopulationExtractor(LoaderClass=self._LoaderClass, game_schema=game_schema,
+                                                          feature_overrides=feature_overrides)
+            # TODO: see if there's a good way to get actual player/session id values here.
+            elif exp_types.players:
+                self._player_extractor = PlayerExtractor(LoaderClass=self._LoaderClass, game_schema=game_schema,
+                                                         player_id="Player", feature_overrides=feature_overrides)
+            elif exp_types.sessions:
+                self._sess_extractor = SessionExtractor(LoaderClass=self._LoaderClass, game_schema=game_schema,
+                                                         player_id="Player", session_id="Session", feature_overrides=feature_overrides)
         else:
             Logger.Log("Could not export population/session data, no feature loader given!", logging.WARNING, depth=1)
 
     def ProcessEvent(self, event:Event) -> None:
         if self._pop_extractor is not None:
             self._pop_extractor.ProcessEvent(event=event)
+        elif self._player_extractor is not None:
+            self._player_extractor.ProcessEvent(event=event)
+        elif self._sess_extractor is not None:
+            self._sess_extractor.ProcessEvent(event=event)
         self._up_to_date = False
 
     def HasLoader(self) -> bool:
@@ -45,6 +61,10 @@ class ExtractorManager:
         if not self._up_to_date:
             if self._pop_extractor is not None:
                 self._latest_results = self._pop_extractor.GetFeatureValues(export_types=self._exp_types, as_str=as_str)
+            elif self._player_extractor is not None:
+                self._latest_results = self._player_extractor.GetFeatureValues(export_types=self._exp_types, as_str=as_str)
+            elif self._sess_extractor is not None:
+                self._latest_results = self._sess_extractor.GetFeatureValues(export_types=self._exp_types, as_str=as_str)
             self._up_to_date = True
 
     def GetFeatureValues(self, as_str:bool = False):
@@ -62,7 +82,12 @@ class ExtractorManager:
         return ret_val
 
     def GetPlayerFeatureNames(self) -> List[str]:
-        return self._pop_extractor.GetPlayerFeatureNames() if self._pop_extractor is not None else []
+        if self._pop_extractor is not None:
+            return self._pop_extractor.GetPlayerFeatureNames()
+        elif self._player_extractor is not None:
+            return self._player_extractor.GetFeatureNames()
+        else:
+            return []
     def GetPlayerFeatures(self, slice_num:int, slice_count:int, as_str:bool = False) -> List[List[Any]]:
         start   : datetime = datetime.now()
         self._try_update(as_str=as_str)
@@ -72,7 +97,14 @@ class ExtractorManager:
         return ret_val
 
     def GetSessionFeatureNames(self) -> List[str]:
-        return self._pop_extractor.GetSessionFeatureNames() if self._pop_extractor is not None else []
+        if self._pop_extractor is not None:
+            return self._pop_extractor.GetSessionFeatureNames()
+        elif self._player_extractor is not None:
+            return self._player_extractor.GetSessionFeatureNames()
+        elif self._sess_extractor is not None:
+            return self._sess_extractor.GetFeatureNames()
+        else:
+            return []
     def GetSessionFeatures(self, slice_num:int, slice_count:int, as_str:bool = False) -> List[List[Any]]:
         start   : datetime = datetime.now()
         self._try_update(as_str=as_str)
@@ -88,10 +120,16 @@ class ExtractorManager:
     def ClearPlayerLines(self) -> None:
         if self._pop_extractor is not None:
             self._pop_extractor.ClearPlayersLines()
+        elif self._player_extractor is not None:
+            self._player_extractor.ClearLines()
 
     def ClearSessionLines(self) -> None:
         if self._pop_extractor is not None:
             self._pop_extractor.ClearSessionsLines()
+        elif self._player_extractor is not None:
+            self._player_extractor.ClearSessionsLines()
+        elif self._sess_extractor is not None:
+            self._sess_extractor.ClearLines()
 
     def _prepareLoaderClass(self, game_id:str) -> Union[Type[FeatureLoader],None]:
         _loader_class: Union[Type[FeatureLoader],None] = None
