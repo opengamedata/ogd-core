8346178cd711aa7073446c98ee31b584a3677d60
LswaN58 <lwswanson2@wisc.edu>
2022-04-11 08:02:17 -0500

    Don't check if connection is open in overridden functions, let that happen at higher level.

--- interfaces/BigQueryInterface.py
+++ interfaces/BigQueryInterface.py
@@ -90,160 +90,149 @@ class BigQueryInterface(DataInterface):
         return {'min':data[0][0], 'max':data[0][1]}
 
     def _rowsFromIDs(self, id_list:List[str], id_mode:IDMode=IDMode.SESSION, versions:Union[List[int],None] = None) -> List[Tuple]:
-        if self._client != None:
-            db_name    : str
-            table_name : str
-            if "BIGQUERY_CONFIG" in self._settings:
-                db_name = self._settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
-                table_name = self._settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
-            else:
-                db_name = default_settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
-                table_name = default_settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
-            if id_mode == IDMode.SESSION:
-                Logger.Log(f"Using session ID mode.", logging.DEBUG)
-                id_string = ','.join([f"{x}" for x in id_list])
-                where_clause = f"""
-                    WHERE param_session.key = 'ga_session_id' and param_session.value.int_value IN ({id_string})
-                    AND   param_version.key = 'app_version' AND param_version.value.double_value >= {AQUALAB_MIN_VERSION}
-                    AND   param_user.key = 'user_code'
-                """
-            elif id_mode == IDMode.PLAYER:
-                Logger.Log(f"Using player ID mode.", logging.DEBUG)
-                id_string = ','.join([f"'{x}'" for x in id_list])
-                where_clause = f"""
-                    WHERE param_session.key = 'ga_session_id'
-                    AND   param_version.key = 'app_version' AND param_version.value.double_value >= {AQUALAB_MIN_VERSION}
-                    AND   param_user.key = 'user_code' and param_user.value.string_value IN ({id_string})
-                """
-            else:
-                Logger.Log(f"Invalid ID mode given (val={id_mode}), defaulting to session mode.", logging.DEBUG)
-                id_string = ','.join([f"{x}" for x in id_list])
-                where_clause = f"""
-                    WHERE param_session.key = 'ga_session_id' and param_session.value.int_value IN ({id_string})
-                    AND   param_version.key = 'app_version' AND param_version.value.double_value >= {AQUALAB_MIN_VERSION}
-                    AND   param_user.key = 'user_code'
-                """
-            if self._game_id == "AQUALAB":
-                query = f"""
-                    SELECT event_name, event_params, device, geo, platform,
-                    param_session.value.int_value as session_id,
-                    concat(FORMAT_DATE('%Y-%m-%d', PARSE_DATE('%Y%m%d', event_date)), FORMAT_TIME('T%H:%M:%S.00', TIME(TIMESTAMP_MICROS(event_timestamp)))) AS timestamp,
-                    param_user.value.string_value as fd_user_id,
-                    FROM `{db_name}.{table_name}`
-                    CROSS JOIN UNNEST(event_params) AS param_session
-                    CROSS JOIN UNNEST(event_params) AS param_version
-                    CROSS JOIN UNNEST(event_params) AS param_user
-                    {where_clause}
-                    ORDER BY `session_id`, `timestamp` ASC
-                """
-            else:
-                query = f"""
-                    SELECT event_name, event_params, device, geo, platform, param_session.value.int_value AS session_id,
-                    concat(FORMAT_DATE('%Y-%m-%d', PARSE_DATE('%Y%m%d', event_date)), FORMAT_TIME('T%H:%M:%S.00', TIME(TIMESTAMP_MICROS(event_timestamp)))) AS timestamp,
-                    param_user.value.string_value as fd_user_id,
-                    FROM `{db_name}.{table_name}`
-                    CROSS JOIN UNNEST(event_params) AS param_session
-                    CROSS JOIN UNNEST(event_params) AS param_user
-                    WHERE param_session.key = 'ga_session_id' AND param_session.value.int_value IN ({id_string})
-                    AND   param_user.key = 'page_location'
-                    ORDER BY `session_id`, `timestamp` ASC
-                """
-            data = self._client.query(query)
-            events = []
-            for row in data:
-                items = tuple(row.items())
-                event = []
-                for item in items:
-                    if item[0] == "event_params":
-                        _params = {param['key']:param['value'] for param in item[1]}
-                        event.append(json.dumps(_params, sort_keys=True))
-                    elif item[0] in ["device", "geo"]:
-                        event.append(json.dumps(item[1], sort_keys=True))
-                    else:
-                        event.append(item[1])
-                events.append(tuple(event))
-            return events if events != None else []
+        db_name    : str
+        table_name : str
+        if "BIGQUERY_CONFIG" in self._settings:
+            db_name = self._settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
+            table_name = self._settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
         else:
-            Logger.Log(f"Could not get data for {len(id_list)} sessions, BigQuery connection is not open.", logging.WARN)
-            return []
+            db_name = default_settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
+            table_name = default_settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
+        if id_mode == IDMode.SESSION:
+            Logger.Log(f"Using session ID mode.", logging.DEBUG)
+            id_string = ','.join([f"{x}" for x in id_list])
+            where_clause = f"""
+                WHERE param_session.key = 'ga_session_id' and param_session.value.int_value IN ({id_string})
+                AND   param_version.key = 'app_version' AND param_version.value.double_value >= {AQUALAB_MIN_VERSION}
+                AND   param_user.key = 'user_code'
+            """
+        elif id_mode == IDMode.PLAYER:
+            Logger.Log(f"Using player ID mode.", logging.DEBUG)
+            id_string = ','.join([f"'{x}'" for x in id_list])
+            where_clause = f"""
+                WHERE param_session.key = 'ga_session_id'
+                AND   param_version.key = 'app_version' AND param_version.value.double_value >= {AQUALAB_MIN_VERSION}
+                AND   param_user.key = 'user_code' and param_user.value.string_value IN ({id_string})
+            """
+        else:
+            Logger.Log(f"Invalid ID mode given (val={id_mode}), defaulting to session mode.", logging.DEBUG)
+            id_string = ','.join([f"{x}" for x in id_list])
+            where_clause = f"""
+                WHERE param_session.key = 'ga_session_id' and param_session.value.int_value IN ({id_string})
+                AND   param_version.key = 'app_version' AND param_version.value.double_value >= {AQUALAB_MIN_VERSION}
+                AND   param_user.key = 'user_code'
+            """
+        if self._game_id == "AQUALAB":
+            query = f"""
+                SELECT event_name, event_params, device, geo, platform,
+                param_session.value.int_value as session_id,
+                concat(FORMAT_DATE('%Y-%m-%d', PARSE_DATE('%Y%m%d', event_date)), FORMAT_TIME('T%H:%M:%S.00', TIME(TIMESTAMP_MICROS(event_timestamp)))) AS timestamp,
+                param_user.value.string_value as fd_user_id,
+                FROM `{db_name}.{table_name}`
+                CROSS JOIN UNNEST(event_params) AS param_session
+                CROSS JOIN UNNEST(event_params) AS param_version
+                CROSS JOIN UNNEST(event_params) AS param_user
+                {where_clause}
+                ORDER BY `session_id`, `timestamp` ASC
+            """
+        else:
+            query = f"""
+                SELECT event_name, event_params, device, geo, platform, param_session.value.int_value AS session_id,
+                concat(FORMAT_DATE('%Y-%m-%d', PARSE_DATE('%Y%m%d', event_date)), FORMAT_TIME('T%H:%M:%S.00', TIME(TIMESTAMP_MICROS(event_timestamp)))) AS timestamp,
+                param_user.value.string_value as fd_user_id,
+                FROM `{db_name}.{table_name}`
+                CROSS JOIN UNNEST(event_params) AS param_session
+                CROSS JOIN UNNEST(event_params) AS param_user
+                WHERE param_session.key = 'ga_session_id' AND param_session.value.int_value IN ({id_string})
+                AND   param_user.key = 'page_location'
+                ORDER BY `session_id`, `timestamp` ASC
+            """
+        data = self._client.query(query)
+        events = []
+        for row in data:
+            items = tuple(row.items())
+            event = []
+            for item in items:
+                if item[0] == "event_params":
+                    _params = {param['key']:param['value'] for param in item[1]}
+                    event.append(json.dumps(_params, sort_keys=True))
+                elif item[0] in ["device", "geo"]:
+                    event.append(json.dumps(item[1], sort_keys=True))
+                else:
+                    event.append(item[1])
+            events.append(tuple(event))
+        return events if events != None else []
 
     def _IDsFromDates(self, min:datetime, max:datetime, versions:Union[List[int],None] = None) -> List[str]:
         ret_val = []
         str_min, str_max = min.strftime("%Y%m%d"), max.strftime("%Y%m%d")
-        if self._client != None:
-            db_name    : str
-            table_name : str
-            if "BIGQUERY_CONFIG" in self._settings:
-                db_name = self._settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
-                table_name = self._settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
-            else:
-                db_name = default_settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
-                table_name = default_settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
-            query = f"""
-                SELECT DISTINCT param.value.int_value AS session_id
-                FROM `{db_name}.{table_name}`,
-                UNNEST(event_params) AS param
-                WHERE param.key = "ga_session_id"
-                AND _TABLE_SUFFIX BETWEEN '{str_min}' AND '{str_max}'
-            """
-            data = self._client.query(query)
-            ids = [str(row['session_id']) for row in data]
-            if ids is not None:
-                ret_val = ids
+        db_name    : str
+        table_name : str
+        if "BIGQUERY_CONFIG" in self._settings:
+            db_name = self._settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
+            table_name = self._settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
         else:
-            Logger.Log(f"Could not get session list for {str_min}-{str_max} range, BigQuery connection is not open.", logging.WARN)
+            db_name = default_settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
+            table_name = default_settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
+        query = f"""
+            SELECT DISTINCT param.value.int_value AS session_id
+            FROM `{db_name}.{table_name}`,
+            UNNEST(event_params) AS param
+            WHERE param.key = "ga_session_id"
+            AND _TABLE_SUFFIX BETWEEN '{str_min}' AND '{str_max}'
+        """
+        data = self._client.query(query)
+        ids = [str(row['session_id']) for row in data]
+        if ids is not None:
+            ret_val = ids
         return ret_val
 
     def _datesFromIDs(self, id_list:List[str], id_mode:IDMode=IDMode.SESSION, versions:Union[List[int],None] = None) -> Dict[str, datetime]:
-        if self._client != None:
-            db_name    : str
-            table_name : str
-            if "BIGQUERY_CONFIG" in self._settings:
-                db_name = self._settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
-                table_name = self._settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
-            else:
-                db_name = default_settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
-                table_name = default_settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
-            if id_mode==IDMode.SESSION:
-                Logger.Log(f"Using session ID mode.", logging.DEBUG)
-                id_string = ','.join([f"{x}" for x in id_list])
-                where_clause = f"""
-                    WHERE param.key = "ga_session_id"
-                    AND param.value.int_value IN ({id_string})
-                """
-            elif id_mode==IDMode.PLAYER:
-                Logger.Log(f"Using player ID mode.", logging.DEBUG)
-                id_string = ','.join([f"'{x}'" for x in id_list])
-                where_clause = f"""
-                    WHERE param.key = "user_code"
-                    AND param.value.string_value IN ({id_string})
-                """
-            else:
-                Logger.Log(f"Invalid ID mode given (val={id_mode}), defaulting to session mode.", logging.DEBUG)
-                id_string = ','.join([f"{x}" for x in id_list])
-                where_clause = f"""
-                    WHERE param.key = "ga_session_id"
-                    AND param.value.int_value IN ({id_string})
-                """
-            query = f"""
-                WITH datetable AS
-                (
-                    SELECT event_date, event_timestamp, event_params,
-                    FORMAT_DATE('%m-%d-%Y', PARSE_DATE('%Y%m%d', event_date)) AS date, 
-                    FORMAT_TIME('%T', TIME(TIMESTAMP_MICROS(event_timestamp))) AS time,
-                    FROM `{db_name}.{table_name}`
-                )
-                SELECT MIN(concat(date, ' ', time)), MAX(concat(date, ' ', time))
-                FROM datetable,
-                UNNEST(event_params) AS param
-                {where_clause}
-            """
-            data = list(self._client.query(query))
-            # TODO: figure out what format to use to parse times from here.
-            return {'min':data[0][0], 'max':data[0][1]}
+        db_name    : str
+        table_name : str
+        if "BIGQUERY_CONFIG" in self._settings:
+            db_name = self._settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
+            table_name = self._settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
         else:
-            Logger.Log(f"Could not get date range for {len(id_list)} sessions, BigQuery connection is not open.", logging.WARN)
-            return {'min':datetime.now(), 'max':datetime.now()}
+            db_name = default_settings["BIGQUERY_CONFIG"][self._game_id]["DB_NAME"]
+            table_name = default_settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
+        if id_mode==IDMode.SESSION:
+            Logger.Log(f"Using session ID mode.", logging.DEBUG)
+            id_string = ','.join([f"{x}" for x in id_list])
+            where_clause = f"""
+                WHERE param.key = "ga_session_id"
+                AND param.value.int_value IN ({id_string})
+            """
+        elif id_mode==IDMode.PLAYER:
+            Logger.Log(f"Using player ID mode.", logging.DEBUG)
+            id_string = ','.join([f"'{x}'" for x in id_list])
+            where_clause = f"""
+                WHERE param.key = "user_code"
+                AND param.value.string_value IN ({id_string})
+            """
+        else:
+            Logger.Log(f"Invalid ID mode given (val={id_mode}), defaulting to session mode.", logging.DEBUG)
+            id_string = ','.join([f"{x}" for x in id_list])
+            where_clause = f"""
+                WHERE param.key = "ga_session_id"
+                AND param.value.int_value IN ({id_string})
+            """
+        query = f"""
+            WITH datetable AS
+            (
+                SELECT event_date, event_timestamp, event_params,
+                FORMAT_DATE('%m-%d-%Y', PARSE_DATE('%Y%m%d', event_date)) AS date, 
+                FORMAT_TIME('%T', TIME(TIMESTAMP_MICROS(event_timestamp))) AS time,
+                FROM `{db_name}.{table_name}`
+            )
+            SELECT MIN(concat(date, ' ', time)), MAX(concat(date, ' ', time))
+            FROM datetable,
+            UNNEST(event_params) AS param
+            {where_clause}
+        """
+        data = list(self._client.query(query))
+        # TODO: figure out what format to use to parse times from here.
+        return {'min':data[0][0], 'max':data[0][1]}
 
     # *** PUBLIC BUILT-INS ***
 
