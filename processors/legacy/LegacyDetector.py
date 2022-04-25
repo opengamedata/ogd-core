8a01d1ee2e7b1c15f2abb62d74e6809ab286404c
LswaN58 <lwswanson2@wisc.edu>
2022-04-25 12:41:28 -0500

    Bring LegacyDetector dummy class up to speed with current set of abstract functions.

--- processors/legacy/LegacyDetector.py
+++ processors/legacy/LegacyDetector.py
@@ -3,18 +3,15 @@ from datetime import datetime
 from typing import Any, List, Union
 # import local files
 from detectors.Detector import Detector
+from schemas.Event import EventSource
 from schemas.Event import Event
 
 class LegacyDetector(Detector):
     """Dummy version of a detector, so that LegacyLoader can return something that's not None.
-
-    :param Feature: Base class for a Custom Feature class.
-    :type Feature: _type_
     """
-    def __init__(self, name:str, description:str):
-        super().__init__(name=name, description=description, count_index=0)
 
-    # *** Implement abstract functions ***
+    # *** IMPLEMENT ABSTRACT FUNCTIONS ***
+
     def _getEventDependencies(self) -> List[str]:
         """_summary_
 
@@ -31,12 +28,20 @@ class LegacyDetector(Detector):
         """
         return
 
-    def _trigger(self) -> Union[Event, None]:
+    def _trigger_condition(self) -> bool:
+        return False
+
+    def _trigger_event(self) -> Union[Event, None]:
         """_summary_
 
         :return: _description_
         :rtype: List[Any]
         """
         ret_val : Event = Event(session_id="Not Implemented", app_id="Not Implemented", timestamp=datetime.now(),
-                                event_name="CustomDetector", event_data={})
+                                event_name="CustomDetector", event_data={}, event_source=EventSource.GENERATED)
         return ret_val
+
+    # *** PUBLIC BUILT-INS and FORMATTERS ***
+
+    def __init__(self, name:str, description:str):
+        super().__init__(name=name, description=description, count_index=0, trigger_callback=lambda x : None)
