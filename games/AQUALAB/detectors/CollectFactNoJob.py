21fba43a502ee666a5a5a2a634c505cf2c5feb65
LswaN58 <lwswanson2@wisc.edu>
2022-04-26 12:06:38 -0500

    Collect more event data in the detector.

--- games/AQUALAB/detectors/CollectFactNoJob.py
+++ games/AQUALAB/detectors/CollectFactNoJob.py
@@ -5,6 +5,7 @@ from typing import Callable, List
 from chardet import detect
 # import local files
 from detectors.Detector import Detector
+from detectors.DetectorEvent import DetectorEvent
 from schemas.Event import Event
 
 class CollectFactNoJob(Detector):
@@ -17,7 +18,11 @@ class CollectFactNoJob(Detector):
         super().__init__(name=name, description=description, count_index=0, trigger_callback=trigger_callback)
         self._found_jobless_fact = False
         self._sess_id = "Unknown"
+        self._player_id = "Unknown"
         self._time = datetime.now()
+        self._fact_id = None
+        self._app_version = "Unknown"
+        self._log_version = "Unknown"
 
     # *** Implement abstract functions ***
     def _getEventDependencies(self) -> List[str]:
@@ -36,8 +41,13 @@ class CollectFactNoJob(Detector):
         """
         if event.event_data['job_name'] == "no-active-job":
             self._sess_id = event.session_id
+            self._player_id = event.user_id
             self._time = event.timestamp
             self._found_jobless_fact = True
+            self._fact_id = event.event_data['fact_id']
+            self._app_version = event.app_version
+            self._log_version = event.log_version
+            self._sequence_index = event.event_sequence_index
         return
 
     def _trigger_condition(self) -> bool:
@@ -47,12 +57,14 @@ class CollectFactNoJob(Detector):
         else:
             return False
 
-    def _trigger_event(self) -> Event:
+    def _trigger_event(self) -> DetectorEvent:
         """_summary_
 
         :return: _description_
         :rtype: List[Any]
         """
-        ret_val : Event = Event(session_id=self._sess_id, app_id="AQUALAB", timestamp=self._time,
-                                event_name="CollectFactNoJob", event_data={})
+        ret_val : DetectorEvent = DetectorEvent(session_id=self._sess_id, app_id="AQUALAB", timestamp=self._time,
+                                                event_name="CollectFactNoJob", event_data={},
+                                                app_version=self._app_version, log_version=self._log_version,
+                                                user_id=self._player_id, event_sequence_index=self._sequence_index)
         return ret_val
