9a11e0050cd46d4126ef1fc9478dac9f8d28405b
LswaN58 <lwswanson2@wisc.edu>
2022-05-02 10:19:02 -0500

    Actually, enums won't need str functions.

--- schemas/IDMode.py
+++ schemas/IDMode.py
@@ -4,11 +4,3 @@ from enum import IntEnum
 class IDMode(IntEnum):
     SESSION = 1
     PLAYER = 2
-
-    def __str__(self):
-        if self.value == 1:
-            return "Session"
-        elif self.value == 2:
-            return "Player"
-        else:
-            raise ValueError("Invalid IDMode value!")
