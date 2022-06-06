261b1f8ea03eec42bdc6960e9f7c89fc8769b555
Luke Swanson <lwswanson2@wisc.edu>
2022-06-06 13:55:47 -0500

    Trying to fix some stuff in JobActiveTime.

--- games/AQUALAB/features/JobActiveTime.py
+++ games/AQUALAB/features/JobActiveTime.py
@@ -26,26 +26,34 @@ class JobActiveTime(PerJobFeature):
 
     def _extractFromEvent(self, event:Event) -> None:
         if event.SessionID != self._session_id:
+            _old_sess = self._session_id
             self._session_id = event.SessionID
-            # if we jumped to a new session, we only want to count time up to last event, then skip the time between sessions to new timestamp.
-            self._updateTotalTime()
-            self._last_start_time = event.timestamp
+            # if we jumped to a new session, we only want to count time up to last event, then skip the time between sessions to new timestamp;
+            # but only if we had a previous session, i.e. this isn't the first event seen.
+            if _old_sess is not None:
+                # Logger.Log(f"JobActiveTime attempting to update total time for {event.UserID} ({_old_sess} -> {self._session_id}) following change in session, index={event.EventSequenceIndex}", logging.INFO)
+                self._updateTotalTime()
+                # Logger.Log("Done", logging.INFO)
+                self._last_start_time = event.timestamp
+                if event.Timestamp is None:
+                    Logger.Log(f"JobActiveTime received an initial event with Timestamp == None!", logging.WARN)
 
         if event.EventName == "accept_job":
             self._last_start_time = event.timestamp
         elif event.EventName == "switch_job":
-            # if we switched into job, this becomes new start time
-            if self._job_map.get(event.EventData["job_name"]['string_value'], None) == self.CountIndex:
+            new_job = event.EventData["job_name"]['string_value']
+            old_job = event.EventData["prev_job_name"]["string_value"]
+            # if we switched into "this" job, this becomes new start time
+            if self._job_map.get(new_job, None) == self.CountIndex:
                 self._last_start_time = event.Timestamp
-            # if we switched out of job, update total time in the job.
-            elif self._job_map.get(event.EventData["prev_job_name"]["string_value"], None) == self.CountIndex:
+                if event.Timestamp is None:
+                    Logger.Log(f"JobActiveTime received a switch_job event with Timestamp == None!", logging.WARN)
+            # if we switched out of "this" job, update total time in the job.
+            # note, if "this" job is no-active-job, we don't care. Further, if we switched into no-active-job, then we just completed a job and don't care.
+            elif self._job_map.get(old_job, None) == self.CountIndex and new_job != "no-active-job" and old_job != "no-active-job":
                 self._updateTotalTime()
         elif event.EventName == "complete_job":
-            if self._last_start_time:
-                self._total_seconds += (event.timestamp - self._last_start_time).total_seconds()
-                self._last_start_time = None
-            else:
-                Logger.Log(f"{event.user_id} ({event.session_id}) completed job {event.event_data['job_name']['string_value']} with no active start time!", logging.DEBUG)
+            self._updateTotalTime()
 
         self._last_event_time = event.timestamp
 
@@ -58,14 +66,14 @@ class JobActiveTime(PerJobFeature):
     def _validateEventCountIndex(self, event:Event):
         ret_val : bool = False
 
-        job_data = event.EventData["job_name"]['string_value']
-        if self._job_map.get(job_data, None) is not None:
-            if self._job_map.get(job_data, None) == self.CountIndex:
+        new_job = event.EventData["job_name"]['string_value']
+        if self._job_map.get(new_job, None) is not None:
+            if self._job_map.get(new_job, None) == self.CountIndex:
                     ret_val = True
             elif event.EventName == "switch_job":
                 # if we got switch job, and were switching away from this instance's job, we still want to process it.
-                prev_job = event.EventData["prev_job_name"]["string_value"]
-                if self._job_map.get(prev_job, None) == self.CountIndex:
+                old_job = event.EventData["prev_job_name"]["string_value"]
+                if self._job_map.get(old_job, None) == self.CountIndex:
                     ret_val = True
         else:
             Logger.Log(f"Got invalid job_name data in {type(self).__name__}", logging.WARNING)
@@ -81,7 +89,8 @@ class JobActiveTime(PerJobFeature):
         if self._last_start_time:
             if self._last_event_time:
                 self._total_seconds += (self._last_event_time - self._last_start_time).total_seconds()
+                self._last_start_time = None
             else:
                 Logger.Log(f"JobActiveTime could not update total time, missing previous event time!", logging.WARNING)
         else:
-            Logger.Log(f"JobActiveTime could not update total time, missing start time!", logging.WARNING)
+            Logger.Log(f"JobActiveTime could not update total time for session {self._session_id}, missing start time!", logging.WARNING)
