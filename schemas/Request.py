5ad2b4f5910eeba426b036d70fcaa029516b3597
Luke Swanson <lwswanson2@wisc.edu>
2022-06-06 07:23:46 -0500

    Move RequestResult out to own file.

--- schemas/Request.py
+++ schemas/Request.py
@@ -1,8 +1,7 @@
 # import standard libraries
 import abc
-from datetime import datetime, timedelta
-from enum import Enum
-from typing import Any, Dict, List, Optional
+from datetime import datetime
+from typing import Dict, List, Optional
 # import local files
 from interfaces.DataInterface import DataInterface
 from schemas.IDMode import IDMode
@@ -54,98 +53,6 @@ class ExporterLocations:
     def __init__(self, files:bool = True, dict:bool = False):
         self.files = files
         self.dict  = dict
-
-class ResultStatus(Enum):
-    NONE = 1
-    SUCCESS = 2
-    FAILURE = 3
-
-class ExportResult:
-    def __init__(self, columns:List[str] = [], values:List[Any] = []):
-        self._columns = columns
-        self._values  = values
-
-    @property
-    def Columns(self) -> List[str]:
-        return self._columns
-    @Columns.setter
-    def Columns(self, new_columns:List[str]):
-        self._columns = new_columns
-
-    @property
-    def Values(self) -> List[Any]:
-        return self._values
-
-    def ToDict(self) -> Dict[str, List[Any]]:
-        return {
-            "cols":self._columns,
-            "vals":self._values
-        }
-
-    def AppendValues(self, new_values:List[Any]):
-        self._values += new_values
-
-class RequestResult:
-    def __init__(self, msg:str="", status:ResultStatus=ResultStatus.NONE,
-                 events:ExportResult  = ExportResult(), sessions:ExportResult   = ExportResult(),
-                 players:ExportResult = ExportResult(), population:ExportResult = ExportResult(),
-                 duration:timedelta=timedelta()):
-        self._message    = msg
-        self._status     = status
-        self._events     = events
-        self._sessions   = sessions
-        self._players    = players
-        self._population = population
-        self._duration   = duration
-
-    @property
-    def Status(self) -> ResultStatus:
-        return self._status
-
-    @property
-    def Message(self) -> str:
-        return self._message
-    
-    @property
-    def Events(self) -> ExportResult:
-        return self._events
-    
-    @property
-    def Sessions(self) -> ExportResult:
-        return self._sessions
-    
-    @property
-    def Players(self) -> ExportResult:
-        return self._players
-    
-    @property
-    def Population(self) -> ExportResult:
-        return self._population
-
-    @property
-    def ValuesDict(self) -> Dict[str, Dict[str, List[Any]]]:
-        return {
-            "population" : self.Population.ToDict(),
-            "players"    : self.Players.ToDict(),
-            "sessions"   : self.Sessions.ToDict(),
-            "events"     : self.Events.ToDict()
-        }
-
-    @property
-    def Duration(self) -> timedelta:
-        return self._duration
-    @Duration.setter
-    def Duration(self, new_duration):
-        self._duration = new_duration
-
-    def RequestSucceeded(self, msg:str, val:Any):
-        self._status = ResultStatus.SUCCESS
-        self._msg = msg
-        self._val = val
-
-    def RequestErrored(self, msg:str):
-        self._status = ResultStatus.FAILURE
-        self._msg = msg
 
 ## @class Request
 #  Dumb struct to hold data related to requests for data export.
