fae3e2f28f9c26dbdd6aaec00d34d437cd1a8f90
Luke Swanson <lwswanson2@wisc.edu>
2022-06-10 09:16:21 -0500

    Add Interface to interfaces module.

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -1,10 +1,12 @@
 __all__ = [
+    "Interface",
     "DataInterface",
 		"BigQueryInterface",
     "CSVInterface",
 		"MySQLInterface"
 ]
 
+from . import Interface
 from . import DataInterface
 from . import BigQueryInterface
 from . import CSVInterface
