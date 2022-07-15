04bbf4c5c1343d72e736555a99ed5dc7acad174f
Luke Swanson <lwswanson2@wisc.edu>
2022-07-15 12:11:55 -0500

    Finish implementing population mode for JobActiveTime.

--- games/AQUALAB/features/JobActiveTime.py
+++ games/AQUALAB/features/JobActiveTime.py
@@ -14,31 +14,43 @@ class JobActiveTime(PerJobFeature):
 
     def __init__(self, params:ExtractorParameters, job_map:dict):
         super().__init__(params=params, job_map=job_map)
-        self._session_id      = None
-        self._last_start_time = None
-        self._last_event_time = None
-        self._total_seconds = 0
+        self._total_seconds = timedelta(0)
+        if self.ExportMode == ExtractionMode.USER:
+            self._session_id      = None
+            self._last_start_time = None
+            self._last_event_time = None
+        elif self.ExportMode == ExtractionMode.POPULATION:
+            pass
+        else:
+            raise NotImplementedError(f"Got invalid export mode of {self.ExportMode.name} in JobActiveTime!")
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
     def _getEventDependencies(self) -> List[str]:
-        return ["all_events"]
+        if self.ExportMode == ExtractionMode.USER:
+            return ["all_events"]
+        else:
+            return []
 
     def _getFeatureDependencies(self) -> List[str]:
-        return []
+        if self.ExportMode == ExtractionMode.POPULATION:
+            return ["JobActiveTime"]
+        else:
+            return []
 
     def _extractFromEvent(self, event:Event) -> None:
         if self.ExportMode == ExtractionMode.USER:
             self._handle_user(event=event)
         elif self.ExportMode == ExtractionMode.POPULATION:
-            self._handle_population(event=event)
-        else:
-            raise NotImplementedError(f"Got invalid export mode of {self.ExportMode.name} in JobActiveTime!")
+            pass
 
     def _extractFromFeatureData(self, feature:FeatureData):
-        return
+        if self.ExportMode == ExtractionMode.USER:
+            pass
+        elif self.ExportMode == ExtractionMode.POPULATION:
+            self._handle_population(feature=feature)
 
     def _getFeatureValues(self) -> List[Any]:
-        return [timedelta(seconds=self._total_seconds)]
+        return [self._total_seconds]
 
     def _validateEventCountIndex(self, event:Event):
         ret_val : bool = False
@@ -103,51 +115,16 @@ class JobActiveTime(PerJobFeature):
     def _updateTotalTime(self):
         if self._last_start_time:
             if self._last_event_time:
-                self._total_seconds += (self._last_event_time - self._last_start_time).total_seconds()
+                self._total_seconds += (self._last_event_time - self._last_start_time)
                 self._last_start_time = None
             else:
                 Logger.Log(f"JobActiveTime could not update total time, missing previous event time!", logging.WARNING)
         else:
             Logger.Log(f"JobActiveTime could not update total time for session {self._session_id}, missing start time!", logging.WARNING)
 
-    def _handle_population(self, event:Event):
-        if event.SessionID != self._session_id:
-            _old_sess = self._session_id
-            self._session_id = event.SessionID
-            # if we jumped to a new session, we only want to count time up to last event, then skip the time between sessions to new timestamp;
-            # but only if we had a previous session, i.e. this isn't the first event seen.
-            if _old_sess is not None:
-                # Logger.Log(f"JobActiveTime attempting to update total time for {event.UserID} ({_old_sess} -> {self._session_id}) following change in session, index={event.EventSequenceIndex}", logging.INFO)
-                self._updateIndividualTime()
-                # Logger.Log("Done", logging.INFO)
-                self._last_start_time = event.timestamp
-                if event.Timestamp is None:
-                    Logger.Log(f"JobActiveTime received an initial event with Timestamp == None!", logging.WARN)
-
-        if event.EventName == "accept_job":
-            self._last_start_time = event.timestamp
-        elif event.EventName == "switch_job":
-            new_job = event.EventData["job_name"]['string_value']
-            old_job = event.EventData["prev_job_name"]["string_value"]
-            # if we switched into "this" job, this becomes new start time
-            if self._job_map.get(new_job, None) == self.CountIndex:
-                self._last_start_time = event.Timestamp
-                if event.Timestamp is None:
-                    Logger.Log(f"JobActiveTime received a switch_job event with Timestamp == None!", logging.WARN)
-            # if we switched out of "this" job, update total time in the job.
-            # note, if "this" job is no-active-job, we don't care. Further, if we switched into no-active-job, then we just completed a job and don't care.
-            elif self._job_map.get(old_job, None) == self.CountIndex and new_job != "no-active-job" and old_job != "no-active-job":
-                self._updateIndividualTime()
-        elif event.EventName == "complete_job":
-            self._updateIndividualTime()
-        self._last_event_time = event.timestamp
-
-    def _updateIndividualTime(self):
-        if self._last_start_time:
-            if self._last_event_time:
-                self._total_seconds += (self._last_event_time - self._last_start_time).total_seconds()
-                self._last_start_time = None
-            else:
-                Logger.Log(f"JobActiveTime could not update total time, missing previous event time!", logging.WARNING)
+    def _handle_population(self, feature:FeatureData):
+        _val = feature.FeatureValues[0]
+        if type(_val) == timedelta:
+            self._total_seconds += _val
         else:
-            Logger.Log(f"JobActiveTime could not update total time for session {self._session_id}, missing start time!", logging.WARNING)
+            Logger.Log(f"JobActiveTime for population got invalid value {_val} of type {type(_val)} for column {feature.FeatureNames[0]}")
