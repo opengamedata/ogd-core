e007e293df5bb5011bdb4c7667745229cad0140e
hahashuo <chenhaishuo1999@163.com>
2022-08-25 15:07:43 -0500

    Add logversion to be subfeature of gameversion

--- games/JOWILDER/features/GameVersion.py
+++ games/JOWILDER/features/GameVersion.py
@@ -15,6 +15,7 @@ class GameVersion(SessionFeature):
     def __init__(self, params:ExtractorParameters):
         super().__init__(params=params)
         self._version : Optional[int] = None
+        self._log_version: Optional[int] = None
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
 
@@ -26,12 +27,15 @@ class GameVersion(SessionFeature):
 
     def _extractFromEvent(self, event:Event) -> None:
         self._version = event.AppVersion
-
+        self._log_version = event.LogVersion
         return
 
     def _extractFromFeatureData(self, feature: FeatureData):
         return
 
     def _getFeatureValues(self) -> List[Any]:
-        return [self._version]
+        return [self._version, self._log_version]
+
+    def Subfeatures(self) -> List[str]:
+        return ["Log"]
 
