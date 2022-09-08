0e527faf8ff8294d649a29a4de4adb2a44ba144b
Luke Swanson <lwswanson2@wisc.edu>
2022-09-08 14:27:17 -0500

    Add a simple initial version of a builtin for counting an event of given type

---
+++ extractors/features/builtin/CountEvent.py
@@ -0,0 +1,76 @@
+# import libraries
+import json
+from typing import Any, List, Optional
+# import local files
+from extractors.features.Feature import Feature
+from schemas.FeatureData import FeatureData
+from schemas.Event import Event
+
+class CountEvent(Feature):
+    """Template file to serve as a guide for creating custom Feature subclasses for games.
+
+    :param Feature: Base class for a Custom Feature class.
+    :type Feature: _type_
+    """
+    def __init__(self, params:ExtractorParameters, schema_args:dict):
+        self._target_event = schema_args['target_event']
+        super().__init__(params=params)
+        self._count = 0
+
+    # *** IMPLEMENT ABSTRACT FUNCTIONS ***
+    def _getEventDependencies(self) -> List[str]:
+        """_summary_
+
+        :return: _description_
+        :rtype: List[str]
+        """
+        return [self._target_event]
+
+    def _getFeatureDependencies(self) -> List[str]:
+        """_summary_
+
+        :return: _description_
+        :rtype: List[str]
+        """
+        return []
+
+    def _extractFromEvent(self, event:Event) -> None:
+        """_summary_
+
+        :param event: _description_
+        :type event: Event
+        """
+        if event.EventType == self._target_event:
+            self._count += 1
+        return
+
+    def _extractFromFeatureData(self, feature: FeatureData):
+        """_summary_
+
+        :param feature: _description_
+        :type feature: FeatureData
+        """
+        return
+
+    def _getFeatureValues(self) -> List[Any]:
+        """_summary_
+
+        :return: _description_
+        :rtype: List[Any]
+        """
+        ret_val : List[Any] = [self._count]
+        return ret_val
+
+    # *** Optionally override public functions. ***
+    def Subfeatures(self) -> List[str]:
+        return [] # >>> fill in names of Subfeatures for which this Feature should extract values. <<<
+    
+    @staticmethod
+    def MinVersion() -> Optional[str]:
+        # >>> replace return statement below with a string defining the minimum logging version for events to be processed by this Feature. <<<
+        return super().MinVersion()
+
+    @staticmethod
+    def MaxVersion() -> Optional[str]:
+        # >>> replace return statement below with a string defining the maximum logging version for events to be processed by this Feature. <<<
+        return super().MaxVersion()
