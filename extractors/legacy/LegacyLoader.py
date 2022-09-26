5645a95c0443207ce72f1f2f75da2789399f3b6e
Luke Swanson <lwswanson2@wisc.edu>
2022-09-26 09:21:46 -0500

    Remove some unused code from LegacyLoader.

--- extractors/legacy/LegacyLoader.py
+++ extractors/legacy/LegacyLoader.py
@@ -9,20 +9,6 @@ class LegacyLoader(ExtractorLoader):
     def __init__(self, player_id:str, session_id:str, game_schema:GameSchema, mode:ExtractionMode, feature_overrides:Optional[List[str]]):
         super().__init__(player_id=player_id, session_id=session_id, game_schema=game_schema, mode=mode, feature_overrides=feature_overrides)
 
-    # def LoadToFeatureRegistry(self, registry:FeatureRegistry) -> None:
-    #     feat = self.LoadFeature(feature_type="", name="", schema_args={}, count_index=0)
-    #     # treat the monolithic LegacyFeature extractor as a single aggregate.
-    #     if feat is not None:
-    #         registry.Register(feat, IterationMode.AGGREGATE)
-
-    # def LoadToDetectorRegistry(self, registry:DetectorRegistry, trigger_callback:Callable[[Event], None]) -> None:
-    #     try:
-    #         feat = self.LoadDetector(detector_type="", name="", schema_args={}, trigger_callback=trigger_callback, count_index=0)
-    #         if feat is not None:
-    #             registry.Register(feat, IterationMode.AGGREGATE)
-    #     except NotImplementedError as err:
-    #         Logger.Log("No detectors to be loaded.", logging.INFO, depth=2)
-
     def _validateMode(self, feature_type) -> bool:
         """Overridden version of _validateMode.
         For LegacyLoader, we never care to check modes, since feature is a monolith and should just go for all versions.
