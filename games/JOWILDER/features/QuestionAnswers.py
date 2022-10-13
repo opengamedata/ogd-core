dcf0808f4b1de21c52ca26792955d6e45982a3b9
Luke Swanson <lwswanson2@wisc.edu>
2022-10-13 11:20:04 -0400

    Bring JOWILDER stuff from miniextractors into master.

---
+++ games/JOWILDER/features/QuestionAnswers.py
@@ -0,0 +1,62 @@
+# import libraries
+from schemas import Event
+from typing import Any, List, Optional
+# import locals
+from schemas.FeatureData import FeatureData
+from extractors.features.PerCountFeature import PerCountFeature
+from extractors.Extractor import ExtractorParameters
+from schemas.Event import Event
+from .. import Jowilder_Enumerators as je
+
+
+
+# NOTE: Assumption is that in question 10 and qustion 16, we will have hover events which indicates new answer
+
+class QuestionAnswers(PerCountFeature):
+    def __init__(self, params: ExtractorParameters):
+        PerCountFeature.__init__(self, params=params)
+        self._guess_list : List[str] = []
+        self.cur_question : Optional[int] = None
+        self.chosen_answer : Optional[int] = None
+
+    # *** IMPLEMENT ABSTRACT FUNCTIONS ***
+    def _validateEventCountIndex(self, event: Event):
+        if event.EventName == "CUSTOM.20":
+            return self.cur_question == self.CountIndex and self.cur_question in [10, 16]
+        if Event.CompareVersions(event.LogVersion, "6") >= 0 and event.EventData.get("cur_cmd_type") == 2:
+                self.cur_question = je.answer_to_question(event.EventData.get("cur_cmd_fqid"), event.EventData["level"])
+        return self.cur_question==self.CountIndex
+    
+    def _getEventDependencies(self) -> List[str]:
+        return ["CUSTOM.11", "CUSTOM.20"]
+        # ["CUSTOM.11", "CUSTOM.20"] = ["wildcard_clicks", "wildcard_hover"]
+
+    def _getFeatureDependencies(self) -> List[str]:
+        return []
+
+    def _extractFromEvent(self, event: Event) -> None:
+        # XXX: Need a new feature as dependency to become independent from wildcard_hover event. For now, it is weird that we need to seperate two kinds of events
+        # NOTE: self.chosen_answer's change: None ->interacted_fqid(click and type==2) -> interacted_fqid(hover, optional step) -> none(click and type==1, that's where we update numguesses if chosen_answer is not None)
+        # NOTE: self.cur_question's function: 0 -> cur_cmd_fqid&level (cmd_type==2) -> if in [10, 16] in a wildcard_hover event, update chosen answer
+        if event.event_name == "CUSTOM.20" and self.chosen_answer and event.EventData.get("cur_cmd_fqid"):
+            self.chosen_answer = event.EventData.get("interacted_fqid")
+            return
+        if Event.CompareVersions(event.LogVersion, "6") >= 0: 
+            if event.EventData.get("cur_cmd_type") == 2:
+                self.cur_question = je.answer_to_question(
+                    event.EventData.get("cur_cmd_fqid"), event.EventData["level"])
+                self.chosen_answer = event.EventData.get("interacted_fqid")
+            elif event.EventData.get("cur_cmd_type") == 1:
+                if self.chosen_answer:
+                    self._guess_list.append(self.chosen_answer)
+                    self.chosen_answer = None
+        return 
+
+    def _extractFromFeatureData(self, feature: FeatureData):
+        return
+
+    def _getFeatureValues(self) -> List[Any]:
+        return [self._guess_list, len(self._guess_list)]
+
+    def Subfeatures(self) -> List[str]:
+        return ["Count"]
---
+++ games/JOWILDER/features/UserEnabled.py
@@ -0,0 +1,63 @@
+# import libraries
+from typing import Any, List, Optional
+from extractors.Extractor import ExtractorParameters
+# import local files
+from extractors.features.SessionFeature import SessionFeature
+from schemas.FeatureData import FeatureData
+from schemas.Event import Event
+
+class UserEnabled(SessionFeature):
+    """Template file to serve as a guide for creating custom Feature subclasses for games.
+
+    :param Feature: Base class for a Custom Feature class.
+    :type Feature: _type_
+    """
+    def __init__(self, params:ExtractorParameters):
+        super().__init__(params=params)
+        self._full_screen : Optional[int] = None
+        self._music : Optional[int] = None
+        self._hq : Optional[int] = None
+
+    # *** IMPLEMENT ABSTRACT FUNCTIONS ***
+
+    def _getEventDependencies(self) -> List[str]:
+        return ["CUSTOM.1"]
+
+    def _getFeatureDependencies(self) -> List[str]:
+        return [] 
+
+    def _extractFromEvent(self, event:Event) -> None:
+        _data = event.EventData
+        true_values = ["1", "true"]
+        for item in ["fullscreen", "music", "hq"]:
+            if _data.get(item) is None:
+                raise(ValueError(f"Can't find {item} item in the event data!"))
+        if str(_data.get("fullscreen")).lower() in true_values:
+            self._full_screen = 1
+        else:
+            self._full_screen = 0
+
+        if str(_data.get("music")).lower() in true_values:
+            self._music = 1
+        else:
+            self._music = 0
+
+        if str(_data.get("hq")).lower() in true_values:
+            self._hq = 1
+        else:
+            self._hq = 0
+
+        return
+
+    def _extractFromFeatureData(self, feature: FeatureData):
+        return
+
+    def _getFeatureValues(self) -> List[Any]:
+        return [self._full_screen, self._music, self._hq]
+
+    # *** Optionally override public functions. ***
+    def Subfeatures(self) -> List[str]:
+        return ["Music", "HQGraphics"] 
+
+    def BaseFeatureSuffix(self) -> str:
+        return "FullScreen"
