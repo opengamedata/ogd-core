e32909d6d9d891a2676313d577f21fea971f2faa
hahashuo <chenhaishuo1999@163.com>
2022-10-13 22:25:47 -0500

    Add SceneChangedFrequently detector

--- games/AQUALAB/detectors/__init__.py
+++ games/AQUALAB/detectors/__init__.py
@@ -2,10 +2,12 @@ __all__ = [
     "CollectFactNoJob",
     "DiveSiteNoEvidence",
     "EchoRoomChange",
-    "Idle"
+    "Idle",
+    "SceneChangeFrequently"
 ]
 
 from . import CollectFactNoJob
 from . import DiveSiteNoEvidence
 from . import EchoRoomChange
 from . import Idle
+from . import SceneChangeFrequently
