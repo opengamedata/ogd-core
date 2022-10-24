00639f0ca89aedc4b0ffd07732e0530251284a72
Luke Swanson <swansonl@cs.wisc.edu>
2022-10-24 10:47:23 -0500

    Add whatever work I'd done on unit testing.

--- TestDriver.py
+++ TestDriver.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
+import utils
 from tests.t_interfaces.t_CSVInterface import t_CSVInterface
 from tests.t_utils import t_utils
 
 test_utils = t_utils()
 test_utils.RunAll()
-test_CSVInterface = t_CSVInterface()
-test_CSVInterface.RunAll()
+test_config = utils.loadJSONFile(filename="t_config.json", path=Path("./tests"))
+if test_config.get("interfaces", False):
+    test_CSVInterface = t_CSVInterface()
+    test_CSVInterface.RunAll()
