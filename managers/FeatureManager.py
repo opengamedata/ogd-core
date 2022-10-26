52cfd8894728a0f6bb09b41ea75844d8dbfe04b4
Luke Swanson <lwswanson2@wisc.edu>
2022-10-26 09:57:59 -0500

    Oops, never hooked up code to check if a particular export type was requested or not.

--- managers/FeatureManager.py
+++ managers/FeatureManager.py
@@ -42,55 +42,61 @@ class FeatureManager:
     # TODO: make this function take list of events, and do the loop over events as low in the hierarchy as possible, which technically should be faster.
     def ProcessEvent(self, event:Event) -> None:
         # 1. process at population level.
-        self._population.ProcessEvent(event=event)
+        if ExportMode.POPULATION in self._exp_types:
+            self._population.ProcessEvent(event=event)
         # 2. process at player level, adding player if needed.
         _player_id = event.UserID or "null"
-        if _player_id not in self._players.keys():
-            self._players[_player_id] = PlayerProcessor(LoaderClass=self._LoaderClass, game_schema=self._game_schema,
-                                                        player_id=_player_id,          feature_overrides=self._overrides)
-        if _player_id not in self._sessions.keys():
-            self._sessions[_player_id] = {}
-            self._used_null_sess[_player_id] = False
-        self._players[_player_id].ProcessEvent(event=event)
-        if _player_id == "null":
-            self._used_null_play = True
+        if ExportMode.PLAYER in self._exp_types:
+            if _player_id not in self._players.keys():
+                self._players[_player_id] = PlayerProcessor(LoaderClass=self._LoaderClass, game_schema=self._game_schema,
+                                                            player_id=_player_id,          feature_overrides=self._overrides)
+            if _player_id not in self._sessions.keys():
+                self._sessions[_player_id] = {}
+                self._used_null_sess[_player_id] = False
+            self._players[_player_id].ProcessEvent(event=event)
+            if _player_id == "null":
+                self._used_null_play = True
         # 3. process at session level, adding session if needed.
-        if event.SessionID not in self._sessions[_player_id].keys():
-            self._sessions[_player_id][event.SessionID] = SessionProcessor(LoaderClass=self._LoaderClass, game_schema=self._game_schema,
-                                                                player_id=_player_id,          session_id=event.SessionID,    feature_overrides=self._overrides)
-        self._sessions[_player_id][event.SessionID].ProcessEvent(event=event)
-        if event.SessionID == None or event.SessionID.upper() == "NULL":
-            self._used_null_sess[_player_id] = True
+        if ExportMode.SESSION in self._exp_types:
+            if event.SessionID not in self._sessions[_player_id].keys():
+                self._sessions[_player_id][event.SessionID] = SessionProcessor(LoaderClass=self._LoaderClass, game_schema=self._game_schema,
+                                                                    player_id=_player_id,          session_id=event.SessionID,    feature_overrides=self._overrides)
+            self._sessions[_player_id][event.SessionID].ProcessEvent(event=event)
+            if event.SessionID == None or event.SessionID.upper() == "NULL":
+                self._used_null_sess[_player_id] = True
         self._up_to_date = False
 
     def ProcessFeatureData(self) -> None:
-        # 1. Get population 1st-order data
-        pop_data = self._population.GetFeatureData(order=1)
-        # 2. Distribute population 1st-order data
-        self._population.ProcessFeatureData(feature_list=pop_data)
-        for player in self._players.values():
-            player.ProcessFeatureData(feature_list=pop_data)
-        for session_list in self._sessions.values():
-            for session in session_list.values():
-                session.ProcessFeatureData(feature_list=pop_data)
-        # 3. For each player, get 1st-order data
-        for player_name,player in self._players.items():
-            play_data = player.GetFeatureData(order=1)
-            # 4. Distribute player 1st-order data
-            self._population.ProcessFeatureData(feature_list=play_data)
-            player.ProcessFeatureData(feature_list=play_data)
-            for session in self._sessions.get(player_name, {}).values():
-                session.ProcessFeatureData(feature_list=play_data)
-        # 5. For each session, get 1st-order data
-        for session_list in self._sessions.values():
-            for session in session_list.values():
-                sess_data = session.GetFeatureData(order=1)
-                # 6. Distribute session 1st-order data
-                self._population.ProcessFeatureData(feature_list=sess_data)
-                player = self._players.get(session._playerID, None)
-                if player is not None:
-                    player.ProcessFeatureData(feature_list=sess_data)
-                session.ProcessFeatureData(feature_list=sess_data)
+        if ExportMode.POPULATION in self._exp_types:
+            # 1. Get population 1st-order data
+            pop_data = self._population.GetFeatureData(order=1)
+            # 2. Distribute population 1st-order data
+            self._population.ProcessFeatureData(feature_list=pop_data)
+            for player in self._players.values():
+                player.ProcessFeatureData(feature_list=pop_data)
+            for session_list in self._sessions.values():
+                for session in session_list.values():
+                    session.ProcessFeatureData(feature_list=pop_data)
+        if ExportMode.PLAYER in self._exp_types:
+            # 3. For each player, get 1st-order data
+            for player_name,player in self._players.items():
+                play_data = player.GetFeatureData(order=1)
+                # 4. Distribute player 1st-order data
+                self._population.ProcessFeatureData(feature_list=play_data)
+                player.ProcessFeatureData(feature_list=play_data)
+                for session in self._sessions.get(player_name, {}).values():
+                    session.ProcessFeatureData(feature_list=play_data)
+        if ExportMode.SESSION in self._exp_types:
+            # 5. For each session, get 1st-order data
+            for session_list in self._sessions.values():
+                for session in session_list.values():
+                    sess_data = session.GetFeatureData(order=1)
+                    # 6. Distribute session 1st-order data
+                    self._population.ProcessFeatureData(feature_list=sess_data)
+                    player = self._players.get(session._playerID, None)
+                    if player is not None:
+                        player.ProcessFeatureData(feature_list=sess_data)
+                    session.ProcessFeatureData(feature_list=sess_data)
 
     def GetFeatureValues(self, as_str:bool = False) -> Dict[str, List[ExportRow]]:
         start = datetime.now()
@@ -99,32 +105,50 @@ class FeatureManager:
         return self._latest_values
 
     def GetPopulationFeatureNames(self) -> List[str]:
-        return self._population.GetExtractorNames()
+        if ExportMode.POPULATION in self._exp_types:
+            return self._population.GetExtractorNames()
+        else:
+            return []
     def GetPopulationFeatures(self, as_str:bool = False) -> List[ExportRow]:
-        start = datetime.now()
-        self._try_update(as_str=as_str)
-        ret_val = self._latest_values.get('population', [])
-        Logger.Log(f"Time to retrieve Population lines: {datetime.now() - start} to get {len(ret_val)} lines", logging.INFO, depth=2)
-        return ret_val
+        if ExportMode.POPULATION in self._exp_types:
+            start = datetime.now()
+            self._try_update(as_str=as_str)
+            ret_val = self._latest_values.get('population', [])
+            Logger.Log(f"Time to retrieve Population lines: {datetime.now() - start} to get {len(ret_val)} lines", logging.INFO, depth=2)
+            return ret_val
+        else:
+            return []
 
     def GetPlayerFeatureNames(self) -> List[str]:
-        return self._players["null"].GetExtractorNames()
+        if ExportMode.PLAYER in self._exp_types:
+            return self._players["null"].GetExtractorNames()
+        else:
+            return []
     def GetPlayerFeatures(self, slice_num:int, slice_count:int, as_str:bool = False) -> List[ExportRow]:
-        start   : datetime = datetime.now()
-        self._try_update(as_str=as_str)
-        ret_val = self._latest_values.get('players', [])
-        Logger.Log(f"Time to retrieve Player lines for slice [{slice_num}/{slice_count}]: {datetime.now() - start} to get {len(ret_val)} lines", logging.INFO, depth=2)
-        return ret_val
+        if ExportMode.PLAYER in self._exp_types:
+            start   : datetime = datetime.now()
+            self._try_update(as_str=as_str)
+            ret_val = self._latest_values.get('players', [])
+            Logger.Log(f"Time to retrieve Player lines for slice [{slice_num}/{slice_count}]: {datetime.now() - start} to get {len(ret_val)} lines", logging.INFO, depth=2)
+            return ret_val
+        else:
+            return []
 
     def GetSessionFeatureNames(self) -> List[str]:
-        return self._sessions["null"]["null"].GetExtractorNames()
+        if ExportMode.SESSION in self._exp_types:
+            return self._sessions["null"]["null"].GetExtractorNames()
+        else:
+            return []
     def GetSessionFeatures(self, slice_num:int, slice_count:int, as_str:bool = False) -> List[ExportRow]:
-        start   : datetime = datetime.now()
-        self._try_update(as_str=as_str)
-        ret_val = self._latest_values.get('sessions', [])
-        time_delta = datetime.now() - start
-        Logger.Log(f"Time to retrieve Session lines for slice [{slice_num}/{slice_count}]: {time_delta} to get {len(ret_val)} lines", logging.INFO, depth=2)
-        return ret_val
+        if ExportMode.SESSION in self._exp_types:
+            start   : datetime = datetime.now()
+            self._try_update(as_str=as_str)
+            ret_val = self._latest_values.get('sessions', [])
+            time_delta = datetime.now() - start
+            Logger.Log(f"Time to retrieve Session lines for slice [{slice_num}/{slice_count}]: {time_delta} to get {len(ret_val)} lines", logging.INFO, depth=2)
+            return ret_val
+        else:
+            return []
 
     def ClearPopulationLines(self) -> None:
         self._population.ClearLines()
