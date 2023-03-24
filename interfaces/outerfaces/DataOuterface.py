daca278a3d4e2307fa7e0f2346b7988652dc676d
Luke Swanson <lwswanson2@wisc.edu>
2023-03-24 15:55:23 -0500

    Include a message when an export mode is removed.

--- interfaces/outerfaces/DataOuterface.py
+++ interfaces/outerfaces/DataOuterface.py
@@ -71,6 +71,7 @@ class DataOuterface(Interface):
 
     def RemoveExportMode(self, mode:ExportMode):
         self._removeExportMode(mode)
+        Logger.Log(f"Removed mode {mode} from output.", logging.INFO)
 
     def WriteEventHeader(self, header:List[str]) -> None:
         self._writeEventsHeader(header=header)
