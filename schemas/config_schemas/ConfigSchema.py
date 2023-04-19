eb2796bbd3e44ab8d465cb42cf316e53d3b8c33a
Luke Swanson <lwswanson2@wisc.edu>
2023-04-19 15:18:30 -0500

    Finish setting up schema for base config file.

--- schemas/config_schemas/ConfigSchema.py
+++ schemas/config_schemas/ConfigSchema.py
@@ -3,79 +3,12 @@ import logging
 from pathlib import Path
 from typing import Any, Dict, Optional
 # import local files
+from schemas.config_schemas.IndexingSchema import FileIndexingSchema
+from schemas.config_schemas.SSHSchema import SSHSchema
 from schemas.config_schemas.GameSourceSchema import GameSourceSchema
 from schemas.config_schemas.DataSourceSchema import DataSourceSchema
 from schemas.Schema import Schema
 from utils import Logger
-
-class FileIndexingSchema(Schema):
-    def __init__(self, name:str, all_elements:Dict[str, Any]):
-        self._local_dir     : Path
-        self._remote_url    : Optional[str]
-        self._templates_url : str
-
-        if not isinstance(all_elements, dict):
-            all_elements = {}
-            Logger.Log(f"For {name} base config, all_elements was not a dict, defaulting to empty dict", logging.WARN)
-        if "LOCAL_DIR" in all_elements.keys():
-            self._local_dir = FileIndexingSchema._parseLocalDir(all_elements["LOCAL_DIR"])
-        else:
-            self._local_dir = Path("./data/")
-            Logger.Log(f"{name} config does not have a 'LOCAL_DIR' element; defaulting to local_dir={self._local_dir}", logging.WARN)
-        if "REMOTE_URL" in all_elements.keys():
-            self._remote_url = FileIndexingSchema._parseRemoteURL(all_elements["REMOTE_URL"])
-        else:
-            self._remote_url = None
-            Logger.Log(f"{name} config does not have a 'REMOTE_URL' element; defaulting to remote_url={self._remote_url}", logging.WARN)
-        if "TEMPLATES_URL" in all_elements.keys():
-            self._templates_url = FileIndexingSchema._parseTemplatesURL(all_elements["TEMPLATES_URL"])
-        else:
-            self._templates_url = "https://github.com/opengamedata/opengamedata-samples"
-            Logger.Log(f"{name} config does not have a 'TEMPLATES_URL' element; defaulting to templates_url={self._templates_url}", logging.WARN)
-
-        _leftovers = { key : val for key,val in all_elements.items() if key not in {"schema", "source", "table", "credential"} }
-        super().__init__(name=name, other_elements=_leftovers)
-
-    @property
-    def LocalDirectory(self) -> Path:
-        return self._local_dir
-
-    @property
-    def RemoteURL(self) -> str:
-        return self._remote_url
-
-    @property
-    def TemplatesURL(self) -> str:
-        return self._templates_url
-
-    @property
-    def AsMarkdown(self) -> str:
-        ret_val : str
-
-        ret_val = f"{self.Name} : Local=_{self.LocalDirectory}_, Remote=_{self.RemoteURL}_"
-        return ret_val
-
-    @staticmethod
-    def _parseLocalDir(dir) -> Path:
-        ret_val : Path
-        if isinstance(dir, Path):
-            ret_val = dir
-        elif isinstance(dir, str):
-            ret_val = Path(dir)
-        else:
-            ret_val = Path(str(dir))
-            Logger.Log(f"File Indexing local data directory was unexpected type {type(dir)}, defaulting to Path(str(dir))={ret_val}.", logging.WARN)
-        return ret_val
-
-    @staticmethod
-    def _parseLogFile(use_log) -> bool:
-        ret_val : bool
-        if isinstance(use_log, bool):
-            ret_val = use_log
-        else:
-            ret_val = False
-            Logger.Log(f"Config to use log file was unexpected type {type(use_log)}, defaulting to False.", logging.WARN)
-        return ret_val
 
 class ConfigSchema(Schema):
     def __init__(self, name:str, all_elements:Dict[str, Any]):
@@ -84,8 +17,8 @@ class ConfigSchema(Schema):
         self._batch_size : int
         self._dbg_level  : int
         self._fail_fast  : bool
-        self._file_idx   : Dict[str, Path | str]
-        self._ssh_config : Dict[str, str | int]
+        self._file_idx   : FileIndexingSchema
+        self._ssh_config : SSHSchema
         self._data_src   : Dict[str, DataSourceSchema]
         self._game_src_map : Dict[str, GameSourceSchema]
 
@@ -96,7 +29,8 @@ class ConfigSchema(Schema):
             self._data_dir = ConfigSchema._parseDataDir(all_elements["DATA_DIR"])
         else:
             self._data_dir = Path("./data/")
-            Logger.Log(f"{name} config does not have a 'DATA_DIR' element; defaulting to data_dir={self._data_dir}", logging.WARN)
+            # No need to warn if outdated DATA_DIR does not exist.
+            # Logger.Log(f"{name} config does not have a 'DATA_DIR' element; defaulting to data_dir={self._data_dir}", logging.WARN)
         if "LOG_FILE" in all_elements.keys():
             self._log_file = ConfigSchema._parseLogFile(all_elements["log_file"])
         else:
@@ -120,15 +54,15 @@ class ConfigSchema(Schema):
         if "FILE_INDEXING" in all_elements.keys():
             self._file_indexing = ConfigSchema._parseFileIndexing(all_elements["FILE_INDEXING"])
         else:
-            self._file_indexing = {}
+            self._file_indexing = FileIndexingSchema(name="FILE_INDEXING", all_elements={})
             Logger.Log(f"{name} config does not have a 'FILE_INDEXING' element; defaulting to file_indexing={self._file_indexing}", logging.WARN)
         if "SSH_CONFIG" in all_elements.keys():
             self._ssh_config = ConfigSchema._parseSSHConfig(all_elements["SSH_CONFIG"])
         else:
-            self._ssh_config = {}
+            self._ssh_config = SSHSchema(name="SSH_CONFIG", all_elements={})
             Logger.Log(f"{name} config does not have a 'SSH_CONFIG' element; defaulting to ssh_config={self._ssh_config}", logging.WARN)
         if "GAME_SOURCES" in all_elements.keys():
-            self._game_sources = ConfigSchema._parseGameSources(all_elements["GAME_SOURCES"])
+            self._game_sources = ConfigSchema._parseDataSources(all_elements["GAME_SOURCES"])
         else:
             self._game_sources = {}
             Logger.Log(f"{name} config does not have a 'GAME_SOURCES' element; defaulting to game_sources={self._game_sources}", logging.WARN)
@@ -162,11 +96,11 @@ class ConfigSchema(Schema):
         return self._fail_fast
 
     @property
-    def FileIndexConfig(self) -> Dict[str, Path | str]:
+    def FileIndexConfig(self) -> FileIndexingSchema:
         return self._file_idx
 
     @property
-    def SSHConfig(self) -> Dict[str, str | int]:
+    def SSHConfig(self) -> SSHSchema:
         return self._ssh_config
 
     @property
@@ -259,4 +193,41 @@ class ConfigSchema(Schema):
         return ret_val
 
     @staticmethod
-    def _parseFileIndexing(indexing) -> 
+    def _parseFileIndexing(indexing) -> FileIndexingSchema:
+        ret_val : FileIndexingSchema
+        if isinstance(indexing, dict):
+            ret_val = FileIndexingSchema(name="FILE_INDEXING", all_elements=indexing)
+        else:
+            ret_val = FileIndexingSchema(name="FILE_INDEXING", all_elements={})
+            Logger.Log(f"Config file indexing was unexpected type {type(indexing)}, defaulting to default indexing config: {ret_val.AsMarkdown}.", logging.WARN)
+        return ret_val
+
+    @staticmethod
+    def _parseSSHConfig(ssh) -> SSHSchema:
+        ret_val : SSHSchema
+        if isinstance(ssh, dict):
+            ret_val = SSHSchema(name="SSH_CONFIG", all_elements=ssh)
+        else:
+            ret_val = SSHSchema(name="SSH_CONFIG", all_elements={})
+            Logger.Log(f"Config ssh config was unexpected type {type(ssh)}, defaulting to default ssh config: {ret_val.AsMarkdown}.", logging.WARN)
+        return ret_val
+
+    @staticmethod
+    def _parseDataSources(sources) -> Dict[str, DataSourceSchema]:
+        ret_val : Dict[str, DataSourceSchema]
+        if isinstance(sources, dict):
+            ret_val = { key : DataSourceSchema(name=val, all_elements=val) for key, val in sources.items() }
+        else:
+            ret_val = {}
+            Logger.Log(f"Config data sources was unexpected type {type(sources)}, defaulting to empty dict: {ret_val}.", logging.WARN)
+        return ret_val
+
+    @staticmethod
+    def _parseGameSourceMap(sources) -> Dict[str, GameSourceSchema]:
+        ret_val : Dict[str, GameSourceSchema]
+        if isinstance(sources, dict):
+            ret_val = { key : GameSourceSchema(name=val, all_elements=val) for key, val in sources.items() }
+        else:
+            ret_val = {}
+            Logger.Log(f"Config game source map was unexpected type {type(sources)}, defaulting to empty dict: {ret_val}.", logging.WARN)
+        return ret_val
