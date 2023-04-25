f02296d21e5e233fe6634974d3deb2d33b2dad7d
Luke Swanson <lwswanson2@wisc.edu>
2023-04-25 08:30:22 -0500

    Work towards having schemas for data sources.

--- schemas/config_schemas/DataSourceSchema.py
+++ schemas/config_schemas/DataSourceSchema.py
@@ -1,4 +1,5 @@
 # import standard libraries
+import abc
 import logging
 from typing import Any, Dict, List, Optional, Union
 # import local files
@@ -6,50 +7,54 @@ from schemas.Schema import Schema
 from utils import Logger
 
 class DataSourceSchema(Schema):
+    def __init__(self, name:str, other_elements:Dict[str, Any]):
+        super().__init__(name=name, other_elements=other_elements)
+
+    @property
+    @abc.abstractmethod
+    def Type(self) -> str:
+        pass
+
+class BigQuerySchema(DataSourceSchema):
     def __init__(self, name:str, all_elements:Dict[str, Any]):
-        self._type     : str
-        self._name     : str
-        self._database : DatabaseSchema
+        self._project_id : str
+        self._dataset_id : str
         self._credential : Optional[str]
 
         if not isinstance(all_elements, dict):
             all_elements = {}
             Logger.Log(f"For {name} Game Source config, all_elements was not a dict, defaulting to empty dict", logging.WARN)
-        if "schema" in all_elements.keys():
-            self._schema = GameSourceSchema._parseSchema(all_elements["schema"])
+        if "PROJECT_ID" in all_elements.keys():
+            self._project_id = BigQuerySchema._parseProjectID(all_elements["PROJECT_ID"])
         else:
-            self._schema = "UNKNOWN"
-            Logger.Log(f"{name} config does not have a 'schema' element; defaulting to schema={self._schema}", logging.WARN)
-        if "source" in all_elements.keys():
-            self._source = GameSourceSchema._parseSource(all_elements["source"])
+            self._project_id = "UNKNOWN"
+            Logger.Log(f"{name} config does not have a 'PROJECT_ID' element; defaulting to project_id={self._project_id}", logging.WARN)
+        if "DATASET_ID" in all_elements.keys():
+            self._dataset_id = BigQuerySchema._parseDatasetID(all_elements["DATASET_ID"])
         else:
-            self._source = "UNKNOWN"
-            Logger.Log(f"{name} config does not have a 'source' element; defaulting to source={self._source}", logging.WARN)
-        if "table" in all_elements.keys():
-            self._table_name = GameSourceSchema._parseTableName(all_elements["table"])
-        else:
-            self._table_name = "UNKNOWN"
-            Logger.Log(f"{name} config does not have a 'table' element; defaulting to table={self._table_name}", logging.WARN)
-        if "credential" in all_elements.keys():
-            self._credential = GameSourceSchema._parseCredential(all_elements["credential"])
+            self._dataset_id = "UNKNOWN"
+            Logger.Log(f"{name} config does not have a 'DATASET_ID' element; defaulting to dataset_id={self._dataset_id}", logging.WARN)
+        if "PROJECT_KEY" in all_elements.keys():
+            self._credential = BigQuerySchema._parseCredential(all_elements["PROJECT_KEY"])
         else:
             self._credential = None
-            Logger.Log(f"{name} config does not have a 'credential' element; defaulting to credential={self._credential}", logging.WARN)
+            Logger.Log(f"{name} config does not have a 'PROJECT_KEY' element; defaulting to credential=None", logging.WARN)
 
-        _leftovers = { key : val for key,val in all_elements.items() if key not in {"schema", "source", "table", "credential"} }
+        _used = {"PROJECT_ID", "DATASET_ID", "PROJECT_KEY"}
+        _leftovers = { key : val for key,val in all_elements.items() if key not in _used }
         super().__init__(name=name, other_elements=_leftovers)
 
     @property
-    def Schema(self) -> str:
-        return self._schema
+    def Type(self) -> str:
+        return "BigQuery"
 
     @property
-    def Source(self) -> str:
-        return self._source
+    def ProjectID(self) -> str:
+        return self._project_id
 
     @property
-    def TableName(self) -> str:
-        return self._table_name
+    def DatasetID(self) -> str:
+        return self._dataset_id
 
     @property
     def Credential(self) -> Optional[str]:
@@ -59,37 +64,27 @@ class DataSourceSchema(Schema):
     def AsMarkdown(self) -> str:
         ret_val : str
 
-        ret_val = f"{self.Name}: _{self.Schema}_ format, source {self.Source}.{self.TableName}"
+        ret_val = f"{self.Name}: `{self.ProjectID}.{self.DatasetID}` ({self.Type})"
         return ret_val
 
     @staticmethod
-    def _parseSchema(schema) -> str:
+    def _parseProjectID(proj_id) -> str:
         ret_val : str
-        if isinstance(schema, str):
-            ret_val = schema
+        if isinstance(proj_id, str):
+            ret_val = proj_id
         else:
-            ret_val = str(schema)
-            Logger.Log(f"Game Source schema type was unexpected type {type(schema)}, defaulting to str(schema)={ret_val}.", logging.WARN)
+            ret_val = str(proj_id)
+            Logger.Log(f"Data Source project ID was unexpected type {type(proj_id)}, defaulting to str(proj_id)={ret_val}.", logging.WARN)
         return ret_val
 
     @staticmethod
-    def _parseSource(source) -> str:
+    def _parseDatasetID(data_id) -> str:
         ret_val : str
-        if isinstance(source, str):
-            ret_val = source
+        if isinstance(data_id, str):
+            ret_val = data_id
         else:
-            ret_val = str(source)
-            Logger.Log(f"Game Source source name was unexpected type {type(source)}, defaulting to str(source)={ret_val}.", logging.WARN)
-        return ret_val
-
-    @staticmethod
-    def _parseTableName(table) -> str:
-        ret_val : str
-        if isinstance(table, str):
-            ret_val = table
-        else:
-            ret_val = str(table)
-            Logger.Log(f"Game Source table name was unexpected type {type(table)}, defaulting to str(table)={ret_val}.", logging.WARN)
+            ret_val = str(data_id)
+            Logger.Log(f"Data Source dataset ID was unexpected type {type(data_id)}, defaulting to str(data_id)={ret_val}.", logging.WARN)
         return ret_val
 
     @staticmethod
