4f7d203d7727212e929fe784ebe1653d6c985096
Luke Swanson <lwswanson2@wisc.edu>
2023-05-08 09:11:40 -0500

    Rename GameSourceSchema.py -> GameSourceMapSchema, and use LegacyConfigSchema in main config.

--- schemas/config_schemas/ConfigSchema.py
+++ schemas/config_schemas/ConfigSchema.py
@@ -4,33 +4,23 @@ from pathlib import Path
 from typing import Any, Dict, Optional
 # import local files
 from schemas.config_schemas.IndexingSchema import FileIndexingSchema
-from schemas.config_schemas.SSHSchema import SSHSchema
-from schemas.config_schemas.GameSourceSchema import GameSourceSchema
-from schemas.config_schemas.DataSourceSchema import DataSourceSchema
+from schemas.config_schemas.GameSourceMapSchema import GameSourceMapElementSchema
+from schemas.config_schemas.DataSourceSchema import DataSourceSchema, BigQuerySchema, MySQLSchema, ParseSourceType
+from schemas.config_schemas.LegacyConfigSchema import LegacyConfigSchema
 from schemas.Schema import Schema
 from utils import Logger
 
 class ConfigSchema(Schema):
     def __init__(self, name:str, all_elements:Dict[str, Any]):
-        self._data_dir   : Path
         self._log_file   : bool
         self._batch_size : int
         self._dbg_level  : int
         self._fail_fast  : bool
         self._file_idx   : FileIndexingSchema
-        self._ssh_config : SSHSchema
         self._data_src   : Dict[str, DataSourceSchema]
-        self._game_src_map : Dict[str, GameSourceSchema]
+        self._game_src_map : Dict[str, GameSourceMapElementSchema]
 
-        if not isinstance(all_elements, dict):
-            all_elements = {}
-            Logger.Log(f"For {name} base config, all_elements was not a dict, defaulting to empty dict", logging.WARN)
-        if "DATA_DIR" in all_elements.keys():
-            self._data_dir = ConfigSchema._parseDataDir(all_elements["DATA_DIR"])
-        else:
-            self._data_dir = Path("./data/")
-            # No need to warn if outdated DATA_DIR does not exist.
-            # Logger.Log(f"{name} config does not have a 'DATA_DIR' element; defaulting to data_dir={self._data_dir}", logging.WARN)
+        self._legacy_elems : LegacyConfigSchema = LegacyConfigSchema(name=name, all_elements=all_elements)
         if "LOG_FILE" in all_elements.keys():
             self._log_file = ConfigSchema._parseLogFile(all_elements["log_file"])
         else:
@@ -54,13 +44,9 @@ class ConfigSchema(Schema):
         if "FILE_INDEXING" in all_elements.keys():
             self._file_indexing = ConfigSchema._parseFileIndexing(all_elements["FILE_INDEXING"])
         else:
-            self._file_indexing = FileIndexingSchema(name="FILE_INDEXING", all_elements={})
+            _fallback_elems = { "LOCAL_DIR" : self._legacy_elems.DataDirectory }
+            self._file_indexing = FileIndexingSchema(name="FILE_INDEXING", all_elements=_fallback_elems)
             Logger.Log(f"{name} config does not have a 'FILE_INDEXING' element; defaulting to file_indexing={self._file_indexing}", logging.WARN)
-        if "SSH_CONFIG" in all_elements.keys():
-            self._ssh_config = ConfigSchema._parseSSHConfig(all_elements["SSH_CONFIG"])
-        else:
-            self._ssh_config = SSHSchema(name="SSH_CONFIG", all_elements={})
-            Logger.Log(f"{name} config does not have a 'SSH_CONFIG' element; defaulting to ssh_config={self._ssh_config}", logging.WARN)
         if "GAME_SOURCES" in all_elements.keys():
             self._game_sources = ConfigSchema._parseDataSources(all_elements["GAME_SOURCES"])
         else:
@@ -78,11 +64,7 @@ class ConfigSchema(Schema):
 
     @property
     def DataDirectory(self) -> Path:
-        if self.FileIndexConfig.LocalDirectory is not None:
-            return self.FileIndexConfig.LocalDirectory
-        else:
-            Logger.Log(f"Did not have a local directory in FILE_INDEXING config item, falling back on DATA_DIR config item.")
-            return self._data_dir
+        return self.FileIndexConfig.LocalDirectory
 
     @property
     def UseLogFile(self) -> bool:
@@ -105,15 +87,11 @@ class ConfigSchema(Schema):
         return self._file_idx
 
     @property
-    def SSHConfig(self) -> SSHSchema:
-        return self._ssh_config
-
-    @property
     def DataSources(self) -> Dict[str, DataSourceSchema]:
         return self._data_src
 
     @property
-    def GameSourceMap(self) -> Dict[str, GameSourceSchema]:
+    def GameSourceMap(self) -> Dict[str, GameSourceMapElementSchema]:
         return self._game_source_map
 
     @property
@@ -121,18 +99,6 @@ class ConfigSchema(Schema):
         ret_val : str
 
         ret_val = f"{self.Name}"
-        return ret_val
-
-    @staticmethod
-    def _parseDataDir(dir) -> Path:
-        ret_val : Path
-        if isinstance(dir, Path):
-            ret_val = dir
-        elif isinstance(dir, str):
-            ret_val = Path(dir)
-        else:
-            ret_val = Path(str(dir))
-            Logger.Log(f"Config data dir was unexpected type {type(dir)}, defaulting to Path(str(dir))={ret_val}.", logging.WARN)
         return ret_val
 
     @staticmethod
@@ -208,30 +174,29 @@ class ConfigSchema(Schema):
         return ret_val
 
     @staticmethod
-    def _parseSSHConfig(ssh) -> SSHSchema:
-        ret_val : SSHSchema
-        if isinstance(ssh, dict):
-            ret_val = SSHSchema(name="SSH_CONFIG", all_elements=ssh)
-        else:
-            ret_val = SSHSchema(name="SSH_CONFIG", all_elements={})
-            Logger.Log(f"Config ssh config was unexpected type {type(ssh)}, defaulting to default ssh config: {ret_val.AsMarkdown}.", logging.WARN)
-        return ret_val
-
-    @staticmethod
     def _parseDataSources(sources) -> Dict[str, DataSourceSchema]:
         ret_val : Dict[str, DataSourceSchema]
         if isinstance(sources, dict):
-            ret_val = { key : DataSourceSchema(name=val, other_elements=val) for key, val in sources.items() }
+            ret_val = {}
+            for key,val in sources.items():
+                _type = val.get('DBYTYPE')
+                match (_type):
+                    case "BIGQUERY":
+                        ret_val[key] = BigQuerySchema(name=val, all_elements=val)
+                    case "MYSQL":
+                        ret_val[key] = MySQLSchema(name=val, all_elements=val)
+                    case _:
+                        Logger.Log(f"Game source {key} did not  have a 'DB_TYPE', and will be skipped!", logging.WARN)
         else:
             ret_val = {}
             Logger.Log(f"Config data sources was unexpected type {type(sources)}, defaulting to empty dict: {ret_val}.", logging.WARN)
         return ret_val
 
     @staticmethod
-    def _parseGameSourceMap(sources) -> Dict[str, GameSourceSchema]:
-        ret_val : Dict[str, GameSourceSchema]
+    def _parseGameSourceMap(sources) -> Dict[str, GameSourceMapElementSchema]:
+        ret_val : Dict[str, GameSourceMapElementSchema]
         if isinstance(sources, dict):
-            ret_val = { key : GameSourceSchema(name=val, all_elements=val) for key, val in sources.items() }
+            ret_val = { key : GameSourceMapElementSchema(name=val, all_elements=val) for key, val in sources.items() }
         else:
             ret_val = {}
             Logger.Log(f"Config game source map was unexpected type {type(sources)}, defaulting to empty dict: {ret_val}.", logging.WARN)
