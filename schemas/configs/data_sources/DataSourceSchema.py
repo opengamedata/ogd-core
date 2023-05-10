9a0f74e85cb7e443edcc0cd2ac4d1043f93c3239
Luke Swanson <lwswanson2@wisc.edu>
2023-05-10 08:01:05 -0500

    DataSourceSchema should absorb the DB_TYPE element.

--- schemas/configs/data_sources/DataSourceSchema.py
+++ schemas/configs/data_sources/DataSourceSchema.py
@@ -1,14 +1,36 @@
 # import standard libraries
 import abc
+import logging
 from typing import Any, Dict
 # import local files
 from schemas.Schema import Schema
+from utils import Logger
 
 class DataSourceSchema(Schema):
     def __init__(self, name:str, other_elements:Dict[str, Any]):
+        self._db_type : str
+        if not isinstance(other_elements, dict):
+            other_elements = {}
+            Logger.Log(f"For {name} Data Source config, other_elements was not a dict, defaulting to empty dict", logging.WARN)
+        # Parse DB info
+        if "DB_TYPE" in other_elements.keys():
+            self._db_type = DataSourceSchema._parseDBType(other_elements["DB_TYPE"])
+        else:
+            self._db_type = "UNKNOWN"
+            Logger.Log(f"{name} config does not have a 'DB_TYPE' element; defaulting to db_host={self._db_type}", logging.WARN)
         super().__init__(name=name, other_elements=other_elements)
 
     @property
     @abc.abstractmethod
     def Type(self) -> str:
         pass
+
+    @staticmethod
+    def _parseDBType(db_type) -> str:
+        ret_val : str
+        if isinstance(db_type, str):
+            ret_val = db_type
+        else:
+            ret_val = str(db_type)
+            Logger.Log(f"Data Source DB type was unexpected type {type(db_type)}, defaulting to str(db_type)={ret_val}.", logging.WARN)
+        return ret_val
