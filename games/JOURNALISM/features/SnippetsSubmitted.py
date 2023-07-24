17f85367cea4c1e8ff70dc5d03c60ae58a1533e2
Ameya kshirsagar <42925930+ameyark28@users.noreply.github.com>
2023-07-24 16:10:26 -0500

    #111 Journalism Feature: SnippetsSubmitted

---
+++ games/JOURNALISM/features/SnippetsSubmitted.py
@@ -0,0 +1,45 @@
+import logging
+from datetime import datetime
+from schemas import Event
+from typing import Any, List, Optional
+# import locals
+from utils import Logger
+from extractors.features.PerLevelFeature import PerLevelFeature
+from extractors.Extractor import ExtractorParameters
+from schemas.Event import Event
+from schemas.ExtractionMode import ExtractionMode
+from schemas.FeatureData import FeatureData
+
+class SnippetsSubmitted(PerLevelFeature):
+    def __init__(self, params:ExtractorParameters):
+        PerLevelFeature.__init__(self, params=params)
+        self._snippet_ids : List[str] = []
+
+
+    # *** IMPLEMENT ABSTRACT FUNCTIONS ***
+    @classmethod
+    def _getEventDependencies(cls, mode:ExtractionMode) -> List[str]:
+        return ["publish_story_click"]
+
+    @classmethod
+    def _getFeatureDependencies(cls, mode:ExtractionMode) -> List[str]:
+        return []
+
+    def _validateEventCountIndex(self, event: Event):
+        return self.CountIndex == 1
+
+    def _extractFromEvent(self, event:Event) -> None:
+        if event.EventName == "publish_story_click":
+            snippet_list = event.EventData["snippet_list"]
+            for snippet in snippet_list:
+                self._snippet_ids.append(snippet["snippet_id"])
+
+    def _getFeatureValues(self) -> List[Any]:
+        return [self._snippet_ids]
+
+    # *** Optionally override public functions. ***
+    
+    @staticmethod
+    def AvailableModes() -> List[ExtractionMode]:
+        return [ExtractionMode.SESSION, ExtractionMode.DETECTOR]
+
