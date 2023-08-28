787aab84aed4b48451c4e9cac1ab70ed121ef57b
renee li <rli366@wisc.edu>
2023-08-28 16:43:22 -0500

    test

--- games/PENGUINS/features/PerRegionFeature.py
+++ games/PENGUINS/features/PerRegionFeature.py
@@ -10,21 +10,40 @@ from schemas.Event import Event
 class PerRegionFeature(PerCountFeature):
     def __init__(self, params:ExtractorParameters, region_map:dict):
         super().__init__(params=params,region_map=dict)
-        self.region_map = region_map
+        self._region_map = region_map
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
+
 
     def _validateEventCountIndex(self, event:Event):
         ret_val : bool = False
 
         region_data = event.EventData["region_name"]
-        if region_data is not None:
-            if region_data in self.region_map and self.region_map[region_data] == self.CountIndex:
-                ret_val = True
+        target_region = self._region_map[self.CountIndex]
+        current_position = {
+            'x': event.GameState['posX'],
+            'y': event.GameState['posY'],
+            'z': event.GameState['posZ']
+        }
+
+        if (current_position['x'] > target_region['minX'] and 
+            current_position['x'] < target_region['maxX'] and
+            current_position['y'] > target_region['minY'] and
+            current_position['y'] < target_region['maxY'] and
+            current_position['z'] > target_region['minZ'] and
+            current_position['z'] < target_region['maxZ']):
+            ret_val = True
         else:
             Logger.Log(f"Got invalid region_name data in {type(self).__name__}", logging.WARNING)
-
+        
         return ret_val
+
+        #if region_data is not None:
+            #if region_data in self.region_map and self.region_map[region_data] == self.CountIndex:
+                #ret_val = True
+        #else:
+            #Logger.Log(f"Got invalid region_name data in {type(self).__name__}", logging.WARNING)
+        #return ret_val
 
     # *** Optionally override public functions. ***
 
