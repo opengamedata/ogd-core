b4e4cb69fb52675ccb60a18ef7acd5bf074e0e5c
Luke Swanson <lwswanson2@wisc.edu>
2024-02-09 12:18:41 -0600

    Add comment blocks for a couple of the enum types.

--- src/ogd/core/schemas/ExtractionMode.py
+++ src/ogd/core/schemas/ExtractionMode.py
@@ -2,6 +2,9 @@
 from enum import IntEnum
 
 class ExtractionMode(IntEnum):
+    """Simple enum to represent the different levels of granularity at which extractions can be carried out:
+    Session, Player, Population, or Detector.
+    """
     SESSION = 1
     PLAYER = 2
     POPULATION = 3
