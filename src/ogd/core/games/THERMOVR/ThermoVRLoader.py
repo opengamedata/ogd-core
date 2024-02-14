0f5fbbd7300b3faeef57cec7669cd38dcf944d07
Luke Swanson <lwswanson2@wisc.edu>
2024-02-14 11:12:54 -0600

    Add in basic loader and folders.

---
+++ src/ogd/core/games/THERMOVR/ThermoVRLoader.py
@@ -0,0 +1,101 @@
+# import standard libraries
+import itertools
+import json
+from pathlib import Path
+from typing import Any, Callable, Dict, List, Optional
+# import local files
+from . import features
+from ogd.core.games import THERMOVR
+from ogd.core.extractors.detectors.Detector import Detector
+from ogd.core.extractors.Extractor import ExtractorParameters
+from ogd.core.extractors.ExtractorLoader import ExtractorLoader
+from ogd.core.extractors.features.Feature import Feature
+from ogd.core.games.THERMOVR.detectors import *
+from ogd.core.games.THERMOVR.features import *
+from ogd.core.schemas.Event import Event
+from ogd.core.schemas.ExtractionMode import ExtractionMode
+from ogd.core.schemas.games.GameSchema import GameSchema
+from ogd.core.utils.utils import loadJSONFile
+
+EXPORT_PATH = "games/THERMOVR/DBExport.json"
+
+## @class ThermoVRLoader
+#  Extractor subclass for extracting features from ThermoVR game data.
+class ThermoVRLoader(ExtractorLoader):
+
+    # *** BUILT-INS & PROPERTIES ***
+
+    ## Constructor for the ThermoVRLoader class.
+    def __init__(self, player_id:str, session_id:str, game_schema: GameSchema, mode:ExtractionMode, feature_overrides:Optional[List[str]]):
+        """Constructor for the ThermoVRLoader class.
+
+        :param player_id: _description_
+        :type player_id: str
+        :param session_id: The id number for the session whose data is being processed by this instance
+        :type session_id: str
+        :param game_schema: A data structure containing information on how the game events and other data are structured
+        :type game_schema: GameSchema
+        :param feature_overrides: A list of features to export, overriding the default of exporting all enabled features.
+        :type feature_overrides: Optional[List[str]]
+        """
+        super().__init__(player_id=player_id, session_id=session_id, game_schema=game_schema, mode=mode, feature_overrides=feature_overrides)
+        self._lab_map = {}
+        data = None
+
+        # Load ThermoVR jobs export and map job names to integer values
+        _dbexport_path = Path(THERMOVR.__file__) if Path(THERMOVR.__file__).is_dir() else Path(THERMOVR.__file__).parent
+        with open(_dbexport_path / "DBExport.json", "r") as file:
+            export = json.load(file)
+
+            task_num = 1
+            for i, job in enumerate(export["jobs"], start=1):
+                self._lab_map[job["id"]] = i
+                self._diff_map[i] = job["difficulties"]
+                for task in job["tasks"]:
+                    task_by_job = job["id"] + "_" + task["id"]
+                    self._task_map[task_by_job] = task_num
+                    task_num += 1
+
+        # Update level count
+        self._game_schema._max_level = len(self._lab_map) - 1
+
+    # *** IMPLEMENT ABSTRACT FUNCTIONS ***
+
+    @staticmethod
+    def _getFeaturesModule():
+        return features
+
+    def _loadFeature(self, feature_type:str, extractor_params:ExtractorParameters, schema_args:Dict[str,Any]) -> Feature:
+        ret_val : Feature
+        # First run through aggregate features
+        if extractor_params._count_index == None:
+            match feature_type:
+                case _:
+                    raise NotImplementedError(f"'{feature_type}' is not a valid aggregate feature type for ThermoVR.")
+        # then run through per-count features.
+        else:
+            match feature_type:
+                case _:
+                    raise NotImplementedError(f"'{feature_type}' is not a valid per-count feature type for ThermoVR.")
+        return ret_val
+
+    def _loadDetector(self, detector_type:str, extractor_params:ExtractorParameters, schema_args:Dict[str,Any], trigger_callback:Callable[[Event], None]) -> Detector:
+        ret_val : Detector
+        match detector_type:
+            case _:
+                raise NotImplementedError(f"'{detector_type}' is not a valid detector for ThermoVR.")
+        return ret_val
+
+    # @staticmethod
+    # def GetThermoVRLabCount(db_export_path:Path=Path(".") / "ogd" / "core" / "games" / "THERMOVR"):
+    #     db_export = loadJSONFile(filename="DBExport.json", path=db_export_path)
+    #     return len(db_export.get("jobs", []))
+
+    # @staticmethod
+    # def GetThermoVRTaskCount(db_export_path:Path=Path(".") / "ogd" / "core" / "games" / "THERMOVR"):
+    #     db_export = loadJSONFile(filename="DBExport.json", path=db_export_path)
+    #     list_o_lists = [job.get('tasks', []) for job in db_export.get('jobs', [])]
+    #     # jobs_to_task_cts = [f"{job.get('id')}: {len(job.get('tasks', []))}" for job in db_export.get('jobs', [])]
+    #     # Logger.Log(f"Task counts by job:\n{jobs_to_task_cts}", logging.DEBUG)
+    #     all_tasks    = list(itertools.chain.from_iterable(list_o_lists))
+    #     return len(all_tasks)
