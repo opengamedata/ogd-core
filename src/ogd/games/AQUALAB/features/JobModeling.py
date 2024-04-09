e0d3c150b9989dac1d4c2dbb084824f3501fc418
Luke Swanson <lwswanson2@wisc.edu>
2024-04-09 12:08:05 -0500

    JobModeling needed its actual class name updated to match file name.

--- src/ogd/games/AQUALAB/features/JobModeling.py
+++ src/ogd/games/AQUALAB/features/JobModeling.py
@@ -10,7 +10,7 @@ from ogd.core.schemas.Event import Event
 from ogd.core.schemas.ExtractionMode import ExtractionMode
 from ogd.core.schemas.FeatureData import FeatureData
 
-class JobModelingTime(PerJobFeature):
+class JobModeling(PerJobFeature):
     def __init__(self, params:GeneratorParameters, job_map:dict):
         self._job_map = job_map
         super().__init__(params=params, job_map=job_map)
