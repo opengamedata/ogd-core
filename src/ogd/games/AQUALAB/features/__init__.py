83da5c72b5000f349ca9538698558bbf87bb6416
Luke Swanson <lwswanson2@wisc.edu>
2024-04-09 11:54:19 -0500

    Go back to individual import statements.

--- src/ogd/games/AQUALAB/features/__init__.py
+++ src/ogd/games/AQUALAB/features/__init__.py
@@ -33,6 +33,8 @@ __all__ = [
     "RegionName",
     "SessionDiveSitesCount",
     "SessionDuration",
+    "SessionGuideCount",
+    "SessionHelpCount",
     "SessionID",
     "SwitchJobsCount",
     "SyncCompletionTime",
@@ -55,59 +57,59 @@ __all__ = [
     "TotalBayouTime"
 ]
 
-from . import (
-    ActiveJobs,
-    ActiveTime,
-    AppVersions,
-    AverageSessionTime,
-    EchoSessionID,
-    EventList,
-    JobActiveTime,
-    JobArgumentation,
-    JobCompletionTime,
-    JobDiveSitesCount,
-    JobDiveTime,
-    JobExperimentation,
-    JobGuideCount,
-    JobHelpCount,
-    JobLocationChanges,
-    JobModeling,
-    JobPlayTime,
-    JobPriorAttempt,
-    JobPriorComplete,
-    JobsAttempted,
-    JobsCompleted,
-    JobStartCount,
-    JobTasksCompleted,
-    JobTriesInArgument,
-    ModelExportCount,
-    ModelInterveneCount,
-    ModelPredictCount,
-    PlayerSummary,
-    PlayLocations,
-    PopulationSummary,
-    RegionJobCount,
-    RegionName,
-    SessionDiveSitesCount,
-    SessionDuration,
-    SessionID,
-    SwitchJobsCount,
-    SyncCompletionTime,
-    TankRulesCount,
-    TopJobCompletionDestinations,
-    TopJobSwitchDestinations,
-    TotalArcticTime,
-    TotalArgumentationTime,
-    TotalBayouTime,
-    TotalCoralTime,
-    TotalDiveTime,
-    TotalExperimentationTime,
-    TotalGuideCount,
-    TotalHelpCount,
-    TotalKelpTime,
-    TotalModelingTime,
-    TotalPlayTime,
-    UserAvgActiveTime,
-    UserAvgSessionDuration,
-    UserTotalSessionDuration
-)
+from . import ActiveJobs
+from . import ActiveTime
+from . import AppVersions
+from . import AverageSessionTime
+from . import EchoSessionID
+from . import EventList
+from . import JobActiveTime
+from . import JobArgumentation
+from . import JobCompletionTime
+from . import JobDiveSitesCount
+from . import JobDiveTime
+from . import JobExperimentation
+from . import JobGuideCount
+from . import JobHelpCount
+from . import JobLocationChanges
+from . import JobModeling
+from . import JobPlayTime
+from . import JobPriorAttempt
+from . import JobPriorComplete
+from . import JobsAttempted
+from . import JobsCompleted
+from . import JobStartCount
+from . import JobTasksCompleted
+from . import JobTriesInArgument
+from . import ModelExportCount
+from . import ModelInterveneCount
+from . import ModelPredictCount
+from . import PlayerSummary
+from . import PlayLocations
+from . import PopulationSummary
+from . import RegionJobCount
+from . import RegionName
+from . import SessionDiveSitesCount
+from . import SessionDuration
+from . import SessionGuideCount
+from . import SessionHelpCount
+from . import SessionID
+from . import SwitchJobsCount
+from . import SyncCompletionTime
+from . import TankRulesCount
+from . import TopJobCompletionDestinations
+from . import TopJobSwitchDestinations
+from . import TotalArcticTime
+from . import TotalArgumentationTime
+from . import TotalBayouTime
+from . import TotalCoralTime
+from . import TotalDiveTime
+from . import TotalExperimentationTime
+from . import TotalGuideCount
+from . import TotalHelpCount
+from . import TotalKelpTime
+from . import TotalModelingTime
+from . import TotalPlayTime
+from . import UserAvgActiveTime
+from . import UserAvgSessionDuration
+from . import UserTotalSessionDuration
