b596ade11970cf2ef49a9accf0f4cbaa6ec6c4a6
RUNXUAN LI(Renee) <78246141+runxuanli@users.noreply.github.com>
2024-05-08 04:34:55 +0000

    get log from each session with the first log_version listed

---
+++ src/ogd/games/PENGUINS/features/LogVersion.py
@@ -0,0 +1,40 @@
+# import libraries
+import logging
+from typing import Any, Dict, List, Optional
+# import locals
+from ogd.core.utils.Logger import Logger
+from ogd.core.generators.Generator import GeneratorParameters
+from ogd.core.generators.extractors.Feature import Feature
+from ogd.games.PENGUINS.features.PerRegionFeature import PerRegionFeature
+from ogd.core.schemas.Event import Event
+from ogd.core.schemas.ExtractionMode import ExtractionMode
+from ogd.core.schemas.FeatureData import FeatureData
+from ogd.core.generators.extractors.SessionFeature import SessionFeature
+
+
+class LogVersion(SessionFeature):
+
+    def __init__(self, params:GeneratorParameters):
+        super().__init__(params=params)
+        self._current_log : int = 0
+
+    # *** IMPLEMENT ABSTRACT FUNCTIONS ***
+    @classmethod
+    def _eventFilter(cls, mode:ExtractionMode) -> List[str]:
+        return []
+
+    @classmethod
+    def _featureFilter(cls, mode:ExtractionMode) -> List[str]:
+        return []
+
+    def _updateFromEvent(self, event:Event) -> None:
+        self._current_log = event.log_version
+
+
+    def _updateFromFeatureData(self, feature:FeatureData):
+        return
+
+    def _getFeatureValues(self) -> List[Any]:
+        return [self._current_log]
+
+    
