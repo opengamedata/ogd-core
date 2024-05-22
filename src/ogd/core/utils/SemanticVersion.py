6493018715c97ebd88b85ff865d5a9d212082562
Luke Swanson <lwswanson2@wisc.edu>
2024-05-21 20:46:47 -0500

    WOrk towards more operators for SemanticVersions.

--- src/ogd/core/utils/SemanticVersion.py
+++ src/ogd/core/utils/SemanticVersion.py
@@ -11,10 +11,13 @@ from ogd.core.utils.Logger import Logger
 # import locals
 
 class SemanticVersion:
-    def __init__(self, major:Optional[int], minor:Optional[int]=None, fix:Optional[int]=None, suffix:Optional[str]=None, suffix_ver:Optional[int]=None, fallback:Optional[str]=None):
+
+    # *** BUILT-INS & PROPERTIES ***
+
+    def __init__(self, major:Optional[int], minor:Optional[int]=None, patch:Optional[int]=None, suffix:Optional[str]=None, suffix_ver:Optional[int]=None, fallback:Optional[str]=None):
         self._major      : Optional[int] = major
         self._minor      : Optional[int] = minor
-        self._fix        : Optional[int] = fix
+        self._patch      : Optional[int] = patch
         self._suffix     : Optional[str] = suffix
         self._suffix_ver : Optional[int] = suffix_ver
         self._fallback   : Optional[str] = fallback
@@ -25,27 +28,58 @@ class SemanticVersion:
     def __str__(self) -> str:
         _major      = str(self._major)       if self._major      is not None else None
         _minor      = f".{self._minor}"      if self._minor      is not None else None
-        _fix        = f".{self._fix}"        if self._fix        is not None else None
+        _patch      = f".{self._patch}"      if self._patch      is not None else None
         _suffix     = f"-{self._suffix}"     if self._suffix     is not None else None
         _suffix_ver = f".{self._suffix_ver}" if self._suffix_ver is not None else None
 
-        return f"{_major}{_minor or ''}{_fix or ''}{_suffix or ''}{_suffix_ver or ''}" if _major is not None else str(self._fallback)
+        return f"{_major}{_minor or ''}{_patch or ''}{_suffix or ''}{_suffix_ver or ''}" if _major is not None else str(self._fallback)
 
-    def __eq__(self, other) -> bool:
-        if type(other) != type(self):
-            if isinstance(other, str):
-                return self == SemanticVersion.FromString(other, verbose=False)
-            return False
-        if self._major is None or other._major is None:
-            return False
-        if self._major       == other._major  \
-        and self._minor      == other._minor  \
-        and self._fix        == other._fix    \
-        and self._suffix     == other._suffix \
-        and self._suffix_ver == other._suffix_ver:
-            return True
+    def __eq__(self, RHS:Any) -> bool:
+        _this : SemanticVersion = self
+        _that : SemanticVersion
+    # 1. Handle weird cases, like bad type or 
+        if not isinstance(RHS, SemanticVersion):
+            if isinstance(RHS, str):
+                _that = SemanticVersion.FromString(RHS, verbose=False)
+            else:
+                return False
+        if _this.IsValid and _that.IsValid:
+    # 2. Handle general case, where we're just comparing each piece
+            if _this._major       == _that._major  \
+            and _this._minor      == _that._minor  \
+            and _this._patch        == _that._patch    \
+            and _this._suffix     == _that._suffix \
+            and _this._suffix_ver == _that._suffix_ver:
+                return True
         return False
 
+    def __lt__(self, RHS:Any) -> bool:
+        _this : SemanticVersion = self
+        _that : SemanticVersion
+    # 1. Handle cases where RHS is not a SemanticVersion.
+        if not isinstance(RHS, SemanticVersion):
+            if isinstance(RHS, str):
+                _that = SemanticVersion.FromString(RHS, verbose=False)
+            else:
+                return False
+    # 2. Handle general case, where we're just comparing each piece
+        if _this._major is not None and _that._major is not None:
+        # a. Compare Majors
+            if _this._major < _that._major:
+                return True
+            elif _this._major == _that._major:
+        # b. Compare minors
+                if _this._minor is not None and _that._minor is not None:
+                    if _this._minor < _that._minor:
+                        return True
+                    elif _this._minor == _that._minor:
+        # c. Compare revisions
+                        if _this._patch is not None and _that._patch is not None:
+                            if _this._patch < _that._patch:
+                                return True
+        return False
+
+    # *** PUBLIC STATICS ***
 
     @staticmethod
     def FromString(semver:str, verbose:bool=True) -> 'SemanticVersion':
@@ -54,6 +88,14 @@ class SemanticVersion:
             Logger.Log(f"Pieces: {pieces}", logging.DEBUG)
 
         return SemanticVersion._parseMajor(semver=semver, pieces=pieces, verbose=verbose)
+
+    # *** PUBLIC METHODS ***
+
+    # *** PROPERTIES ***
+
+    @property
+    def IsValid(self) -> bool:
+        return self._major is not None
 
     @staticmethod
     def _parseMajor(semver:str, pieces:List[str], verbose:bool) -> 'SemanticVersion':
@@ -112,7 +154,7 @@ class SemanticVersion:
             if len(pieces) > 1:
                 ret_val = SemanticVersion._parseSuffix(semver=semver, pieces=pieces[1:], major=major, minor=minor, fix=_fix, verbose=verbose)
             else:
-                ret_val = SemanticVersion(major=major, minor=minor, fix=_fix)
+                ret_val = SemanticVersion(major=major, minor=minor, patch=_fix)
         finally:
             return ret_val
 
@@ -125,7 +167,7 @@ class SemanticVersion:
         if len(pieces) > 1:
             ret_val = SemanticVersion._parseSuffixVersion(semver=semver, pieces=pieces[1:], major=major, minor=minor, fix=fix, suffix=_suffix, verbose=verbose)
         else:
-            ret_val = SemanticVersion(major=major, minor=minor, fix=fix, suffix=_suffix)
+            ret_val = SemanticVersion(major=major, minor=minor, patch=fix, suffix=_suffix)
         return ret_val
 
     @staticmethod
@@ -139,9 +181,9 @@ class SemanticVersion:
             _default_ver = f"{major}.{minor}.{fix}-{suffix}"
             if verbose:
                 Logger.Log(f"Could not parse semantic version from {semver}, {pieces[0]} was not an int! Defaulting to version={_default_ver}\nError Details: {err}", level=logging.WARN)
-            ret_val = SemanticVersion(major=major, minor=minor, fix=fix, suffix=suffix)
+            ret_val = SemanticVersion(major=major, minor=minor, patch=fix, suffix=suffix)
         else:
-            ret_val = SemanticVersion(major=major, minor=minor, fix=fix, suffix=suffix, suffix_ver=_suffix_version)
+            ret_val = SemanticVersion(major=major, minor=minor, patch=fix, suffix=suffix, suffix_ver=_suffix_version)
             if len(pieces) > 1 and verbose:
                 Logger.Log(f"Semantic version string {semver} had excess parts, reducing to version={ret_val}", level=logging.WARN)
         finally:
