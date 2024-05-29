fbd2ada3b6e5e6a86d57f4b2eb79a776ebb3e9db
Luke Swanson <lwswanson2@wisc.edu>
2024-05-29 13:56:36 -0500

    Don't have required Python version high enough to use 'Self' type hints.

--- src/ogd/core/generators/extractors/builtin/BuiltinExtractor.py
+++ src/ogd/core/generators/extractors/builtin/BuiltinExtractor.py
@@ -1,7 +1,7 @@
 ## import standard libraries
 import abc
 import logging
-from typing import Any, Dict, List, Optional, Self, Type
+from typing import Any, Dict, List, Optional, Type
 # import locals
 from ogd.core.generators.Generator import Generator, GeneratorParameters
 from ogd.core.generators.extractors.Extractor import Extractor
@@ -14,7 +14,7 @@ class BuiltinExtractor(Extractor):
     ## Abstract function to get a list of event types the Feature wants.
     @classmethod
     @abc.abstractmethod
-    def _createDerivedGenerator(cls, params:GeneratorParameters, schema_args:Dict[str,Any]) -> Type[Self]:
+    def _createDerivedGenerator(cls, params:GeneratorParameters, schema_args:Dict[str,Any]) -> Type["BuiltinExtractor"]:
         """ Abstract function to generate a derived version of the Generator subclass, using the schema args.
             In general, this should just be a matter of creating a type that overwrites the list of events in `_eventFilter` or similar,
             but subclasses are left a lot of leeway in what they change about the derived type.
