e8c730ac4a8e3a5dd979c110308b15c3c3b9186d
Luke Swanson <lwswanson2@wisc.edu>
2024-05-28 22:46:23 -0500

    Update some init files in light of those changes.

--- src/ogd/core/__init__.py
+++ src/ogd/core/__init__.py
@@ -3,6 +3,8 @@ __all__ = [
 	"schemas",
     "generators",
 	"interfaces",
+	"registries",
+	"processors",
 	"managers",
 	"requests"
 ]
@@ -11,5 +13,7 @@ from . import utils
 from . import schemas
 from . import generators
 from . import interfaces
+from . import registries
+from . import processors
 from . import managers
 from . import requests
--- src/ogd/core/generators/__init__.py
+++ src/ogd/core/generators/__init__.py
@@ -2,12 +2,10 @@ __all__ = [
     "Generator",
     "detectors",
     "extractors",
-    "registries",
     "GeneratorLoader"
 ]
 
 from . import Generator
 from . import detectors
 from . import extractors
-from . import registries
 from . import GeneratorLoader
