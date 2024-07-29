142b8fb7b3d04409cb7d77f64898acbac4ad77cf
Luke Swanson <lwswanson2@wisc.edu>
2024-07-29 15:42:32 -0500

    No need to watch for county switch, doesn't help us here.

--- src/ogd/games/BLOOM/features/CountyBloomAlertCount.py
+++ src/ogd/games/BLOOM/features/CountyBloomAlertCount.py
@@ -11,36 +11,25 @@ from ogd.games.BLOOM.features.PerCountyFeature import PerCountyFeature
 class CountyBloomAlertCount(PerCountyFeature):
     def __init__(self, params: GeneratorParameters):
         super().__init__(params=params)
-        self.county_bloom_alert_counts: Dict[str, int] = {}
-        self.focused_county: Optional[str] = None
+        self._count : int = 0
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
     @classmethod
     def _eventFilter(cls, mode: ExtractionMode) -> List[str]:
-        return ["bloom_alert", "switch_county"]
+        return ["bloom_alert"]
 
     @classmethod
     def _featureFilter(cls, mode: ExtractionMode) -> List[str]:
         return []
 
     def _updateFromEvent(self, event: Event) -> None:
-        if event.EventName == "switch_county":
-            self.focused_county = event.GameState.get("current_county", None)
-        elif event.EventName == "bloom_alert":
-            county_name = event.GameState.get("current_county", None)
-            if county_name:
-                if county_name not in self.county_bloom_alert_counts:
-                    self.county_bloom_alert_counts[county_name] = 1
-                else:
-                    self.county_bloom_alert_counts[county_name] += 1
+        self._count += 1
 
     def _updateFromFeatureData(self, feature: FeatureData):
         pass
 
     def _getFeatureValues(self) -> List[Any]:
-        if self.focused_county:
-            return [self.county_bloom_alert_counts.get(self.focused_county, 0)]
-        return [0]
+        return [self._count]
 
     def Subfeatures(self) -> List[str]:
         return []
