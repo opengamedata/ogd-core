43207716b33283b638740b96fa74c59d6b1d5561
Luke Swanson <lwswanson2@wisc.edu>
2024-07-29 11:55:11 -0500

    Oops, gotta actually pass in the list of events to exclude...

--- src/ogd/core/managers/ExportManager.py
+++ src/ogd/core/managers/ExportManager.py
@@ -269,7 +269,7 @@ class ExportManager:
         _exclude_rows = None
         if request.GameID == 'BLOOM':
             _exclude_rows = ['algae_growth_end', 'algae_growth_begin']
-        ret_val = request.Interface.EventsFromIDs(id_list=next_slice_ids, id_mode=request.Range.IDMode, exclude_rows=None)
+        ret_val = request.Interface.EventsFromIDs(id_list=next_slice_ids, id_mode=request.Range.IDMode, exclude_rows=_exclude_rows)
         time_delta = datetime.now() - start
         if ret_val is not None:
             Logger.Log(f"Retrieval time for slice [{slice_num}/{slice_count}]: {time_delta} to get {len(ret_val)} events", logging.INFO, depth=2)
