7fa36f97065c6ac38f7c3c27a3eae165d7629cde
Luke Swanson <lwswanson2@wisc.edu>
2024-07-30 18:31:33 -0500

    Better debug outputs.

--- src/ogd/games/BLOOM/features/PerCountyFeature.py
+++ src/ogd/games/BLOOM/features/PerCountyFeature.py
@@ -24,8 +24,8 @@ class PerCountyFeature(PerCountFeature):
                 if self.COUNTY_LIST.index(county_name) == self.CountIndex:
                     ret_val = True
             except ValueError:
-                self.WarningMessage(f"County name {county_name} not found in COUNTY_LIST in {type(self).__name__} event name = {event.EventName}, game state = {event.GameState}")
+                self.WarningMessage(f"County name {county_name} not found in COUNTY_LIST in {type(self).__name__}, for event {event.EventName} with game state {event.GameState}")
         else:
-            self.WarningMessage(f"Got invalid current_county data in {type(self).__name__}")
+            self.WarningMessage(f"Got invalid current_county name {county_name} for event {event.EventName} in {type(self).__name__}")
 
         return ret_val
