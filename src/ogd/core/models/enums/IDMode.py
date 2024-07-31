440e5a3b86ea22acb17e2078d7988223376afb1a
Luke Swanson <lwswanson2@wisc.edu>
2024-07-31 14:37:39 -0500

    Add comment blocks for IDMode.

--- src/ogd/core/models/enums/IDMode.py
+++ src/ogd/core/models/enums/IDMode.py
@@ -1,7 +1,23 @@
+"""IDMode Module
+"""
+
 # import standard libraries
 from enum import IntEnum
 
 class IDMode(IntEnum):
+    """Enum representing the different kinds of IDs in OpenGameData.
+
+    Namely:
+
+    * Session IDs
+    * User IDs (or Player IDs)
+    * App IDs (or Game IDs)
+
+    :param IntEnum: _description_
+    :type IntEnum: _type_
+    :return: _description_
+    :rtype: _type_
+    """
     SESSION = 1
     USER = 2
     GAME = 3
