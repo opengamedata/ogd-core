f2c8069e05cd818441f7b6b800c5982c029080fd
Ameya kshirsagar <42925930+ameyark28@users.noreply.github.com>
2024-08-08 18:15:58 -0500

    #187 Feature update

--- src/ogd/games/BLOOM/features/CountyUnlockCount.py
+++ src/ogd/games/BLOOM/features/CountyUnlockCount.py
@@ -1,5 +1,5 @@
 # import libraries
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Set
 from ogd.core.generators.Generator import GeneratorParameters
 from ogd.core.generators.extractors.Feature import Feature
 from ogd.core.models.Event import Event
@@ -9,9 +9,10 @@ from ogd.core.models.FeatureData import FeatureData
 class CountyUnlockCount(Feature):
     def __init__(self, params: GeneratorParameters):
         super().__init__(params=params)
-        self.county_unlocks: Dict[str, int] = {}
+        self.county_unlocks: Dict[str, int] = {} 
+        self.county_set: Set[str] = set()
 
-    # *** IMPLEMENT ABSTRACT FUNCTIONS ***
+
     @classmethod
     def _eventFilter(cls, mode: ExtractionMode) -> List[str]:
         return ["county_unlocked"]
@@ -27,18 +28,22 @@ class CountyUnlockCount(Feature):
                 self.county_unlocks[county_name] = 1
             else:
                 self.county_unlocks[county_name] += 1
+            
+           
+            self.county_set.add(county_name)
 
     def _updateFromFeatureData(self, feature: FeatureData):
         pass
 
     def _getFeatureValues(self) -> List[Any]:
-        total_county_unlocks = sum(self.county_unlocks.values())
-        return [total_county_unlocks, self.county_unlocks]
+        total_county_unlocks = sum(self.county_unlocks.values()) 
+        unique_county_count = len(self.county_set) 
+        
+        return [unique_county_count, total_county_unlocks, self.county_unlocks]
 
     def Subfeatures(self) -> List[str]:
-        return ["Breakdown"]
+        return ["RawTotal", "Breakdown"]
 
-    # *** Optionally override public functions. ***
     @staticmethod
     def MinVersion() -> Optional[str]:
         return "1"
