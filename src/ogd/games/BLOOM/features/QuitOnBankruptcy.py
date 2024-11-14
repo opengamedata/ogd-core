7e1cb10e558ade66e2c407ab7a4b67fce9c10c47
Luke Swanson <lwswanson2@wisc.edu>
2024-11-14 17:51:11 -0600

    QuitOnBankruptcy doesn't need a 'bankruptcy reason, it's always due to out of money.
    Looking back, I don't see an issue created for this one, that's my bad.
    Additional change, get county from the EventData, so we have county that
    went bankrupt, not just the county the player was in when bankruptcy
    occurred somewhere.

--- src/ogd/games/BLOOM/features/QuitOnBankruptcy.py
+++ src/ogd/games/BLOOM/features/QuitOnBankruptcy.py
@@ -15,7 +15,6 @@ class QuitOnBankruptcy(PerCountyFeature):
         self.last_fail_type: Optional[str] = None
         self.won: Optional[bool] = None
         self.last_fail_county: Optional[str] = None
-        self.bankruptcy_reason: Optional[str] = None
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
     @classmethod
@@ -31,11 +30,7 @@ class QuitOnBankruptcy(PerCountyFeature):
         # Update failure type, county, and reason if a lose event is triggered
         if event.EventName == "lose_game":
             self.last_fail_type = event.EventData.get("lose_condition", None)
-            self.last_fail_county = event.GameState.get("current_county", None)
-
-            
-            if self.last_fail_type == "OutOfMoney":
-                self.bankruptcy_reason = "Insufficient Funds"  # Assuming reason details
+            self.last_fail_county = event.EventData.get("county_name", None)
 
         # Update win status if a win event is triggered
         elif event.EventName == "win_game":
@@ -49,11 +44,11 @@ class QuitOnBankruptcy(PerCountyFeature):
     def _getFeatureValues(self) -> List[Any]:
         # Check if the player lost and the last failure type was "OutOfMoney"
         if self.last_fail_type == "OutOfMoney" and not self.won:
-            return [1, self.last_fail_county, self.bankruptcy_reason]
+            return [1, self.last_fail_county]
         else:
             # Return default values if the condition isn't met
-            return [0, None, None]
+            return [0, None]
 
     def Subfeatures(self) -> List[str]:
         # Subfeatures are: last_fail_county, bankruptcy_reason
-        return ["FailureCounty", "BankruptcyReason"]
+        return ["FailureCounty"]
