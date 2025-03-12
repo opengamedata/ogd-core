fe2c2b6792298ffcc8c4ff6231a89188816ae13f
Luke Swanson <lwswanson2@wisc.edu>
2025-03-12 13:45:23 -0500

    Simplify logic for handling job switches, and remove some trailing whitespace.

--- src/ogd/games/AQUALAB/features/SuccessfulAdvice.py
+++ src/ogd/games/AQUALAB/features/SuccessfulAdvice.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Any, List, Optional
 # Import locals
 from ogd.common.utils.Logger import Logger
@@ -14,15 +15,15 @@ class SuccessfulAdvice(PerJobFeature):
         self._received_recommendation = False
         self._switched_away = False
         self._switched_back = False
-    
+
     @classmethod
     def _eventFilter(cls, mode: ExtractionMode) -> List[str]:
         return ["all_events"]
-    
+
     @classmethod
     def _featureFilter(cls, mode: ExtractionMode) -> List[str]:
         return []
-    
+
     def _updateFromEvent(self, event: Event) -> None:
 
         if event.EventName == "recommended_job":
@@ -30,30 +31,26 @@ class SuccessfulAdvice(PerJobFeature):
             if attempted_job_name in self._job_map and attempted_job_name == self.TargetJobName:
                 self._received_recommendation = True
                 self._successful_advice = False
-        
+
         elif event.EventName == "switch_job" and self._received_recommendation:
             prev_job_name = event.EventData.get("prev_job_name")
-            if prev_job_name and prev_job_name in self._job_map:
-                prev_job_index = self._job_map[prev_job_name]
-                if prev_job_index == self.CountIndex:
-                    self._switched_away = True 
-            
-            current_job_name = event.GameState.get("job_name")
-            if current_job_name and current_job_name in self._job_map:
-                current_job_index = self._job_map[current_job_name]
-                if current_job_index == self.CountIndex and self._switched_away:
-                    self._switched_back = True
+            new_job_name  = event.GameState.get("job_name")
+            if prev_job_name == self.TargetJobName and new_job_name != self.TargetJobName:
+                self._switched_away = True 
+            elif prev_job_name != self.TargetJobName and new_job_name == self.TargetJobName:
+                self._switched_back = True
+            else:
+                Logger.Log(f"Got unexpected job switch: switch from {prev_job_name} to {new_job_name} was sent to SuccessfulAdvice feature for {self.TargetJobName}!", logging.WARNING)
 
         elif event.EventName == "complete_job" and self._switched_away and self._switched_back:
             self._successful_advice = True
-        
+
     def _updateFromFeatureData(self, feature: FeatureData):
         return
-    
+
     def _getFeatureValues(self) -> List[Any]:
         return [self._successful_advice]
-    
+
     @staticmethod
     def MinVersion() -> Optional[str]:
         return "1"
-
