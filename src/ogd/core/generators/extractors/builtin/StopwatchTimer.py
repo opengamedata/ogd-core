089a0abee1dd4c6a5f101ce7cf55b73a909d6ac3
Luke Swanson <lwswanson2@wisc.edu>
2025-04-24 15:58:52 -0500

    Further work to get more reasonable values from StopwatchTimer
    Turns out we need to account for more corner cases.
    Rather than a single 'reset' event, we need a list of events that count
    as 'resets', as well as a list of events to be ignored in general.
    The 'resets' act like an 'end' event, but also are meant to be ignored
    in time counting.
    This way, we have start and end for normal gameplay, ignore for anything
    we don't care to include in the counts, i.e. events that only occur when
    players re-open the game after time away, but before they're back to
    playing the game, and reset events in case the 'ignore' events coincide
    with whatever mode/interaction we were recording being reset.
    Also, added documentation of these params to the module comment block.
    For future reference, need a good way to document these.

--- src/ogd/core/generators/extractors/builtin/StopwatchTimer.py
+++ src/ogd/core/generators/extractors/builtin/StopwatchTimer.py
@@ -10,9 +10,20 @@ from ogd.common.models.enums.ExtractionMode import ExtractionMode
 from ogd.common.models.FeatureData import FeatureData
 
 class StopwatchTimer(BuiltinExtractor):
-    _start_event = "NO EVENT"
-    _end_event   = "NO EVENT"
-    _reset_event = "NO EVENT"
+    """Built-in feature to count time between occurrences of 'start' and 'stop' events.
+
+    It takes the following parameters:
+    * start_event   : The event marking when to start the virtual 'stopwatch'
+    * end_event     : The event marking when to stop the virtual 'stopwatch
+    * ignore_events : An optional list of events that should be ignored from time counting.
+        For example, main-menu events when a player is trying to resume a game after time away.
+    * reset_events  : An optional list of events that will be *both* ignored *and* treated as additional end_events.
+        For example, a "session_start" event that marks a point where the player resumes the game, but outside a mode that they may have been in when they quit.
+    """
+    _start_event   = "NO EVENT"
+    _end_event     = "NO EVENT"
+    _ignore_events = []
+    _reset_events  = []
 
     def __init__(self, params: GeneratorParameters, schema_args:Dict[str,Any]):
         super().__init__(params=params, schema_args=schema_args)
@@ -25,9 +36,10 @@ class StopwatchTimer(BuiltinExtractor):
     @classmethod
     def _createDerivedGenerator(cls, params:GeneratorParameters, schema_args:Dict[str,Any]) -> Type[BuiltinExtractor]:
         class_params = {
-            "_start_event" : schema_args.get("start_event", cls._start_event),
-            "_end_event"   : schema_args.get("end_event",   cls._end_event),
-            "_reset_event" : schema_args.get("reset_event", cls._reset_event)
+            "_start_event"   : schema_args.get("start_event",   cls._start_event),
+            "_end_event"     : schema_args.get("end_event",     cls._end_event),
+            "_ignore_events" : schema_args.get("ignore_events", cls._ignore_events),
+            "_reset_events"  : schema_args.get("reset_events",  cls._reset_events),
         }
         return type(params._name, (StopwatchTimer,), class_params)
 
@@ -44,13 +56,18 @@ class StopwatchTimer(BuiltinExtractor):
 
     def _updateFromEvent(self, event: Event) -> None:
         if event.EventSource == EventSource.GAME:
-            if event.EventName == self._reset_event:
+            # Ignore anything in the ignore and reset lists.
+            if event.EventName in self._reset_events:
                 self.previous_time = None
+                self.counting = False
+            elif event.EventName in self._ignore_events:
+                self.previous_time = None
+            # when we're counting, count the stuff
             if self.counting and self.previous_time is not None:
                 event_duration = event.Timestamp - self.previous_time
                 self.total_time += event_duration
             self.previous_time = event.Timestamp
-        # if player paused game, we want to start counting paused time
+        # After counting, check if we should start/stop the timer.
         if event.EventName == self._start_event:
             self.counting = True
         elif event.EventName == self._end_event:
