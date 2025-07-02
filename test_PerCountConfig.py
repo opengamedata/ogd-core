e08a5d5da07118277a5988fca0acd9b2959574bd
Luke Swanson <lwswanson2@wisc.edu>
2025-07-02 00:30:22 -0500

    Add a todo item for the PerCountConfig.

--- test_PerCountConfig.py
+++ test_PerCountConfig.py
@@ -14,6 +14,7 @@ class test_PerCountConfig(TestCase):
     """Testbed for the PerCountConfig class.
 
         TODO : Test more 'enabled' options/combinations.
+        TODO : Tests with certain elements skipped
     """
 
     @classmethod
