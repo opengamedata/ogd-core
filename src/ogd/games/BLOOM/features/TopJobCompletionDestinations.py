2c6fcff441595c96f4fa2b2ba38d9f65d8cffcb4
vijayrampatel <vpatel57@wisc.edu>
2025-07-20 15:36:46 -0500

    added print to debug

--- src/ogd/games/BLOOM/features/TopJobCompletionDestinations.py
+++ src/ogd/games/BLOOM/features/TopJobCompletionDestinations.py
@@ -13,14 +13,6 @@ class TopJobCompletionDestinations(Feature):
     def __init__(self, params: GeneratorParameters):
         super().__init__(params=params)
 
-        # county_list = ["Hillside","Forest", "Prairie", "Wetland", "Urban"]
-        
-        # if self.CountIndex is not None:
-        #     self.county_index = self.CountIndex
-        #     self.county_name = county_list[self.CountIndex]
-        # else:
-        #     raise ValueError("JobsAttempted was not given a count index!")
-        
         self.last_unlocked_county = {} 
         self.county_completion_pairs = defaultdict(lambda: defaultdict(list)) 
 
@@ -37,35 +29,28 @@ class TopJobCompletionDestinations(Feature):
         player_id = event.user_id
         event_name = event.EventName
         
-        if event_name == "game_start" and self.last_unlocked_county[player_id] == None:
+        if event_name == "game_start" and self.last_unlocked_county.get(player_id, None):
             # print("vent:", event)
             # print("data :",event.EventData)
             # county_name = event.EventData.get("county_name")
             # if county_name == "Hillside":
             self.last_unlocked_county[player_id] = "Hillside"
             print(f"Game started for player {player_id}, setting last unlocked county to Hillside")
+            # print("last unlocked county:", self.last_unlocked_county)
 
-        # elif event_name == "county_unlocked":
-        #     current_county = event.EventData.get("county_name")
-        #     last_county = self.last_unlocked_county.get(player_id, None)
-        #     if last_county and last_county != current_county:
-        #         if player_id not in self.county_completion_pairs[last_county][current_county]:
-        #             self.county_completion_pairs[last_county][current_county].append(player_id)
+        elif event_name == "county_unlocked":
+            current_county = event.EventData.get("county_name")
+            last_county = self.last_unlocked_county.get(player_id, None)
+            print(last_county, current_county, player_id)
+            # if last_county ==  "Hillside":
+                # print("Hillside is the last unlocked county, skipping update.")
+            if last_county and last_county != current_county:
+                if player_id not in self.county_completion_pairs[last_county][current_county]:
+                    self.county_completion_pairs[last_county][current_county].append(player_id)
 
-        #     self.last_unlocked_county[player_id] = current_county
+            self.last_unlocked_county[player_id] = current_county
 
-
-        # print(f"Processing event: {event}")
-        # player_id = event.user_id
-
-        # current_county = event.EventData.get("county_name")
-        # last_county = self.last_unlocked_county.get(player_id)
-
-        # if last_county and last_county != current_county:
-        #     if player_id not in self.county_completion_pairs[last_county][current_county]:
-        #         self.county_completion_pairs[last_county][current_county].append(player_id)
-
-        # self.last_unlocked_county[player_id] = current_county
+        # print("it is here", self.last_unlocked_county)
 
     def _updateFromFeatureData(self, feature: FeatureData):
         return
@@ -73,6 +58,9 @@ class TopJobCompletionDestinations(Feature):
     def _getFeatureValues(self) -> List[Any]:
         # print("County completion pairs:", dict(self.county_completion_pairs))
         ret_val = {}
+
+        # print("Processing county completion pairs...")
+        # print("Last unlocked county:", self.last_unlocked_county)
         for src, dests in self.county_completion_pairs.items():
             sorted_dests = sorted(
                 dests.items(),
