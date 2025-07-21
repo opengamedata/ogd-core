e20bee6d12e604d30dd08333b9ff5f0ff0d915be
vijayrampatel <vpatel57@wisc.edu>
2025-07-21 12:09:47 -0500

    created different processor

---
+++ src/ogd/core/processors/PlayerModelProcessor.py
@@ -0,0 +1,48 @@
+import logging
+from typing import List, Type, Optional
+
+from ogd.common.models.FeatureData import FeatureData
+from ogd.core.generators.GeneratorLoader import GeneratorLoader
+from ogd.core.registries.ModelRegistry import ModelRegistry
+from ogd.core.processors.ExtractorProcessor import ExtractorProcessor
+from ogd.common.models.Event import Event
+from ogd.common.models.enums.ExtractionMode import ExtractionMode
+from ogd.common.schemas.games.GameSchema import GameSchema
+from ogd.common.utils.Logger import Logger
+from ogd.common.utils.typing import ExportRow
+
+class ModelProcessor(ExtractorProcessor):
+    def __init__(self, LoaderClass:Type[GeneratorLoader], game_schema: GameSchema, feature_overrides:Optional[List[str]]=None):
+        self._player_id    : str = "population"
+        self._session_id   : str = "population"
+        super().__init__(LoaderClass=LoaderClass, game_schema=game_schema, feature_overrides=feature_overrides)
+
+    def InitializeModels(self):
+        self._registry = self._createRegistry()
+        self._registry._loadFromSchema(schema=self._game_schema, loader=self._loader, overrides=self._overrides)
+            
+    def ProcessFeature(self, feature: FeatureData):
+        if self._registry:
+            self._registry._updateFromFeatureData(feature)
+
+    def TrainModels(self):
+        if self._registry:
+            self._registry.TrainModels()
+
+    @property
+    def _mode(self) -> ExtractionMode:
+        return ExtractionMode.POPULATION
+
+    def _createRegistry(self) -> ModelRegistry:
+        return ModelRegistry(mode=ExtractionMode.PLAYER)
+    
+    def _getGeneratorNames(self) -> List[str]:
+        return self._registry._getGeneratorNames() if self._registry else []
+    @property
+    def _playerID(self) -> str: return self._player_id
+    @property
+    def _sessionID(self) -> str: return self._session_id
+    def _processEvent(self, event: Event): pass
+    def _getLines(self) -> List[ExportRow]: return []
+    def _getFeatureData(self, order:int) -> List[FeatureData]: return []
+    def _clearLines(self) -> None: self._registry = self._createRegistry()
