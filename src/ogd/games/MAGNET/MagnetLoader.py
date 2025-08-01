be5a5b427f179cf5774e112c3fd5745d80451b71
Luke Swanson <lwswanson2@wisc.edu>
2025-08-01 11:53:38 -0500

    Missed Magnet loader in last commit.

--- src/ogd/games/MAGNET/MagnetLoader.py
+++ src/ogd/games/MAGNET/MagnetLoader.py
@@ -4,7 +4,7 @@ from typing import Any, Callable, Dict, List, Optional
 from . import features
 from ogd.core.generators.detectors.Detector import Detector
 from ogd.core.generators.Generator import GeneratorParameters
-from ogd.core.generators.extractors.Feature import Feature
+from ogd.core.generators.extractors.Extractor import Extractor
 from ogd.core.generators.legacy.LegacyLoader import LegacyLoader
 from ogd.games.MAGNET.features.MagnetExtractor import MagnetExtractor
 from ogd.common.models.Event import Event
