fd19ad3fcf2e0a597c133a240df28f1edbeb8a23
Luke Swanson <lwswanson2@wisc.edu>
2025-08-11 09:48:06 -0500

    Add Events prop for EventProcessor, and maintain an EventSet rather than list of events.

--- src/ogd/core/processors/EventProcessor.py
+++ src/ogd/core/processors/EventProcessor.py
@@ -1,31 +1,45 @@
 # import libraries
-import json
+import logging
 from datetime import datetime
-from typing import List, Type, Optional
+from typing import List
 # import locals
 from ogd.core.processors.Processor import Processor
+from ogd.common.filters.collections.DatasetFilterCollection import DatasetFilterCollection
 from ogd.common.models.Event import Event
-from ogd.common.schemas.games.GameSchema import GameSchema
+from ogd.common.models.EventSet import EventSet
+from ogd.common.configs.GameStoreConfig import GameStoreConfig
 from ogd.common.utils.typing import ExportRow
+from ogd.common.utils.Logger import Logger
 
 class EventProcessor(Processor):
 
     # *** BUILT-INS & PROPERTIES ***
 
-    def __init__(self, game_schema:GameSchema):
+    def __init__(self, game_schema:GameStoreConfig, filters:DatasetFilterCollection=DatasetFilterCollection()):
         super().__init__(game_schema=game_schema)
-        self._events : List[Event] = []
+        self._events : EventSet = EventSet(events=[], filters=filters)
+
+    @property
+    def Events(self) -> EventSet:
+        return self._events
 
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
 
     def _processEvent(self, event:Event):
-        self._events.append(event)
+        self._events.Events.append(event)
 
     def _getLines(self) -> List[ExportRow]:
-        return [_event.ColumnValues() for _event in self._events]
+        ret_val : List[ExportRow]
+
+        start   = datetime.now()
+        ret_val = [_event.ColumnValues for _event in self._events.Events]
+        time_delta = datetime.now() - start
+        Logger.Log(f"Time to retrieve Event lines: {time_delta} to get {len(ret_val)} lines", logging.INFO, depth=2)
+
+        return ret_val
 
     def _clearLines(self) -> None:
-        self._events = []
+        self._events = EventSet(events=[], filters=DatasetFilterCollection())
 
     # *** PUBLIC STATICS ***
 
