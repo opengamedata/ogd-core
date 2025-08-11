6b9eefc1238d0b984fb56f0716f7278691df0790
Luke Swanson <lwswanson2@wisc.edu>
2025-08-11 09:37:48 -0500

    Clean up imports, including from common, in Processor base class.

--- src/ogd/core/processors/Processor.py
+++ src/ogd/core/processors/Processor.py
@@ -1,15 +1,13 @@
 ## import standard libraries
 import abc
 import json
-import logging
 from datetime import datetime
-from typing import Any, Dict, List, Type, Optional
+from typing import List
 # import locals
 from ogd.common.models.Feature import Feature
-from ogd.common.schemas.games.GameSchema import GameSchema
+from ogd.common.configs.GameStoreConfig import GameStoreConfig
 from ogd.common.models.Event import Event
 from ogd.common.utils.typing import ExportRow
-from ogd.common.utils.utils import Logger
 
 ## @class Processor
 class Processor(abc.ABC):
@@ -31,11 +29,11 @@ class Processor(abc.ABC):
 
     # *** BUILT-INS ***
 
-    def __init__(self, game_schema: GameSchema):
-        self._game_schema : GameSchema = game_schema
+    def __init__(self, game_schema: GameStoreConfig):
+        self._game_schema : GameStoreConfig = game_schema
 
     def __str__(self):
-        return f"Processor object for {self._game_schema.GameName} data"
+        return f"Processor object for {self._game_schema.GameID} data"
 
     # *** PUBLIC STATICS ***
 
