807f353f6e479e950be260ece4b1d4fa92090d78
Luke Swanson <lwswanson2@wisc.edu>
2025-08-14 22:41:45 -0500

    Add WIP starting point for GameStoreConfig to manage the 4 different ways to have in and out for things.

---
+++ src/ogd/core/configs/GameStoreConfig.py
@@ -0,0 +1,206 @@
+# import standard libraries
+from typing import Dict, Final, LiteralString, Optional, Self
+# import local files
+from ogd.common.schemas.Schema import Schema
+from ogd.common.configs.DataTableConfig import DataTableConfig
+from ogd.common.configs.storage.DataStoreConfig import DataStoreConfig
+from ogd.common.configs.storage.BigQueryConfig import BigQueryConfig
+from ogd.common.configs.storage.FileStoreConfig import FileStoreConfig
+from ogd.common.configs.storage.DatasetRepositoryConfig import DatasetRepositoryConfig
+from ogd.common.schemas.tables.TableSchema import TableSchema
+from ogd.common.schemas.tables.EventTableSchema import EventTableSchema
+from ogd.common.schemas.tables.FeatureTableSchema import FeatureTableSchema
+from ogd.common.schemas.locations.DatabaseLocationSchema import DatabaseLocationSchema
+from ogd.common.utils.typing import Map
+
+class GameStoreConfig(Schema):
+    """A simple Schema structure containing configuration information for a particular game's data.
+    
+    When given to an interface, this schema is treated as the location from which to retrieve data.
+    When given to an outerface, this schema is treated as the location in which to store data.
+    (note that some interfaces/outerfaces, such as debugging i/o-faces, may ignore the configuration)
+    Key properties of this schema are:
+    - `Name` : Typically, the name of the Game whose source configuration is indicated by this schema
+    - `Source` : A data source where game data is stored
+    - `DatabaseName` : The name of the specific database within the source that contains this game's data
+    - `TableName` : The neame of the specific table within the database holding the given game's data
+    - `TableConfig` : A schema indicating the structure of the table containing the given game's data.
+
+    TODO : use a TableConfig for the table_schema instead of just the name of the schema, like we do with source_schema.
+    TODO : Implement and use a smart Load(...) function of TableConfig to load schema from given name, rather than FromFile.
+    """
+
+    _DEFAULT_GAME_ID     : Final[LiteralString] = "UNKNOWN GAME"
+    _DEFAULT_EVENTS_FROM : Final[DataTableConfig] = DataTableConfig(
+        name="DefaultEventSource",
+        store_name="DefaultStore",
+        schema_name="DefaultTable",
+        table_location=DatabaseLocationSchema.Default(),
+        store_config=BigQueryConfig.Default(),
+        table_schema=EventTableSchema.FromFile(schema_name="OPENGAMEDATA_BQ")
+    )
+    _DEFAULT_EVENTS_TO   : Final[DataTableConfig] = DataTableConfig(
+        name="DefaultEventDest",
+        store_name="DefaultStore",
+        schema_name="DefaultTable",
+        table_location=None,
+        store_config=FileStoreConfig.Default(),
+        table_schema=EventTableSchema.FromFile(schema_name="OGD_EVENTS_FILE")
+    )
+    _DEFAULT_FEATS_FROM  : Final[DataTableConfig] = DataTableConfig(
+        name="DefaultFeatSource",
+        store_name="DefaultStore",
+        schema_name="DefaultTable",
+        table_location=DatabaseLocationSchema.Default(),
+        store_config=BigQueryConfig.Default(),
+        table_schema=FeatureTableSchema.FromFile(schema_name="OPENGAMEDATA_BQ")
+    )
+    _DEFAULT_FEATS_TO    : Final[DataTableConfig] = DataTableConfig(
+        name="DefaultFeatDest",
+        store_name="DefaultStore",
+        schema_name="DefaultTable",
+        table_location=None,
+        store_config=DatasetRepositoryConfig.Default(),
+        table_schema=FeatureTableSchema.FromFile(schema_name="OGD_EVENTS_FILE")
+    )
+
+    # *** BUILT-INS & PROPERTIES ***
+
+    def __init__(self, name:str, game_id:Optional[str],
+                 events_from : Optional[DataTableConfig],
+                 events_to   : Optional[DataTableConfig],
+                 feats_from  : Optional[DataTableConfig],
+                 feats_to    : Optional[DataTableConfig],
+                 other_elements:Optional[Map]=None):
+        """Constructor for the `DataTableConfig` class.
+        
+        If optional params are not given, data is searched for in `other_elements`.
+
+        Expected format:
+
+        ```
+        {
+            "source" : "DATA_SOURCE_NAME",
+            "database": "db_name",
+            "schema" : "TABLE_SCHEMA_NAME",
+            "table" : "table_name"
+        },
+        ```
+
+        :param name: _description_
+        :type name: str
+        :param game_id: _description_
+        :type game_id: Optional[str]
+        :param source_name: _description_
+        :type source_name: Optional[str]
+        :param schema_name: _description_
+        :type schema_name: Optional[str]
+        :param table_location: _description_
+        :type table_location: Optional[DatabaseLocationSchema]
+        :param other_elements: _description_
+        :type other_elements: Optional[Map]
+        """
+        unparsed_elements : Map = other_elements or {}
+
+        self._game_id     : str             = game_id     or name
+        # TODO : use parsing functions
+        self._events_from : DataTableConfig = events_from or self._DEFAULT_EVENTS_FROM
+        self._events_to   : DataTableConfig = events_to   or self._DEFAULT_EVENTS_TO # TODO : in addition to parsing, fall back on default dest being the standard output to repo at ./data/
+        self._feats_from  : DataTableConfig = feats_from  or self._DEFAULT_FEATS_FROM # TODO : in addition to parsing, fall back on default source being same store as events, just a different table schema
+        self._feats_to    : DataTableConfig = feats_to    or self._DEFAULT_FEATS_TO # TODO : in addition to parsing, fall back on default dest being the standard output to repo at ./data/
+
+        super().__init__(name=name, other_elements=other_elements)
+
+    @property
+    def GameID(self) -> str:
+        """Property to get the Game ID (also called App ID) associated with the given game source
+
+        By convention, this is a human-readable simplification of the games name, in CONSTANT_CASE format
+
+        :return: _description_
+        :rtype: str
+        """
+        return self._game_id
+
+        # TODO : add props to acces the different parts
+
+    # *** IMPLEMENT ABSTRACT FUNCTIONS ***
+
+    @property
+    def AsMarkdown(self) -> str:
+        ret_val : str
+
+        ret_val = f"{self.Name}: _{self.TableSchemaName}_ format, source {self.StoreName} : {self.Location.Location}"
+        return ret_val
+
+    @classmethod
+    def Default(cls) -> "DataTableConfig":
+        return DataTableConfig(
+            name="DefaultDataTableConfig",
+            game_id=cls._DEFAULT_GAME_ID,
+            store_name=cls._DEFAULT_STORE_NAME,
+            store_config=None,
+            schema_name=cls._DEFAULT_TABLE_SCHEMA_NAME,
+            table_schema=None,
+            table_location=cls._DEFAULT_TABLE_LOC,
+            other_elements={}
+        )
+
+    @classmethod
+    def _fromDict(cls, name:str, unparsed_elements:Map,
+                  key_overrides:Optional[Dict[str, str]]=None,
+                  default_override:Optional[Self]=None) -> "DataTableConfig":
+        """Create a DataTableConfig from a given dictionary
+
+        TODO : Add example of what format unparsed_elements is expected to have.
+        TODO : data_sources shouldn't really be a param here. Better to have e.g. a way to register the list into DataTableConfig class, or something.
+
+        :param name: _description_
+        :type name: str
+        :param all_elements: _description_
+        :type all_elements: Dict[str, Any]
+        :param logger: _description_
+        :type logger: Optional[logging.Logger]
+        :param data_sources: _description_
+        :type data_sources: Dict[str, DataStoreConfig]
+        :return: _description_
+        :rtype: DataTableConfig
+        """
+        return DataTableConfig(name=name, game_id=None, store_name=None, schema_name=None,
+                                table_location=None, other_elements=unparsed_elements)
+
+    # *** PUBLIC STATICS ***
+
+    # *** PUBLIC METHODS ***
+
+    # *** PRIVATE STATICS ***
+
+    @staticmethod
+    def _parseStoreName(unparsed_elements:Map) -> str:
+        return DataTableConfig.ParseElement(
+            unparsed_elements=unparsed_elements,
+            valid_keys=["source", "source_name", "store", "store_name"],
+            to_type=str,
+            default_value=DataTableConfig._DEFAULT_STORE_NAME,
+            remove_target=True
+        )
+
+    @staticmethod
+    def _parseTableSchemaName(unparsed_elements:Map) -> str:
+        return DataTableConfig.ParseElement(
+            unparsed_elements=unparsed_elements,
+            valid_keys=["table_schema", "schema"],
+            to_type=str,
+            default_value=DataTableConfig._DEFAULT_TABLE_SCHEMA_NAME,
+            remove_target=True
+        )
+
+    @staticmethod
+    def _parseTableLocation(unparsed_elements:Map) -> DatabaseLocationSchema:
+        return DatabaseLocationSchema.FromDict(
+            name="TableLocation",
+            unparsed_elements=unparsed_elements,
+            default_override=DataTableConfig._DEFAULT_TABLE_LOC
+        )
+
+    # *** PRIVATE METHODS ***
