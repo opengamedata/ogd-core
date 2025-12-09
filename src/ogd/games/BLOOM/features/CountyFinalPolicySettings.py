7e2237bfb40652f7e52a1dc8b0ce71ac05515fc3
Luke Swanson <lwswanson2@wisc.edu>
2025-12-09 20:15:46 +0000

    When unlocking a county, set null policies to 'not set' instead.

--- src/ogd/games/BLOOM/features/CountyFinalPolicySettings.py
+++ src/ogd/games/BLOOM/features/CountyFinalPolicySettings.py
@@ -20,21 +20,28 @@ class CountyFinalPolicySettings(PerCountyFeature):
     # *** IMPLEMENT ABSTRACT FUNCTIONS ***
     @classmethod
     def _eventFilter(cls, mode: ExtractionMode) -> List[str]:
-        return ["select_policy_card"]
+        return ["select_policy_card", "county_unlocked"]
 
     @classmethod
     def _featureFilter(cls, mode: ExtractionMode) -> List[str]:
         return []
 
     def _updateFromEvent(self, event: Event) -> None:
-        policy_name = event.EventData.get("policy", None)
-        choice_name = event.EventData.get("choice_name", None)
+        match event.EventName:
+            case "county_unlocked":
+                self.policy_settings["SalesTaxPolicy"]  = self.policy_settings["SalesTaxPolicy"] or "Not Set"
+                self.policy_settings["RunoffPolicy"]    = self.policy_settings["RunoffPolicy"] or "Not Set"
+                self.policy_settings["ImportTaxPolicy"] = self.policy_settings["ImportTaxPolicy"] or "Not Set"
+                self.policy_settings["SkimmingPolicy"]  = self.policy_settings["SkimmingPolicy"] or "Not Set"
+            case "select_policy_card":
+                policy_name = event.EventData.get("policy", None)
+                choice_name = event.EventData.get("choice_name", None)
 
-        if policy_name is not None and choice_name is not None:
-            if policy_name in self.policy_settings:
-                self.policy_settings[policy_name] = choice_name
-            else:
-                self.WarningMessage(f"Got a select_policy_card with unexpected policy type {policy_name}")
+                if policy_name is not None and choice_name is not None:
+                    if policy_name in self.policy_settings:
+                        self.policy_settings[policy_name] = choice_name
+                    else:
+                        self.WarningMessage(f"Got a select_policy_card with unexpected policy type {policy_name}")
 
     def _updateFromFeatureData(self, feature: FeatureData):
         pass
