f3266fd7ea32e838d18ff03b8cd733c1923ae656
TheAlmightyHelix <ccyyhh97@hotmail.com>
2026-01-06 22:15:52 -0600

    Enhance PlayerProgression to track session IDs and hibernation time. Update time calculations for game events to account for session gaps.

--- src/ogd/games/BLOOM/features/PlayerProgression.py
+++ src/ogd/games/BLOOM/features/PlayerProgression.py
@@ -15,13 +15,15 @@ class PlayerProgression(Feature):
         self.nodes = {} # dict of nodes
         self.links = {} # nested dict (src_hub_id -> dest_hub_id -> count & type) of links
         self.prev_node = {}
-        self.prev_event_timestamp = None
+        self.current_session_id = None
+        self.prev_event_timestamp = timedelta(0)
+        self.hybernation_time = timedelta(0)
+        self.game_start_timestamp = timedelta(0)
 
         
     
     @classmethod
     def _eventFilter(cls, mode: ExtractionMode) -> List[str]:
-        # return ["click_new_game", "click_resume_game", "county_unlocked", "win_game"]
         return ["all_events"]
 
     @classmethod
@@ -29,19 +31,42 @@ class PlayerProgression(Feature):
         return []
 
     def _updateFromEvent(self, event: Event) -> None:
-        # if event.EventName == "click_resume_game" and self.prev_event_timestamp is not None:
+        if event.UserID is None or event.UserID == "":
+            return
+        
+        # measure time gap between sessions
+        session_id = event.SessionID
+        if event.EventName == "click_play_game":
+            self.game_start_timestamp = event.Timestamp
 
+        if self.current_session_id is None:
+            self.current_session_id = session_id
+        
+        if session_id != self.current_session_id:
+            self.hybernation_time += event.Timestamp - self.prev_event_timestamp
+            self.prev_event_timestamp = event.Timestamp
+            self.current_session_id = session_id
+
+        # update starting node (Hillside) based on click_play_game event
         # if event.EventName == "click_play_game":
-        #     self.prev_node = {}
-        #     return
+        #     self.nodes["Hillside"] = {
+        #         "node_count": 1,
+        #         "percentage_completed": 0,
+        #         "time_spent": timedelta(0),
+        #     }
+        #     self.prev_node = {"id": "Hillside", "timestamp": event.Timestamp}
 
+
+        # update ending node (Urban) based on win_game event
         if event.EventName == "win_game":     
             self.nodes["Urban"] = {     
                 "node_count": 1,
                 "percentage_completed": 1,
-                "time_spent": event.Timestamp - self.prev_node["timestamp"],
+                "time_spent": event.Timestamp - self.prev_node["timestamp"] - self.hybernation_time,
             }
+            self.hybernation_time = timedelta(0)
             
+        # update intermediate nodes and links for county_unlocked event
         if event.EventName == "county_unlocked":
             cur_node = {"id": event.EventData.get("county_name"), "timestamp": event.Timestamp}
 
@@ -55,9 +80,9 @@ class PlayerProgression(Feature):
             
             if self.prev_node and event.EventName == "county_unlocked":
                 self.nodes[self.prev_node["id"]]["percentage_completed"] = 1
-                self.nodes[self.prev_node["id"]]["time_spent"] = event.Timestamp - self.prev_node["timestamp"]
+                self.nodes[self.prev_node["id"]]["time_spent"] = event.Timestamp - self.prev_node["timestamp"] - self.hybernation_time
+                self.hybernation_time = timedelta(0)
                 
-
             # update links
             if self.prev_node and cur_node["id"] != self.prev_node["id"]:
                 if self.prev_node["id"] not in self.links:
